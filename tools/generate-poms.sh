63bce29e672223018a893ecb4ac7cd49c24f05a9
Angus Davis <angusdavis@google.com>
2014-07-09 17:17:59 -0700

    Autodetect version in pom.xml files 	Change on 2014/07/01 by pclay <pclay@google.com> ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=70333230

--- tools/generate-poms.sh
+++ tools/generate-poms.sh
@@ -20,57 +20,32 @@
 # mvn -Phadoop2 package -f pom.xml-hadoop2
 #
 # This script should be invoked as
-# tools/generate-poms.sh <version>
+# tools/generate-poms.sh
 #
 # This script is based off of the hbase project's generate-hadoopX-poms.sh
 
 set -o errexit
 
-function print_usage()
-{
-  echo ""
-  echo "Usage: tools/generate.sh <currentVersion>"
-  echo "  currentVersion - The current version contained in the root pom.xml"
-}
-
 function generate_pom_files()
 {
-  hadoop_specific_version=$1
-  hadoop_specific_pom=$2
+  hadoop_specific_suffix=$1
+  hadoop_specific_pom="pom.xml-${hadoop_specific_suffix}"
 
   pom_files=$(find ./ -name pom.xml)
 
-  for pom_file in $pom_files; do
-    full_hadoop_specific_pom_path="$(dirname $pom_file)/${hadoop_specific_pom}"
+  for pom_file in ${pom_files}; do
+    full_hadoop_specific_pom_path="$(dirname ${pom_file})/${hadoop_specific_pom}"
 
-    echo "Generating POM $full_hadoop_specific_pom_path"
+    echo "Generating POM ${full_hadoop_specific_pom_path}"
 
-    sed -e "s/<version>${VERSION}/<version>$hadoop_specific_version/" \
+    sed -e "/<artifactId>bigdataoss-parent<\/artifactId>/{n;
+            s/\(<version>[^<]*\)/\1-${hadoop_specific_suffix}/;}" \
         -e "s/\(<module>[^<]*\)/\1\/${hadoop_specific_pom}/" \
         -e "s/\(relativePath>\.\.\)/\1\/${hadoop_specific_pom}/" \
-        $pom_file > $full_hadoop_specific_pom_path
+        ${pom_file} > ${full_hadoop_specific_pom_path}
 
   done
 }
 
-declare -r VERSION=$1
-
-if [ -z "$VERSION" ]; then
-  echo "Current version not provided."
-  print_usage
-  exit 1
-fi
-
-# Sanity check version:
-if ! grep -q -e "<version>$VERSION</version>" pom.xml; then
-  echo "Failed to find version $VERSION in pom.xml"
-  exit 1
-fi
-
-declare -r HADOOP1_POM_NAME="pom.xml-hadoop1"
-declare -r HADOOP1_VERSION="$VERSION-hadoop1"
-declare -r HADOOP2_POM_NAME="pom.xml-hadoop2"
-declare -r HADOOP2_VERSION="$VERSION-hadoop2"
-
-generate_pom_files $HADOOP1_VERSION $HADOOP1_POM_NAME
-generate_pom_files $HADOOP2_VERSION $HADOOP2_POM_NAME
+generate_pom_files 'hadoop1'
+generate_pom_files 'hadoop2'
