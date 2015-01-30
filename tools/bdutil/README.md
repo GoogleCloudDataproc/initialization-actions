1243f6476ddd2369d5aee4e055af07a6dbe74167
Angus Davis <angusdavis@google.com>
2015-01-29 16:26:46 -0800

    Move bdutil to its own repository: GoogleCloudPlatform/bdutil.

--- tools/bdutil/README.md
+++
@@ -1,3 +0,0 @@
-# bdutil
-
-Tools for creating Hadoop and Spark clusters on Google Compute Engine. See http://cloud.google.com/hadoop for more information.
--- tools/bdutil/platforms/hdp/resources/public-hostname-gcloud.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-curl -Ls -m 5 http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google"
-
