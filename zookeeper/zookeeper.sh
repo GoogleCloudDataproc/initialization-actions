8de9409bf91c5083c175b0563f0af17c11220fe3
James Malone <jamalone@gmail.com>
2015-11-24 14:27:44 -0800

    Adding Kafka and ZooKeeper
    Adding init actions for ZooKeeper and Kafka.

---
+++ zookeeper/zookeeper.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#    Copyright 2015 Google, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.set -x -e
+set -x -e
+
+# Variables for this script
+ZOOKEEPER_VERSION="3.4.6"
+ROLE=$(/usr/share/google/get_metadata_value attributes/role)
+CLUSTER_NAME=$(hostname | sed 's/\(.*\)-w-[0-9]*.*/\1/g')
+
+# Download and extract ZooKeeper
+cd ~
+wget http://www.us.apache.org/dist/zookeeper/zookeeper-${ZOOKEEPER_VERSION}/zookeeper-${ZOOKEEPER_VERSION}.tar.gz
+tar zxvf zookeeper-${ZOOKEEPER_VERSION}.tar.gz
+mkdir -p /var/lib/zookeeper
+
+# Configure ZooKeeper node ID
+NODE_NUMBER=1
+if [[ "${ROLE}" == 'Worker' ]]; then
+	NODE_NUMBER=$((`hostname | sed 's/.*-w-\([0-9]\)*.*/\1/g'`+2))
+fi
+echo ${NODE_NUMBER} > /var/lib/zookeeper/myid
+
+# Write ZooKeeper configuration file
+cat > zookeeper-${ZOOKEEPER_VERSION}/conf/zoo.cfg <<EOF
+tickTime=2000
+dataDir=/var/lib/zookeeper
+clientPort=2181
+initLimit=5
+syncLimit=2
+server.1=${CLUSTER_NAME}-m.c.hadoop-cloud-dev.google.com.internal:2888:3888
+server.2=${CLUSTER_NAME}-w-0.c.hadoop-cloud-dev.google.com.internal:2888:3888
+server.3=${CLUSTER_NAME}-w-1.c.hadoop-cloud-dev.google.com.internal:2888:3888
+EOF
+
+# Start ZooKeeper
+zookeeper-${ZOOKEEPER_VERSION}/bin/zkServer.sh start
