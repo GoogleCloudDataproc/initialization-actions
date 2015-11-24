9f64b7e92f46ddf50a449ed82bebc1dad536545c
angusdavis <angusdavis@google.com>
2015-11-23 16:48:30 -0800

    Fix handling of splits in AvroRecordReader.
    In sharded exports, each file is always read from 0 to the length of the file
    and those should not be impacted by this.
    
    Add unit tests around AvroRecordReader to complement integration tests.
    	Change on 2015/11/23 by angusdavis <angusdavis@google.com>
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=108563364

--- bigquery/src/main/java/com/google/cloud/hadoop/io/bigquery/AvroRecordReader.java
+++ bigquery/src/main/java/com/google/cloud/hadoop/io/bigquery/AvroRecordReader.java
@@ -8,6 +8,7 @@ import org.apache.avro.file.FileReader;
 import org.apache.avro.file.SeekableInput;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -33,19 +34,29 @@ public class AvroRecordReader extends RecordReader<LongWritable, GenericData.Rec
   Schema schema;
   GenericData.Record currentRecord;
   long inputFileLength;
+  long splitStart;
+  long splitLength;
 
   @Override
   public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext)
       throws IOException, InterruptedException {
+    initializeInternal(inputSplit, taskAttemptContext.getConfiguration());
+  }
+
+  protected void initializeInternal(InputSplit inputSplit, Configuration conf)
+      throws IOException, InterruptedException {
     Preconditions.checkState(
         inputSplit instanceof FileSplit, "AvroRecordReader requires FileSplit input splits.");
-
     FileSplit fileSplit = (FileSplit) inputSplit;
+    splitStart = fileSplit.getStart();
+    splitLength = fileSplit.getLength();
+
     Path filePath = fileSplit.getPath();
-    FileSystem fs = filePath.getFileSystem(taskAttemptContext.getConfiguration());
+    FileSystem fs = filePath.getFileSystem(conf);
     FileStatus status = fs.getFileStatus(filePath);
-    final FSDataInputStream stream = fs.open(filePath);
     inputFileLength = status.getLen();
+
+    final FSDataInputStream stream = fs.open(filePath);
     dataFileReader = DataFileReader.openReader(
         new SeekableInput() {
           @Override
@@ -72,7 +83,8 @@ public class AvroRecordReader extends RecordReader<LongWritable, GenericData.Rec
             stream.close();
           }
         }, new GenericDatumReader<GenericData.Record>());
-
+    // Sync to the first sync point after the start of the split:
+    dataFileReader.sync(fileSplit.getStart());
     schema = dataFileReader.getSchema();
     currentRecord = new GenericData.Record(schema);
   }
@@ -80,7 +92,8 @@ public class AvroRecordReader extends RecordReader<LongWritable, GenericData.Rec
   @Override
   public boolean nextKeyValue() throws IOException, InterruptedException {
     Preconditions.checkState(currentRecord != null);
-    if (dataFileReader.hasNext()) {
+    // Stop reading as soon as we hit a sync point out of our split:
+    if (dataFileReader.hasNext() && !dataFileReader.pastSync(splitStart + splitLength)) {
       currentKey.set(currentKey.get() + 1);
       dataFileReader.next(currentRecord);
       return true;
@@ -102,7 +115,11 @@ public class AvroRecordReader extends RecordReader<LongWritable, GenericData.Rec
   @Override
   public float getProgress() throws IOException, InterruptedException {
     Preconditions.checkState(dataFileReader != null);
-    return inputFileLength / (float) dataFileReader.tell();
+    if (splitLength == 0) {
+      return 1.0f;
+    }
+    float splitRelativeLocation = dataFileReader.tell() - splitStart;
+    return splitRelativeLocation / splitLength;
   }
 
   @Override
---
+++ bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/AvroRecordReaderTest.java
@@ -0,0 +1,130 @@
+package com.google.cloud.hadoop.io.bigquery;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.truth.Truth;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(JUnit4.class)
+public class AvroRecordReaderTest {
+
+  private static final int RECORD_COUNT = 50;
+  private static final int AUTO_SYNC_INTERVAL = 32; /* Auto sync every 32 bytes */
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private File testAvroFile;
+  private List<String> allAddedKeys;
+
+  @Before
+  public void setup() throws IOException {
+    Schema schema =
+        SchemaBuilder.record("BigQueryRecord").fields()
+            .name("key").type().stringBuilder().endString().noDefault()
+            .name("value1").type().stringBuilder().endString().noDefault()
+            .name("value2").type().intBuilder().endInt().noDefault()
+            .endRecord();
+
+    GenericDatumWriter<GenericData.Record> recordWriter = new GenericDatumWriter<>(schema);
+    testAvroFile = temporaryFolder.newFile("TestAvroFile");
+    if (testAvroFile.exists()) {
+      testAvroFile.delete();
+    }
+    DataFileWriter<GenericData.Record> dataFileWriter =
+        new DataFileWriter<>(recordWriter).create(schema, testAvroFile);
+    dataFileWriter.setSyncInterval(AUTO_SYNC_INTERVAL);
+    ImmutableList.Builder<String> addedKeysBuilder = ImmutableList.builder();
+    for (int idx = 0; idx < RECORD_COUNT; idx++) {
+      GenericData.Record record = new GenericData.Record(schema);
+      String key = String.format("key-%s", idx);
+      record.put("key", key);
+      record.put("value1", String.format("value-%s", idx));
+      record.put("value2", new Integer(idx * RECORD_COUNT));
+      dataFileWriter.append(record);
+      addedKeysBuilder.add(key);
+    }
+    dataFileWriter.close();
+    allAddedKeys = addedKeysBuilder.build();
+  }
+
+  /**
+   * Collect all values contained in the field "key" within all records in the given reader.
+   */
+  private List<String> collectRecordKeys(AvroRecordReader recordReader)
+      throws IOException, InterruptedException {
+    List<String> result = new ArrayList<>();
+    while (recordReader.nextKeyValue()) {
+      result.add(recordReader.currentRecord.get("key").toString());
+    }
+    return result;
+  }
+
+  /**
+   * Count all records available until the reader reports that no more are available.
+   */
+  private int remainingRecordCount(AvroRecordReader recordReader)
+      throws IOException, InterruptedException {
+    return collectRecordKeys(recordReader).size();
+  }
+
+  @Test
+  public void testSingleSplit() throws IOException, InterruptedException {
+    FileSplit fileSplit =
+        new FileSplit(
+            new Path("file://" + testAvroFile.getAbsolutePath()),
+            0,
+            testAvroFile.length(),
+            new String[0]);
+    AvroRecordReader recordReader = new AvroRecordReader();
+    recordReader.initializeInternal(fileSplit, new Configuration());
+    Assert.assertEquals(RECORD_COUNT, remainingRecordCount(recordReader));
+    recordReader.close();
+  }
+
+  @Test
+  public void testMultipleSplits() throws IOException, InterruptedException {
+    long fileLength = testAvroFile.length();
+    List<FileSplit> splits = new ArrayList<>();
+    Path hadoopPath = new Path("file://" + testAvroFile.getAbsolutePath());
+
+    for (int blockStart = 0; blockStart < fileLength; blockStart += AUTO_SYNC_INTERVAL) {
+      splits.add(new FileSplit(hadoopPath, blockStart, AUTO_SYNC_INTERVAL, new String[0]));
+    }
+
+    List<String> allRecordKeys = new ArrayList<>();
+    long totalFileRecords = 0;
+    for (FileSplit split : splits) {
+      try (AvroRecordReader reader = new AvroRecordReader()) {
+        reader.initializeInternal(split, new Configuration());
+        List<String> keysInSplit = collectRecordKeys(reader);
+        allRecordKeys.addAll(keysInSplit);
+        int recordsInSplit = keysInSplit.size();
+        totalFileRecords += recordsInSplit;
+        // Not all 'blocks' contain records, but none should have all records
+        Truth.assertThat(recordsInSplit)
+            .isLessThan(RECORD_COUNT);
+      }
+    }
+
+    Truth.assertThat(allRecordKeys).containsExactlyElementsIn(allAddedKeys);
+    Assert.assertEquals(RECORD_COUNT, totalFileRecords);
+  }
+}
