ee3ec14c0ef3f2f833b2eeeddcbd19a4ce697ad4
Nicholaus Halecky <nehalecky@gmail.com>
2015-12-28 11:55:13 -0800

    ENH: Parameterize Jupyter / IPython config paths

--- jupyter/setup-jupyter.sh
+++ jupyter/setup-jupyter.sh
@@ -6,13 +6,10 @@ set -x -e
 NOTEBOOK_DIR="./notebooks"
 
 # 1. Ensure pyspark configuration exists
-IPYTHON_STARTUP_PATH="~/.ipython/profile_default/startup/"
+#  /root/.ipython/profile_default/startup/00-pyspark-setup.py
+IPYTHON_STARTUP_CONFIG_PATH="/dataproc_intialization_actions/jupyter/00_setup_pyspark.py"
+IPYTHON_STARTUP_PATH="/root/.ipython/profile_default/startup/"
 
-if [[ ! -d $IPYTHON_STARTUP_PATH ]]; then
-    mkdir -p ~/.ipython/profile_default/startup/
-else
-    echo "IPython startup path detected, skipping..."
-fi
 
 # 2. Install and launch Jupyter (only run on the master node)
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
@@ -21,6 +18,16 @@ if [[ "${ROLE}" == 'Master' ]]; then
     # 1. Install Jupyter
     # Assumes conda is already installed and available in $PATH
     conda install jupyter
+
+    # 2. Ensure PySpark is configured
+    if [[ ! -d $IPYTHON_STARTUP_PATH ]]; then
+        echo "IPython startup config path not detected, creating..."
+        mkdir -p $IPYTHON_STARTUP_CONFIG_PATH
+        echo "Copying IPython config from data proc init action repo..."
+        cp -p $IPYTHON_STARTUP_CONFIG_PATH $IPYTHON_STARTUP_PATH
+    else
+        echo "IPython startup path detected, skipping..."
+    fi
 
 	# 2. Launch Jupyter notebook on port 8123
 	if [[ ! -d $NOTEBOOK_DIR ]]; then
