d48cde66937c6dbd15410d0afa9eab2ef85161e5
Nicholaus Halecky <nehalecky@gmail.com>
2016-01-13 17:27:27 -0800

    DEV: Update Miniconda versions

--- conda/bootstrap-conda.sh
+++ conda/bootstrap-conda.sh
@@ -12,14 +12,14 @@ CONDA_INSTALL_PATH="/usr/local/bin/miniconda"
 # specify base operating system
 OS_TYPE="Linux-x86_64.sh"
 # specify Miniconda release
-MINICONDA_VER='3.18.3'
+MINICONDA_VER='3.19.0'
 ## Python 2 or 3?
 #MINICONDA_VARIANT="Miniconda2"  #for Python 2.7.x
-#expectedHash="3f1d3e33dd154aacd7367931d595a74c"
+#expectedHash="628f158791daf7d634fb6894045a6be1"
 MINICONDA_VARIANT="Miniconda3"  #for Python 3.5.x
-expectedHash="245a839794fe2c3e7a35691ae1d3033f"
+expectedHash="b834b525d3f42add8f2af0153d13f498"
 
-## 0.2 Compute Miniconda version
+## 0. Compute Miniconda version
 miniconda="$MINICONDA_VARIANT-$MINICONDA_VER-$OS_TYPE"
 
 # 1. Setup Miniconda Install
