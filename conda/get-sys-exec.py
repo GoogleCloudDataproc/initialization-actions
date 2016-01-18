6440da5813ae38da781f7dd51ebed18cadb55b5d
Nicholaus Halecky <nehalecky@gmail.com>
2016-01-18 11:17:00 -0800

    TST: Update test to include PYTHONHASHSEED info

--- conda/get-sys-exec.py
+++ conda/get-sys-exec.py
@@ -1,8 +1,18 @@
 import pyspark
 import sys
+import os
 
-sc = pyspark.SparkContext()
-distData = sc.parallelize(range(100))
-python_distros = distData.map(lambda x: sys.executable).distinct().collect()
+pyhashseed = os.environ['PYTHONHASHSEED']
+print(pyhashseed)
+print(type(pyhashseed))
+print(sys.version)
 
-print(python_distros)
+
+if sys.version >= '3.3' and 'PYTHONHASHSEED' not in os.environ:
+    raise Exception("Randomness of hash of string should be disabled via PYTHONHASHSEED")
+
+else:
+   sc = pyspark.SparkContext()
+   distData = sc.parallelize(range(100))
+   python_distros = distData.map(lambda x: sys.executable).distinct().collect()
+   print(python_distros)
