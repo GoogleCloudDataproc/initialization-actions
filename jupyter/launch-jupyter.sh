3f8dd7b85297b49d56086924d51b1f600fe52672
Nicholaus Halecky <nehalecky@gmail.com>
2016-01-19 18:21:55 -0800

    ENH: Add script to launch Jupyter notebook (kernel)

---
+++ jupyter/launch-jupyter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x -e
+
+# 0. Set parameters
+# TODO: Detect if directory exists in working directory
+NOTEBOOK_DIR="/root/notebooks"
+
+# 2. Launch Jupyter notebook on port 8123
+if [[ ! -d $NOTEBOOK_DIR ]]; then
+    nohup jupyter notebook \
+        --no-browser \
+        --ip=* \
+        --port=8123 | tee -a /var/log/jupyter_notebook.log &
+else
+    nohup jupyter notebook \
+        --notebook-dir=$NOTEBOOK_DIR \
+        --no-browser \
+        --ip=* \
+        --port=8123 | tee -a /var/log/jupyter_notebook.log &
+fi
+echo "Jupyter notebook launched on port 8123!"
