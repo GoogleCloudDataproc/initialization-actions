14b31b9239c4d57a3f11f99c1ffd57fbf67fabff
Nicholaus Halecky <nehalecky@gmail.com>
2016-01-21 14:12:02 -0800

    BUG: Attempt to allow Jupyter to launch on bootstrap

--- jupyter/launch-jupyter.sh
+++ jupyter/launch-jupyter.sh
@@ -7,15 +7,8 @@ NOTEBOOK_DIR="/root/notebooks"
 
 # 2. Launch Jupyter notebook on port 8123
 if [[ ! -d $NOTEBOOK_DIR ]]; then
-    nohup jupyter notebook \
-        --no-browser \
-        --ip=* \
-        --port=8123 | tee -a /var/log/jupyter_notebook.log &
+    nohup jupyter notebook --no-browser --ip=* --port=8123 | tee -a /var/log/jupyter_notebook.log &
 else
-    nohup jupyter notebook \
-        --notebook-dir=$NOTEBOOK_DIR \
-        --no-browser \
-        --ip=* \
-        --port=8123 | tee -a /var/log/jupyter_notebook.log &
+    nohup jupyter notebook --notebook-dir=$NOTEBOOK_DIR --no-browser --ip=* --port=8123 | tee -a /var/log/jupyter_notebook.log &
 fi
 echo "Jupyter notebook launched on port 8123!"
