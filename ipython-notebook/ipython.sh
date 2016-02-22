0b104c72e11efbdd0a399c776f02f7c8b6f469b4
Quentin Pradet <quentin@pradet.me>
2016-02-22 15:49:59 +0400

    ipython-notebook: Update py4j path

--- ipython-notebook/ipython.sh
+++ ipython-notebook/ipython.sh
@@ -41,7 +41,7 @@ import sys
 spark_home = '/usr/lib/spark/'
 os.environ["SPARK_HOME"] = spark_home
 sys.path.insert(0, os.path.join(spark_home, 'python'))
-sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.8.2.1-src.zip'))
+sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.9-src.zip'))
 execfile(os.path.join(spark_home, 'python/pyspark/shell.py'))
 _EOF
 	
