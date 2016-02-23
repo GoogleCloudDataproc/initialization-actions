331f3a229493b466021b9bd9dedc963d99cfda4b
Nicholaus Halecky <nehalecky@gmail.com>
2016-02-23 01:01:12 -0800

    ENH: Parameterize dataproc info

--- jupyter/launch-jupyter-interface.sh
+++ jupyter/launch-jupyter-interface.sh
@@ -3,8 +3,11 @@ set -e
 
 # NOTE: Ensure that Jupyter notebook is running on cluster master node
 # TODO: detect the project cluster name using gcloud tools
-DATAPROC_CLUSTER_NAME="spark-cluster"
-GCP_ZONE="us-east1-b"
+#DATAPROC_CLUSTER_NAME="spark-cluster"
+#GCP_ZONE="us-east1-b"
+if [[ ! -v JUPYTER_PORT ]]; then
+    JUPYTER_PORT=8123
+fi
 
 # 0. Set default path to Chrome application (by operating system type).
 # OS X
@@ -17,14 +20,14 @@ CHROME_APP_PATH="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
 # Following configuration at:
 # https://cloud.google.com/dataproc/cluster-web-interfaces
 # 1. Setup ssh tunnel and socks proxy
-gcloud compute ssh  --zone=$GCP_ZONE \
-    --ssh-flag="-D 1080" --ssh-flag="-N" --ssh-flag="-n" "$DATAPROC_CLUSTER_NAME-m" &
+gcloud compute ssh  --ssh-flag="-D 1080" \
+     --ssh-flag="-N" --ssh-flag="-n" "$DATAPROC_CLUSTER_NAME-m" &
 
 # 2.Launch Chrome instance, referencing the proxy server.
 # TODO: Parameterize the chrome app path
 # eval $CHROME_APP_PATH \
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
-    "http://$DATAPROC_CLUSTER_NAME-m:8123" \
+    "http://$DATAPROC_CLUSTER_NAME-m:$JUPYTER_PORT" \
     --proxy-server="socks5://localhost:1080" \
     --host-resolver-rules="MAP * 0.0.0.0 , EXCLUDE localhost" \
     --user-data-dir=/tmp/
--- jupyter/launch-jupyter.sh
+++ jupyter/launch-jupyter.sh
@@ -4,11 +4,13 @@ set -x -e
 # 0. Set parameters
 # TODO: Detect if directory exists in working directory
 NOTEBOOK_DIR="/root/notebooks"
+JUPYTER_PORT=8123
+JUPYTER_IP=*
 
 # 2. Launch Jupyter notebook on port 8123
 if [[ ! -d $NOTEBOOK_DIR ]]; then
-    nohup jupyter notebook --no-browser --ip=* --port=8123 | tee -a /var/log/jupyter_notebook.log &
+    nohup jupyter notebook --no-browser --ip=JUPYTER_IP --port=$JUPYTER_PORT | tee -a /var/log/jupyter_notebook.log &
 else
-    nohup jupyter notebook --notebook-dir=$NOTEBOOK_DIR --no-browser --ip=* --port=8123 | tee -a /var/log/jupyter_notebook.log &
+    nohup jupyter notebook --notebook-dir=$NOTEBOOK_DIR --no-browser --ip=JUPYTER_IP --port=$JUPYTER_PORT  | tee -a /var/log/jupyter_notebook.log &
 fi
-echo "Jupyter notebook launched on port 8123!"
+echo "Jupyter notebook launched on $JUPYTER_IP:$JUPYTER_PORT!"
