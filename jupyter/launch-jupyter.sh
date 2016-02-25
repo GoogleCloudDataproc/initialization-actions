86ad687f32e6997ceb8d34fbb8acae18f07e3b21
Jeffrey Scott Keone Payne <jeffkpayne@gmail.com>
2016-02-25 12:34:36 -0800

    BUG: Change from tee to > for jupyter log file redirect

--- jupyter/launch-jupyter.sh
+++ jupyter/launch-jupyter.sh
@@ -9,8 +9,8 @@ JUPYTER_IP=*
 
 # 2. Launch Jupyter notebook on port 8123
 if [[ ! -d $NOTEBOOK_DIR ]]; then
-    nohup jupyter notebook --no-browser --ip=$JUPYTER_IP --port=$JUPYTER_PORT | tee -a /var/log/jupyter_notebook.log &
+    nohup jupyter notebook --no-browser --ip=$JUPYTER_IP --port=$JUPYTER_PORT > /var/log/jupyter_notebook.log &
 else
-    nohup jupyter notebook --notebook-dir=$NOTEBOOK_DIR --no-browser --ip=$JUPYTER_IP --port=$JUPYTER_PORT  | tee -a /var/log/jupyter_notebook.log &
+    nohup jupyter notebook --notebook-dir=$NOTEBOOK_DIR --no-browser --ip=$JUPYTER_IP --port=$JUPYTER_PORT > /var/log/jupyter_notebook.log &
 fi
 echo "Jupyter notebook launched on $JUPYTER_IP:$JUPYTER_PORT!"
