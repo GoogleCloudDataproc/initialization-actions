1a611fe7de8b0176136138cfc8bf4bacebda85d0
Jeffrey Scott Keone Payne <jeffkpayne@gmail.com>
2016-03-08 03:44:52 -0800

    BUG: Use original way to start spark context

--- jupyter/start_pyspark_for_kernel.py
+++ jupyter/start_pyspark_for_kernel.py
@@ -1,10 +1,24 @@
-import findspark
+# To be copied to ~/.ipython/profile_default/startup/
+# See associated README.md
 
-findspark.init()
+import os
+import sys
 
-import pyspark
-from pyspark.sql import HiveContext
+spark_home = '/usr/lib/spark/'
+os.environ["SPARK_HOME"] = spark_home
+sys.path.insert(0, os.path.join(spark_home, 'python'))
 
-sc = pyspark.SparkContext()
-sqlContext = HiveContext(sc)
+# # If PySpark isn't specified, use currently running Python binary:
+# pyspark_python = None
+# pyspark_python = pyspark_python or sys.executable
+# os.environ['PYSPARK_PYTHON'] = pyspark_python
+
+# Launch PySpark Shell
+spark_shell_path = os.path.join(spark_home, 'python/pyspark/shell.py')
+# attempt to execute first via Python 2
+try:
+    execfile(spark_shell_path)
+# and if not, Python 3
+except NameError:
+    exec(open(spark_shell_path).read())
 
