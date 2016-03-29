c880d9a590cd55634f2519fdc4f55e87f13bf19d
Jeffrey Scott Keone Payne <jeffkpayne@gmail.com>
2016-03-29 16:53:14 -0700

    ENH: Applied changes suggested by dennishuo.

--- jupyter/internal/setup-jupyter-kernel.sh
+++ jupyter/internal/setup-jupyter-kernel.sh
@@ -3,9 +3,7 @@ set -e
 
 DIR="${BASH_SOURCE%/*}"
 [[ ! -d "$DIR" ]] && DIR="$PWD"
-
-source "$DIR/../util/utils.sh"
-[[ ! -v CONDA_BIN_PATH ]] && source /etc/profile.d/conda_config.sh
+source "$DIR/../../util/utils.sh"
 
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 JUPYTER_KERNEL_DIR="/dataproc-initialization-actions/jupyter/kernels/pyspark"
@@ -15,7 +13,6 @@ JUPYTER_PORT=$(/usr/share/google/get_metadata_value attributes/JUPYTER_PORT)
 JUPYTER_IP=*
 
 [[ "${ROLE}" != 'Master' ]] && throw "$0 should only be run on the Master node!"
-[[ ! $JUPYTER_PORT =~ ^[0-9]+$ ]] && throw "metadata must contain a valid 'JUPITER_PORT' value, but instead has the value \"$JUPYTER_PORT\"!"
 
 echo "Creating notebook directory $JUPYTER_NOTEBOOK_DIR if necessary..."
 [[ ! -d $JUPYTER_NOTEBOOK_DIR ]] && mkdir -p $JUPYTER_NOTEBOOK_DIR
@@ -32,6 +29,5 @@ echo "Installing pyspark Kernel..."
 jupyter kernelspec install $JUPYTER_KERNEL_DIR
 echo "c.MappingKernelManager.default_kernel_name = 'pyspark'" >> ~/.jupyter/jupyter_notebook_config.py
 
-pip install findspark
-echo "Jupyter setup...!"
+echo "Jupyter setup!"
 
