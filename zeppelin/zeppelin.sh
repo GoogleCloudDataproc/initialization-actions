3e165b67ac40c852b8572a643ed13268559bf51a
Patrick Clay <pclay@google.com>
2016-08-22 17:26:14 -0700

    Misc Zeppelin Improvements:
    * This fixes a issue with Zeppelin's PySpark configuration in Dataproc 1.0.
    * It also configures R and BigQuery in Dataproc 1.1.

--- zeppelin/zeppelin.sh
+++ zeppelin/zeppelin.sh
@@ -20,7 +20,61 @@ set -x -e
 
 # Only run on the master node
 ROLE="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
+INTERPRETER_FILE='/etc/zeppelin/conf/interpreter.json'
+
 if [[ "${ROLE}" == 'Master' ]]; then
   # Install zeppelin
   apt-get install -y zeppelin
+
+  for i in {1..6}; do
+    if [[ -r "${INTERPRETER_FILE}" ]]; then
+      break
+    else
+      sleep 10
+    fi
+  done
+  # Ideally we would use Zeppelin's REST API, but it is difficult, especially
+  # between versions. So sed the JSON file.
+  service zeppelin stop
+  # Set spark.yarn.isPython to fix Zeppelin pyspark in Dataproc 1.0.
+  sed -i 's/\(\s*\)"spark\.app\.name[^,}]*/&,\n\1"spark.yarn.isPython": "true"/' \
+      "${INTERPRETER_FILE}"
+
+  # Link in hive configuration.
+  if [[ ! -r  /etc/zeppelin/conf ]]; then
+    ln -s /etc/hive/conf/hive-site.xml /etc/zeppelin/conf
+  fi
+
+  # Install R libraries and configure BigQuery for Zeppelin 0.6.1+
+  ZEPPELIN_VERSION=$(dpkg-query --showformat='${Version}' --show zeppelin)
+  if dpkg --compare-versions "${ZEPPELIN_VERSION}" '>=' 0.6.1; then
+    # Set BigQuery project ID if present.
+    PROJECT=$(/usr/share/google/get_metadata_value ../project/project-id)
+    sed -i "s/\(\"zeppelin.bigquery.project_id\"\)[^,}]*/\1: \"${PROJECT}\"/" \
+        "${INTERPRETER_FILE}"
+
+    # TODO(pmkc): Add googlevis to Zeppelin Package recommendations
+    apt-get install -y r-cran-googlevis
+
+## Uncomment to compile and install 'mplot' and 'rCharts'.
+#    # Install compile dependencies
+#    apt-get install -y \
+#      r-cran-doparallel r-cran-httr r-cran-memoise r-cran-openssl \
+#      r-cran-rcurl r-cran-plyr r-cran-shiny r-cran-glmnet r-cran-data.table \
+#      libssl-dev libcurl4-openssl-dev
+#
+#    cat << EOF > install_script.r
+#options('Ncpus'=10, repos = 'http://cran.us.r-project.org')
+#install.packages('devtools')
+#require('devtools')
+## Install version 0.7.7 to avoid dependency on glmulti and RJava
+#install_version('mplot', version = '0.7.7', upgrade_dependencies = FALSE)
+#install_github('ramnathv/rCharts', upgrade_dependencies = FALSE)
+#update.packages('ggplot', ask = FALSE)
+#EOF
+#    R -f install_script.r
+  fi
+
+  # Restart Zeppelin
+  service zeppelin start
 fi
