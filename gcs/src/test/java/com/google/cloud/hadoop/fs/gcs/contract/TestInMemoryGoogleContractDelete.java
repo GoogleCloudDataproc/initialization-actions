30748d30a70255a49a8f43aea412b6be0891dfd3
dhuo <dhuo@google.com>
2016-10-04 13:02:03 -0700

    Add ContractDelete tests; these worked out of the box.
    	Change on 2016/10/04 by dhuo <dhuo@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=135141973

---
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/contract/TestInMemoryGoogleContractDelete.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.hadoop.fs.gcs.contract;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.contract.AbstractContractDeleteTest;
+import org.apache.hadoop.fs.contract.AbstractFSContract;
+
+/**
+ * GCS contract tests covering file delete using in-memory fakes.
+ */
+public class TestInMemoryGoogleContractDelete extends AbstractContractDeleteTest {
+
+  @Override
+  protected AbstractFSContract createContract(Configuration conf) {
+    return new InMemoryGoogleContract(conf);
+  }
+}
