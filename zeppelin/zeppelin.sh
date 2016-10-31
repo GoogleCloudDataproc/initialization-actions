484b5743118f6b4aba6ee4f57d05f002c5020117
Igor Postanogov <ipostanogov@outlook.com>
2016-10-31 22:48:17 +0600

    Fix typo in zeppelin conf file name (#78)

--- zeppelin/zeppelin.sh
+++ zeppelin/zeppelin.sh
@@ -47,7 +47,7 @@ if [[ "${ROLE}" == 'Master' ]]; then
 
   if [[ -n "${ZEPPELIN_PORT}" ]]; then
     echo "export ZEPPELIN_PORT=${ZEPPELIN_PORT}" \
-        >> /etc/zeppelin/conf/zeppeline-env.sh
+        >> /etc/zeppelin/conf/zeppelin-env.sh
   fi
 
   # Install R libraries and configure BigQuery for Zeppelin 0.6.1+
