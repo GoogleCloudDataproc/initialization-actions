8ab314d281ced8051fae21f1ba26e18bc61f7bc5
Tarık Yılmaz <tarik.yilmaz@metglobal.com>
2017-03-09 17:35:31 +0300

    Update missing dependencies

--- oozie/oozie.sh
+++ oozie/oozie.sh
@@ -15,7 +15,7 @@
 # Initialization action for installing Apache Oozie on a Google Cloud
 # Dataproc cluster. This script will install and configure Oozie to run on the
 # master node of a Dataproc cluster. The version of Oozie which is installed
-# comes from the BigTop repository.
+# comes from the BigTop repository. 
 #
 # You can find more information about Oozie at http://oozie.apache.org/
 # For more information in init actions and Google Cloud Dataproc see the Cloud
@@ -32,7 +32,6 @@ if [[ "${ROLE}" == 'Master' ]]; then
   # Upgrade the repository and install Tomcat
   apt-get update
   apt-get install bigtop-tomcat -y
-  apt-get install libssl-dev -y
 
   # install Oozie from remote repository url
   wget http://dataproc-bigtop-repo.storage.googleapis.com/v1.0-RC0/contrib/o/oozie/oozie_4.2.0-1_all.deb
@@ -72,16 +71,18 @@ EOF
   sed -i '/<\/configuration>/e cat core-site-patch.xml' \
       /etc/hadoop/conf/core-site.xml
 
-  # Clean up temporary fles
+  # Clean up temporary files
   rm -rf ext-2.2 ext-2.2.zip core-site-patch.xml
 
   # Required for Oozie sharedlib setup
-  # throws ClassNotFoundException about org.apache.commons.io.Charsets
   wget http://central.maven.org/maven2/commons-io/commons-io/2.3/commons-io-2.3.jar
   wget http://central.maven.org/maven2/org/apache/htrace/htrace-core4/4.2.0-incubating/htrace-core4-4.2.0-incubating.jar
+  wget http://central.maven.org/maven2/jline/jline/2.12/jline-2.12.jar
+  wget http://central.maven.org/maven2/org/apache/hive/hive-common/1.2.1/hive-common-1.2.1.jar
 
-  # Remove HTrace 3.1.0
+  # Remove HTrace 3.1.0 and Hadoop Auth 2.7.1
   rm /usr/lib/oozie/lib/htrace-core-3.1.0-incubating.jar
+  rm /usr/lib/oozie/lib/hadoop-auth-2.7.1.jar
 
   # Copy Commons-IO and new HTrace
   mv commons-io-2.3.jar /usr/lib/oozie/lib/
@@ -104,5 +105,17 @@ EOF
 
   # Update available sharedlib
   oozie admin -sharelibupdate -oozie http://${HOSTNAME}:11000/oozie
-fi
 
+  # Figure out sharelib directory on HDFS
+  SHARELIB_DIR=$(hdfs dfs -ls hdfs:///user/oozie/share/lib/|tail -n 1|awk '{print $8}')
+
+  # Remove the old jline dependency
+  hdfs dfs -rm ${SHARELIB_DIR}/hive2/jline-0.9.94.jar
+
+  # Download and install missing dependencies
+  hdfs dfs -put jline-2.12.jar ${SHARELIB_DIR}/hive2/
+  hdfs dfs -put hive-common-1.2.1.jar ${SHARELIB_DIR}/hive2/
+
+  # Update available sharedlib one more time
+  oozie admin -sharelibupdate -oozie http://${HOSTNAME}:11000/oozie
+fi
