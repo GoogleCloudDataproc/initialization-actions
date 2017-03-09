7050011149235d6788a59a1f24c49cbe1643311d
Vince Gonzalez <vince.gonzalez@gmail.com>
2017-03-09 09:17:05 -0500

    add gcs plugin

--- drill/drill.sh
+++ drill/drill.sh
@@ -13,6 +13,7 @@ DRILL_VERSION="1.9.0"
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 # Determine the cluster name
 CLUSTER=$(/usr/share/google/get_metadata_value attributes/dataproc-cluster-name)
+DATAPROC_BUCKET=gs://$(/usr/share/google/get_metadata_value attributes/dataproc-bucket)
 
 # naively generate the zookeeper string
 ZK=$CLUSTER-m:2181,$CLUSTER-w-0:2181,$CLUSTER-w-1:2181
@@ -39,6 +40,12 @@ mkdir -p /etc/drill && ln -sf $DRILL_HOME/conf /etc/drill/
 # Point drill logs to $DRILL_LOG_DIR
 echo DRILL_LOG_DIR=$DRILL_LOG_DIR >> $DRILL_HOME/conf/drill-env.sh
 
+# Copy GCS connector to drill jars
+ln -sf /usr/lib/hadoop/lib/gcs-connector-1.6.0-hadoop2.jar $DRILL_HOME/jars/3rdparty
+
+# Symlink core-site.xml to $DRILL_HOME/conf
+ln -sf /etc/hadoop/conf/core-site.xml /etc/drill/conf
+
 # Start drillbit
 sudo -u $DRILL_USER $DRILL_HOME/bin/drillbit.sh status ||\
 	sudo -u $DRILL_USER $DRILL_HOME/bin/drillbit.sh start && sleep 10
@@ -59,9 +66,83 @@ cat > /tmp/hive_plugin.json <<EOF
 }
 EOF
 
+# Create GCS storage plugin
+cat > /tmp/gcs_plugin.json <<EOF
+{
+    "config": {
+        "connection": "$DATAPROC_BUCKET",
+        "enabled": true,
+        "formats": {
+            "avro": {
+                "type": "avro"
+            },
+            "csv": {
+                "delimiter": ",",
+                "extensions": [
+                    "csv"
+                ],
+                "type": "text"
+            },
+            "csvh": {
+                "delimiter": ",",
+                "extensions": [
+                    "csvh"
+                ],
+                "extractHeader": true,
+                "type": "text"
+            },
+            "json": {
+                "extensions": [
+                    "json"
+                ],
+                "type": "json"
+            },
+            "parquet": {
+                "type": "parquet"
+            },
+            "psv": {
+                "delimiter": "|",
+                "extensions": [
+                    "tbl"
+                ],
+                "type": "text"
+            },
+            "sequencefile": {
+                "extensions": [
+                    "seq"
+                ],
+                "type": "sequencefile"
+            },
+            "tsv": {
+                "delimiter": "\t",
+                "extensions": [
+                    "tsv"
+                ],
+                "type": "text"
+            }
+        },
+        "type": "file",
+        "workspaces": {
+            "root": {
+                "defaultInputFormat": null,
+                "location": "/",
+                "writable": false
+            },
+            "tmp": {
+                "defaultInputFormat": null,
+                "location": "/tmp",
+                "writable": true
+            }
+        }
+    },
+    "name": "gs"
+}
+EOF
+
 curl -d@/tmp/hive_plugin.json -H "Content-Type: application/json" -X POST http://localhost:8047/storage/hive.json
+curl -d@/tmp/gcs_plugin.json -H "Content-Type: application/json" -X POST http://localhost:8047/storage/gs.json
 
 # Clean up
-rm -f /tmp/drill_plugin.json
+rm -f /tmp/*_plugin.json
 
 set +x +e
