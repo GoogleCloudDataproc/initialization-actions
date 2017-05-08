af732d959801ad63b9cd35b7e8b37e8701a6d7a8
Sam McVeety <sgmc@google.com>
2017-05-08 13:53:13 -0700

    Autostart Jupyter

--- jupyter/internal/launch-jupyter-kernel.sh
+++ jupyter/internal/launch-jupyter-kernel.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Starting Jupyter notebook..."
-nohup jupyter notebook --allow-root --no-browser > /var/log/jupyter_notebook.log 2>&1 &
+echo "Installing Jupyter service..."
 
+INIT_SCRIPT="/usr/lib/systemd/system/jupyter-notebook.service"
+
+cat << EOF > ${INIT_SCRIPT}
+[Unit]
+Description=Start Jupyter Notebook Server at reboot
+
+[Service]
+Type=simple
+ExecStart=jupyter notebook --allow-root  --no-browser > /var/log/jupyter_notebook.log 2>&1
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod a+rw ${INIT_SCRIPT}
+
+echo "Starting Jupyter notebook..."
+
+systemctl daemon-reload
+systemctl enable jupyter-notebook
+systemctl start jupyter-notebook
+
+echo "Jupyter installation succeeded" >&2
