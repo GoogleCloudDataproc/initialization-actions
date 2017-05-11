d592d80b033a6748201786049306335b4fed7c10
Dennis Huo <huoisme@gmail.com>
2017-05-10 21:16:32 -0700

    Fix jupyter systemd redirect and conda env (#113)
    * Fix jupyter systemd stdout/stderr redirect.
    
    The piping is a shell command, but systemd runs ExecStart shell-less,
    so the redirect must be embedded in a /bin/bash exec command.
    
    * Add launch_jupyter.sh helper to source conda.sh.
    
    Default environment variables running jupyter will not pick up the
    configured conda environment, and actual PySpark jobs will see an
    error like "Python in worker has different version 2.7 than that
    in driver 3.5, PySpark cannot run with different minor versions".
    Since it's tricky to otherwise get the conda environment inline,
    it's now a good reason to use the helper script as the ExecStart
    command rather than fitting it inline in the .service file. Also,
    add a "systemctl status" check at the end since otherwise a failed
    exec won't propagate to the init action's error status.

--- jupyter/internal/launch-jupyter-kernel.sh
+++ jupyter/internal/launch-jupyter-kernel.sh
@@ -3,16 +3,28 @@ set -e
 
 echo "Installing Jupyter service..."
 
+# Create a separate runner file to make it easier to pull in the right
+# environment variables, etc,. before launching the notebook.
+JUPYTER_LAUNCHER='/usr/local/bin/launch_jupyter.sh'
+
+cat << EOF > ${JUPYTER_LAUNCHER}
+#!/bin/bash
+
+source /etc/profile.d/conda.sh
+/opt/conda/bin/jupyter notebook --allow-root --no-browser
+EOF
+chmod 750 ${JUPYTER_LAUNCHER}
+
 INIT_SCRIPT="/usr/lib/systemd/system/jupyter-notebook.service"
 
 cat << EOF > ${INIT_SCRIPT}
 [Unit]
-Description=Start Jupyter Notebook Server at reboot
+Description=Jupyter Notebook Server
 
 [Service]
 Type=simple
-ExecStart=/opt/conda/bin/jupyter notebook --allow-root  --no-browser \
-  > /var/log/jupyter_notebook.log 2>&1
+ExecStart=/bin/bash -c 'exec ${JUPYTER_LAUNCHER} \
+    &> /var/log/jupyter_notebook.log'
 
 [Install]
 WantedBy=multi-user.target
@@ -24,6 +36,7 @@ echo "Starting Jupyter notebook..."
 
 systemctl daemon-reload
 systemctl enable jupyter-notebook
-systemctl start jupyter-notebook
+systemctl restart jupyter-notebook
+systemctl status jupyter-notebook
 
 echo "Jupyter installation succeeded" >&2
