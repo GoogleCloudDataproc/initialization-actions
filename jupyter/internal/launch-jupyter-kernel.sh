0188eed4182da5aaf271a5096d67ae14d8d3b98a
Dennis Huo <dhuo@google.com>
2017-05-10 21:07:28 -0700

    Add launch_jupyter.sh helper to source conda.sh.
    Default environment variables running jupyter will not pick up the
    configured conda environment, and actual PySpark jobs will see an
    error like "Python in worker has different version 2.7 than that
    in driver 3.5, PySpark cannot run with different minor versions".
    Since it's tricky to otherwise get the conda environment inline,
    it's now a good reason to use the helper script as the ExecStart
    command rather than fitting it inline in the .service file. Also,
    add a "systemctl status" check at the end since otherwise a failed
    exec won't propagate to the init action's error status.

--- jupyter/internal/launch-jupyter-kernel.sh
+++ jupyter/internal/launch-jupyter-kernel.sh
@@ -3,6 +3,18 @@ set -e
 
 echo "Installing Jupyter service..."
 
+# Create a separate runner file to make it easier to pull in the right
+# environment variables, etc,. before launching the notebook.
+JUPYTER_LAUNCHER='/usr/local/bin/launch_jupyter.sh'
+
+cat << EOF > ${JUPYTER_LAUNCHER}
+#!/bin/bash
+
+source /etc/profile.d/conda.sh
+/opt/conda/bin/jupyter notebook --allow-root --no-browser
+EOF
+chmod 750 ${JUPYTER_LAUNCHER}
+
 INIT_SCRIPT="/usr/lib/systemd/system/jupyter-notebook.service"
 
 cat << EOF > ${INIT_SCRIPT}
@@ -11,8 +23,8 @@ Description=Jupyter Notebook Server
 
 [Service]
 Type=simple
-ExecStart=/bin/bash -c 'exec /opt/conda/bin/jupyter notebook --allow-root  --no-browser \
-  &> /var/log/jupyter_notebook.log'
+ExecStart=/bin/bash -c 'exec ${JUPYTER_LAUNCHER} \
+    &> /var/log/jupyter_notebook.log'
 
 [Install]
 WantedBy=multi-user.target
@@ -24,6 +36,7 @@ echo "Starting Jupyter notebook..."
 
 systemctl daemon-reload
 systemctl enable jupyter-notebook
-systemctl start jupyter-notebook
+systemctl restart jupyter-notebook
+systemctl status jupyter-notebook
 
 echo "Jupyter installation succeeded" >&2
