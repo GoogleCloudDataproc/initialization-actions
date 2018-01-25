504690ccf61207e926d8c14ae2c3bddd11662f16
karth295 <karth295@uw.edu>
2018-01-25 15:13:32 -0800

    Only store Jupyter notebooks in GCS. (#156)

--- jupyter/internal/setup-jupyter-kernel.sh
+++ jupyter/internal/setup-jupyter-kernel.sh
@@ -6,7 +6,7 @@ DIR="${BASH_SOURCE%/*}"
 source "$DIR/../../util/utils.sh"
 
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
-JUPYTER_NOTEBOOK_DIR="/root/notebooks"
+DATAPROC_BUCKET=$(curl -f -s -H Metadata-Flavor:Google http://metadata/computeMetadata/v1/instance/attributes/dataproc-bucket)
 JUPYTER_PORT=$(/usr/share/google/get_metadata_value attributes/JUPYTER_PORT || true)
 [[ ! $JUPYTER_PORT =~ ^[0-9]+$ ]] && JUPYTER_PORT=8123
 JUPYTER_AUTH_TOKEN=$(/usr/share/google/get_metadata_value attributes/JUPYTER_AUTH_TOKEN || true)
@@ -14,8 +14,9 @@ JUPYTER_IP=*
 
 [[ "${ROLE}" != 'Master' ]] && throw "$0 should only be run on the Master node!"
 
-echo "Creating notebook directory $JUPYTER_NOTEBOOK_DIR if necessary..."
-[[ ! -d $JUPYTER_NOTEBOOK_DIR ]] && mkdir -p $JUPYTER_NOTEBOOK_DIR
+# Must not include gs:// and must exist
+NOTEBOOK_DIR="$DATAPROC_BUCKET/notebooks"
+hadoop fs -mkdir -p "gs://$NOTEBOOK_DIR"
 
 echo "Creating Jupyter config..."
 jupyter notebook --allow-root --generate-config -y --ip=127.0.0.1
@@ -23,7 +24,8 @@ echo "c.Application.log_level = 'DEBUG'" >> ~/.jupyter/jupyter_notebook_config.p
 echo "c.NotebookApp.ip = '*'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.open_browser = False" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.port = $JUPYTER_PORT" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.notebook_dir = '$JUPYTER_NOTEBOOK_DIR'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.contents_manager_class = 'jgscm.GoogleStorageContentManager'" >> ~/.jupyter/jupyter_notebook_config.py
+echo "c.GoogleStorageContentManager.default_path = '$NOTEBOOK_DIR'" >> ~/.jupyter/jupyter_notebook_config.py
 echo "c.NotebookApp.token = u'$JUPYTER_AUTH_TOKEN'" >> ~/.jupyter/jupyter_notebook_config.py
 
 echo "Installing pyspark Kernel..."
