742a99c5d85c27ebf8ab20c6ac311c25cd196ff4
Kamil Szewczyk <Kamil.Szewczyk@polidea.com>
2018-02-23 22:24:21 +0100

    Applying review suggestions #2

--- kafka/kafka.sh
+++ kafka/kafka.sh
@@ -37,25 +37,28 @@ function err() {
 
 function install_and_configure_kafka_server() {
   # Find zookeeper list first, before attempting any installation.
-  local ZOOKEEPER_CLIENT_PORT=$(grep 'clientPort' /etc/zookeeper/conf/zoo.cfg \
+  local zookeeper_client_port
+  zookeeper_client_port=$(grep 'clientPort' /etc/zookeeper/conf/zoo.cfg \
     | cut -d '=' -f 2)
-  local ZOOKEEPER_LIST=$(grep '^server\.' /etc/zookeeper/conf/zoo.cfg \
+  
+  local zookeeper_list
+  zookeeper_list=$(grep '^server\.' /etc/zookeeper/conf/zoo.cfg \
     | cut -d '=' -f 2 \
     | cut -d ':' -f 1 \
-    | sed "s/$/:${ZOOKEEPER_CLIENT_PORT}/" \
+    | sed "s/$/:${zookeeper_client_port}/" \
     | xargs echo  \
     | sed "s/ /,/g")
 
-  if [[ -z "${ZOOKEEPER_LIST}" ]]; then
+  if [[ -z "${zookeeper_list}" ]]; then
     # Didn't find zookeeper quorum in zoo.cfg, but possibly workers just didn't
     # bother to populate it. Check if YARN HA is configured.
-    ZOOKEEPER_LIST=$(bdconfig get_property_value --configuration_file \
+    zookeeper_list=$(bdconfig get_property_value --configuration_file \
       /etc/hadoop/conf/yarn-site.xml \
       --name yarn.resourcemanager.zk-address 2>/dev/null)
   fi
 
   # If all attempts failed, error out.
-  if [[ -z "${ZOOKEEPER_LIST}" ]]; then
+  if [[ -z "${zookeeper_list}" ]]; then
     err 'Failed to find configured Zookeeper list; try --num-masters=3 for HA'
   fi
 
@@ -67,13 +70,14 @@ function install_and_configure_kafka_server() {
   chown kafka:kafka -R /var/lib/kafka-logs
 
   # Note: If modified to also run brokers on master nodes, this logic for
-  # generating BROKER_ID will need to be changed.
-  BROKER_ID=$(hostname | sed 's/.*-w-\([0-9]\)*.*/\1/g')
+  # generating broker_id will need to be changed.
+  local broker_id
+  broker_id=$(hostname | sed 's/.*-w-\([0-9]\)*.*/\1/g')
   sed -i 's|log.dirs=/tmp/kafka-logs|log.dirs=/var/lib/kafka-logs|' \
     "${KAFKA_PROP_FILE}"
-  sed -i 's|^\(zookeeper\.connect=\).*|\1'${ZOOKEEPER_LIST}'|' \
+  sed -i 's|^\(zookeeper\.connect=\).*|\1'${zookeeper_list}'|' \
     "${KAFKA_PROP_FILE}"
-  sed -i 's,^\(broker\.id=\).*,\1'${BROKER_ID}',' \
+  sed -i 's,^\(broker\.id=\).*,\1'${broker_id}',' \
     "${KAFKA_PROP_FILE}"
   echo 'delete.topic.enable = true' >> "${KAFKA_PROP_FILE}"
 
@@ -82,11 +86,12 @@ function install_and_configure_kafka_server() {
 }
 
 function main() {
-  local ROLE="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
+  local role
+  role="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
   update_apt_get || err 'Unable to update packages lists.'
 
   # Only run the installation on workers; verify zookeeper on master(s).
-  if [[ "${ROLE}" == 'Master' ]]; then
+  if [[ "${role}" == 'Master' ]]; then
     service zookeeper-server status \
       || err 'Required zookeeper-server not running on master!'
     # On master nodes, just install kafka libs but not kafka-server.
