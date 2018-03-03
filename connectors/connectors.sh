8ecaa1a835373ece765401cb74056868959f4cd8
Igor Dvorzhak <idv@google.com>
2018-03-03 10:14:58 -0800

    Connectors versions parameterization
    Allows to specify connector versions through metadata.

--- connectors/connectors.sh
+++ connectors/connectors.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-# Determine new connector versions:
+VM_CONNECTORS_DIR=/usr/lib/hadoop/lib
+
+# Returns latest connector version:
 # 1. List all GSC/BigQuery connector jars.
 # 2. Filter (grep) connector binaries, because javadoc/src jars are also listed.
 # 3. Extract (cut) version number from connector paths.
 # 4. Sort connector version numbers numerically.
 # 5. Use last sorted version number as latest connector version number.
-LATEST_BIGQUERY_CONNECTOR_VERSION=$(
-  gsutil ls gs://hadoop-lib/bigquery/bigquery-connector-*-hadoop2.jar | \
-  grep -E "/bigquery-connector-([0-9]+\.)+[0-9]+-hadoop2\.jar" | \
-  cut -d'-' -f4 | \
-  sort -t'.' -n -k1,1 -k2,2 -k3,3 | \
-  tail -n1
-)
-LATEST_GCS_CONNECTOR_VERSION=$(
-  gsutil ls gs://hadoop-lib/gcs/gcs-connector-*-hadoop2.jar | \
-  grep -E "/gcs-connector-([0-9]+\.)+[0-9]+-hadoop2\.jar" | \
-  cut -d'-' -f4 | \
-  sort -t'.' -n -k1,1 -k2,2 -k3,3 | \
-  tail -n1
-)
+get_latest_connector_version() {
+  local name=$1 # connector name: "bigquery" or "gcs"
+  local path_prefix=gs://hadoop-lib/${name}/${name}-connector
+  gsutil ls "${path_prefix}-*-hadoop2.jar" | \
+    grep -E "${path_prefix}-([0-9]+\.)+[0-9]+-hadoop2\.jar" | \
+    cut -d'-' -f4 | \
+    sort -t'.' -n -k1,1 -k2,2 -k3,3 | \
+    tail -n1
+}
 
-LATEST_BIGQUERY_CONNECTOR_PATH=gs://hadoop-lib/bigquery/bigquery-connector-${LATEST_BIGQUERY_CONNECTOR_VERSION}-hadoop2.jar
-LATEST_GCS_CONNECTOR_PATH=gs://hadoop-lib/gcs/gcs-connector-${LATEST_GCS_CONNECTOR_VERSION}-hadoop2.jar
+get_connector_version() {
+  local name=$1 # connector name: "bigquery" or "gcs"
+  local metadata_key=attributes/${name}-connector-version
+  /usr/share/google/get_metadata_value "${metadata_key}" || \
+    get_latest_connector_version "${name}"
+}
 
-VM_CONNECTORS_DIR=/usr/lib/hadoop/lib
+upload_connector() {
+  local name=$1 # connector name: "bigquery" or "gcs"
+  local version=$(get_connector_version "$name")
+  local path=gs://hadoop-lib/${name}/${name}-connector-${version}-hadoop2.jar
+  gsutil cp "$path" ${VM_CONNECTORS_DIR}/
+}
 
-# Update BigQuery connector
-rm -f ${VM_CONNECTORS_DIR}/bigquery-connector-*
-gsutil cp "$LATEST_BIGQUERY_CONNECTOR_PATH" ${VM_CONNECTORS_DIR}/
+rm -f ${VM_CONNECTORS_DIR}/{gcs,bigquery}-connector-*
 
-# Update GCS connector
-rm -f ${VM_CONNECTORS_DIR}/gcs-connector-*
-gsutil cp "$LATEST_GCS_CONNECTOR_PATH" ${VM_CONNECTORS_DIR}/
+upload_connector "bigquery"
+upload_connector "gcs"
