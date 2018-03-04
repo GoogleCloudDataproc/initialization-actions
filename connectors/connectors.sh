068ea8d5edb0f0298a1c53db828a51eb02b2fccb
Igor Dvorzhak <idv@google.com>
2018-03-03 23:25:26 -0800

    Improve connectors versions validation.

--- connectors/connectors.sh
+++ connectors/connectors.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-declare -A MIN_CONNECTOR_VERSIONS
-MIN_CONNECTOR_VERSIONS=(
-  ["bigquery"]="0.10.1"
-  ["gcs"]="1.6.0")
+set -euxo pipefail
+
+VM_CONNECTORS_DIR=/usr/lib/hadoop/lib
 
 declare -A GCS_TO_BQ_VERSIONS_MAP
 GCS_TO_BQ_VERSIONS_MAP=(
-  ["1.6.0"]="0.10.1"
-  ["1.6.1"]="0.10.2"
-  ["1.6.2"]="0.10.3"
-  ["1.6.3"]="0.10.4"
   ["1.7.0"]="0.11.0")
 
-min_version() {
-  echo -e "$1\n$2" | sort -r -t'.' -n -k1,1 -k2,2 -k3,3 | tail -n1
-}
+BIGQUERY_CONNECTOR_VERSION=$(/usr/share/google/get_metadata_value attributes/bigquery-connector-version || true)
+GCS_CONNECTOR_VERSION=$(/usr/share/google/get_metadata_value attributes/gcs-connector-version || true)
 
 validate_version() {
   local name=$1    # connector name: "bigquery" or "gcs"
   local version=$2 # connector version
-  local min_valid_version=${MIN_CONNECTOR_VERSIONS[$name]}
-  if [[ "$version" ]] && [[ "$(min_version "$min_valid_version" "$version")" != "$min_valid_version" ]]; then
-    echo "ERROR: $name-connector version should be greater than or equal to $min_valid_version, but was $version"
-    exit 1
+  case "$name" in
+   bigquery) local -a valid_versions=("${GCS_TO_BQ_VERSIONS_MAP[@]}") ;;
+   gcs) local -a valid_versions=("${!GCS_TO_BQ_VERSIONS_MAP[@]}") ;;
+   *) echo "Error: invalid connector name - '$name'"; return 1;;
+  esac
+  if [[ $version ]] && [[ ! "${valid_versions[@]}" =~ "$version" ]]; then
+    local IFS=, # print versions concatenated by comma
+    echo "ERROR: $name-connector version should be one of [${valid_versions[*]}], but was '$version'"
+    return 1
   fi
 }
 
@@ -36,14 +35,9 @@ upload_connector() {
   fi
 }
 
-VM_CONNECTORS_DIR=/usr/lib/hadoop/lib
-
-BIGQUERY_CONNECTOR_VERSION=$(/usr/share/google/get_metadata_value attributes/bigquery-connector-version)
-GCS_CONNECTOR_VERSION=$(/usr/share/google/get_metadata_value attributes/gcs-connector-version)
-
 if [[ -z $BIGQUERY_CONNECTOR_VERSION ]] && [[ -z $GCS_CONNECTOR_VERSION ]]; then
   echo "ERROR: None of connector versions are specified"
-  exi 1
+  exit 1
 fi
 
 validate_version "bigquery" "$BIGQUERY_CONNECTOR_VERSION"
