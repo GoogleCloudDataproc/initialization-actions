ef0bf80c8aac9521269744263ae51b7d3b038f49
Igor Dvorzhak <idv@google.com>
2018-03-06 16:33:24 -0800

    Require only GCS connector version
    Init action will updates BigQuery connector based on specified GCS connector version, if BigQuery connector already exists.

--- connectors/connectors.sh
+++ connectors/connectors.sh
@@ -8,20 +8,14 @@ declare -A GCS_TO_BQ_VERSIONS_MAP
 GCS_TO_BQ_VERSIONS_MAP=(
   ["1.7.0"]="0.11.0")
 
-BIGQUERY_CONNECTOR_VERSION=$(/usr/share/google/get_metadata_value attributes/bigquery-connector-version || true)
 GCS_CONNECTOR_VERSION=$(/usr/share/google/get_metadata_value attributes/gcs-connector-version || true)
 
-validate_version() {
-  local name=$1    # connector name: "bigquery" or "gcs"
-  local version=$2 # connector version
-  case "$name" in
-   bigquery) local -a valid_versions=("${GCS_TO_BQ_VERSIONS_MAP[@]}") ;;
-   gcs) local -a valid_versions=("${!GCS_TO_BQ_VERSIONS_MAP[@]}") ;;
-   *) echo "Error: invalid connector name - '$name'"; return 1;;
-  esac
+validate_gcs_connector_version() {
+  local version=$1 # connector version
+  local -a valid_versions=("${!GCS_TO_BQ_VERSIONS_MAP[@]}")
   if [[ $version ]] && [[ ! "${valid_versions[@]}" =~ "$version" ]]; then
     local IFS=, # print versions concatenated by comma
-    echo "ERROR: $name-connector version should be one of [${valid_versions[*]}], but was '$version'"
+    echo "ERROR: gcs-connector version should be one of [${valid_versions[*]}], but was '$version'"
     return 1
   fi
 }
@@ -35,21 +29,20 @@ download_connector() {
   fi
 }
 
-if [[ -z $BIGQUERY_CONNECTOR_VERSION ]] && [[ -z $GCS_CONNECTOR_VERSION ]]; then
-  echo "ERROR: None of connector versions are specified"
+if [[ -z $GCS_CONNECTOR_VERSION ]]; then
+  echo "ERROR: GCS connector versions not specified"
   exit 1
 fi
 
-validate_version "bigquery" "$BIGQUERY_CONNECTOR_VERSION"
-validate_version "gcs" "$GCS_CONNECTOR_VERSION"
+validate_gcs_connector_version "$GCS_CONNECTOR_VERSION"
 
-if [[ "$BIGQUERY_CONNECTOR_VERSION" ]] && [[ "$GCS_CONNECTOR_VERSION" ]] \
-    && [[ "${GCS_TO_BQ_VERSIONS_MAP[$GCS_CONNECTOR_VERSION]}" != "$BIGQUERY_CONNECTOR_VERSION" ]]; then
-  echo "ERROR: bigquery-connector and gcs-connector versions should be from the same release"
-  exit 1
+BIGQUERY_CONNECTOR_VERSION=${GCS_TO_BQ_VERSIONS_MAP[$GCS_CONNECTOR_VERSION]}
+
+# Update BigQuery connector only if it already exists
+if compgen -G "${VM_CONNECTORS_DIR}/bigquery-connector-*" > /dev/null; then
+  rm -f ${VM_CONNECTORS_DIR}/bigquery-connector-*
+  download_connector "bigquery" "$BIGQUERY_CONNECTOR_VERSION"
 fi
 
-rm -f ${VM_CONNECTORS_DIR}/{gcs,bigquery}-connector-*
-
-download_connector "bigquery" "$BIGQUERY_CONNECTOR_VERSION"
+rm -f ${VM_CONNECTORS_DIR}/gcs-connector-*
 download_connector "gcs" "$GCS_CONNECTOR_VERSION"
