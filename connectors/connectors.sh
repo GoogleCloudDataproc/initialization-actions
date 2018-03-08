cfafe516bc7155bbdc151e4bf2faa7c9ab450127
Igor Dvorzhak <idv@google.com>
2018-03-08 10:55:16 -0800

    Validate connector version inside update function

--- connectors/connectors.sh
+++ connectors/connectors.sh
@@ -20,7 +20,7 @@ validate_version() {
   local name=$1    # connector name: "bigquery" or "gcs"
   local version=$2 # connector version
   local min_valid_version=${MIN_CONNECTOR_VERSIONS[$name]}
-  if [[ $version ]] && [[ "$(min_version "$min_valid_version" "$version")" != "$min_valid_version" ]]; then
+  if [[ "$(min_version "$min_valid_version" "$version")" != "$min_valid_version" ]]; then
     echo "ERROR: $name-connector version should be greater than or equal to $min_valid_version, but was $version"
     return 1
   fi
@@ -30,7 +30,13 @@ update_connector() {
   local name=$1    # connector name: "bigquery" or "gcs"
   local version=$2 # connector version
   if [[ $version ]]; then
+    # validate new connector version
+    validate_version "${name}" "$BIGQUERY_CONNECTOR_VERSION"
+
+    # remove old connector
     rm -f "${VM_CONNECTORS_DIR}/${name}-connector-*"
+
+    # download new connector
     local path=gs://hadoop-lib/${name}/${name}-connector-${version}-hadoop2.jar
     gsutil cp "$path" "${VM_CONNECTORS_DIR}/"
   fi
@@ -40,9 +46,6 @@ if [[ -z $BIGQUERY_CONNECTOR_VERSION ]] && [[ -z $GCS_CONNECTOR_VERSION ]]; then
   echo "ERROR: None of connector versions are specified"
   exit 1
 fi
-
-validate_version "bigquery" "$BIGQUERY_CONNECTOR_VERSION"
-validate_version "gcs" "$GCS_CONNECTOR_VERSION"
 
 update_connector "bigquery" "$BIGQUERY_CONNECTOR_VERSION"
 update_connector "gcs" "$GCS_CONNECTOR_VERSION"
