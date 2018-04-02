9034adf2474e89ae29c259eed85c027339515e5a
Karthik Palaniappan <karthikpal@google.com>
2018-04-02 00:00:32 -0700

    Make a pyspark kernel

--- jupyter2/jupyter2.sh
+++ jupyter2/jupyter2.sh
@@ -2,14 +2,21 @@
 
 set -euxo pipefail
 
-readonly ROLE="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
+readonly DATAPROC_MASTER="$(/usr/share/google/get_metadata_value attributes/dataproc-master)"
 readonly DATAPROC_BUCKET="$(/usr/share/google/get_metadata_value attributes/dataproc-bucket)"
 readonly JUPYTER_AUTH_TOKEN="$(/usr/share/google/get_metadata_value attributes/JUPYTER_AUTH_TOKEN || true)"
 readonly JUPYTER_PORT="$(/usr/share/google/get_metadata_value attributes/JUPYTER_PORT || echo 8123)"
 
+# Place pyspark kernel.json file in /usr/local/share/jupyter (system-wide dir).
+# Reference: https://ipython.readthedocs.io/en/latest/install/kernel_install.html
+readonly KERNELSPEC_FILE=/usr/local/share/jupyter/kernels/pyspark/kernel.json
+
 # GCS directory in which to store notebooks.
 # DATAPROC_BUCKET must not include gs:// and bucket must exist
 readonly NOTEBOOK_DIR="${DATAPROC_BUCKET}/notebooks"
+
+# Systemd init script for jupyter
+readonly INIT_SCRIPT="/usr/lib/systemd/system/jupyter.service"
 
 function update_apt_get() {
   for ((i = 0; i < 10; i++)); do
@@ -29,7 +36,7 @@ function err() {
 function install_jupyter() {
   apt-get install -y python-dev python-tk
   easy_install pip
-  pip install --upgrade jupyter jgscm matplotlib
+  pip install --upgrade jupyter jgscm==0.1.7 matplotlib
 }
 
 function configure_jupyter() {
@@ -53,27 +60,41 @@ c.GoogleStorageContentManager.default_path = '${NOTEBOOK_DIR}'
 c.NotebookApp.token = u'${JUPYTER_AUTH_TOKEN}'
 EOF
 
-  echo "Jupyter setup!"
+  echo "Creating kernelspec"
+  mkdir -p "$(dirname ${KERNELSPEC_FILE})"
+  # {connection_file} is a magic variable that Jupyter fills in for us
+  # Note: we can only use it in argv, so cannot use env to set those
+  # environment variables.
+  cat << EOF > "${KERNELSPEC_FILE}"
+{
+ "argv": [
+    "bash",
+    "-c",
+    "PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS='kernel -f {connection_file}' pyspark"],
+ "display_name": "PySpark",
+ "language": "python"
+}
+EOF
+  # Ensure Jupyter has picked up the new kernel
+  jupyter kernelspec list | grep pyspark || err "Failed to create kernelspec"
 
-  local init_script="/usr/lib/systemd/system/jupyter.service"
-
-  cat << EOF > ${init_script}
+  cat << EOF > "${INIT_SCRIPT}"
 [Unit]
-Description=PySpark-based Jupyter Notebook Server
+Description=Jupyter Notebook Server
 After=hadoop-yarn-resourcemanager.service
 
 [Service]
 Type=simple
-Environment=PYSPARK_DRIVER_PYTHON=jupyter
-Environment=PYSPARK_DRIVER_PYTHON_OPTS=notebook
-ExecStart=/bin/bash -c '/usr/bin/pyspark &> /var/log/jupyter_notebook.log'
+ExecStart=/bin/bash -c 'jupyter notebook &> /var/log/jupyter_notebook.log'
 Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-  chmod a+rw ${init_script}
+  chmod a+rw "${INIT_SCRIPT}"
+
+  echo "Jupyter setup!"
 }
 
 function launch_jupyter() {
@@ -84,7 +105,8 @@ function launch_jupyter() {
 }
 
 function main() {
-  if [[ "${ROLE}" == 'Master' ]]; then
+  # In an HA cluster, only run on -m-0
+  if [[ "${HOSTNAME}" == "${DATAPROC_MASTER}" ]]; then
     update_apt_get || err 'Failed to update apt-get'
     install_jupyter
     configure_jupyter
