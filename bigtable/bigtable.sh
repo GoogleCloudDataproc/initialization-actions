743b7ade3d87e50d2b745b6ab5aeeb73c5f9b658
Kamil Szewczyk <Kamil.Szewczyk@polidea.com>
2018-05-18 11:33:11 +0200

    Updated README, add HBase lib to classpaths, BigTable client is also installed and configured on workers too.

--- bigtable/bigtable.sh
+++ bigtable/bigtable.sh
@@ -40,12 +40,31 @@ function err() {
 function install_big_table_client() {
     wget -q "${HBASE_BIGTABLE_DL_LINK}" -O "${HBASE_HOME}/lib/${BIGTABLE_HBASE_CLIENT}" \
       || err 'Unable to install BigTable client libs.'
+    ln -s "${HBASE_HOME}/lib/${BIGTABLE_HBASE_CLIENT}" \
+      "/usr/lib/hadoop-mapreduce/lib/${BIGTABLE_HBASE_CLIENT}"
     echo "export PATH=\"${HBASE_HOME}/bin:$PATH\"" >> /etc/profile
 }
 
 function configure_big_table_client() {
 
-        cat << EOF > hbase-site.xml
+#Update classpaths
+ cat << 'EOF' >> /etc/hadoop/conf/hadoop-env.sh
+HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:/usr/lib/hbase/*"
+HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:/usr/lib/hbase/lib/*"
+HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:/etc/hbase/conf"
+EOF
+  cat << 'EOF' >> /etc/hadoop/conf/mapred-env.sh
+HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:/usr/lib/hbase/*"
+HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:/usr/lib/hbase/lib/*"
+HADOOP_CLASSPATH="${HADOOP_CLASSPATH}:/etc/hbase/conf"
+EOF
+  cat << 'EOF' >> /etc/spark/conf/spark-env.sh
+SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/usr/lib/hbase/*"
+SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/usr/lib/hbase/lib/*"
+SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/etc/hbase/conf"
+EOF
+
+  cat << EOF > hbase-site.xml
 <?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
 <configuration>
@@ -68,16 +87,14 @@ function main() {
   local role
   role="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
   big_table_instance="$(/usr/share/google/get_metadata_value attributes/bigtable-instance)"
-  big_table_project="$(/usr/share/google/get_metadata_value attributes/bigtable-project)"
+  big_table_project="$(/usr/share/google/get_metadata_value attributes/bigtable-project || \ 
+    /usr/share/google/get_metadata_value ../project/project-id)"
 
   update_apt_get || err 'Unable to update packages lists.'
   apt-get install -y hbase || err 'Unable to install hbase.'
-  
-  # Only run the installation on Masters
-  if [[ "${role}" == 'Master' ]]; then
-    install_big_table_client
-    configure_big_table_client
-  fi
+
+  install_big_table_client || err 'Unable to install big table client'.
+  configure_big_table_client || err 'Failed to configure big table client.'
 
 }
 
