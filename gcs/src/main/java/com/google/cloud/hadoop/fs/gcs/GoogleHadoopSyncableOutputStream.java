160fd0f9e6a3893db1cb6250597adc7385811f05
idv <idv@google.com>
2018-10-29 10:20:52 -0700

    #cleanup change generated by Refaster ([] to directories [] [] with template java/com/google/devtools/javatools/refactory/refactorings:Java8Refactoring
    	Change on 2018/10/29 by idv <idv@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=219149285

--- gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopSyncableOutputStream.java
+++ gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopSyncableOutputStream.java
@@ -30,7 +30,6 @@ import java.nio.channels.WritableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -298,13 +297,12 @@ public class GoogleHadoopSyncableOutputStream extends OutputStream implements Sy
           destResourceId,
           GoogleCloudStorageFileSystem.objectOptionsFromFileOptions(fileOptions));
       curDestGenerationId = composedObject.getContentGeneration();
-      deletionFutures.add(cleanupThreadpool.submit(new Callable<Void>() {
-        @Override
-        public Void call() throws IOException {
-          ghfs.getGcsFs().getGcs().deleteObjects(ImmutableList.of(tempResourceId));
-          return null;
-        }
-      }));
+      deletionFutures.add(
+          cleanupThreadpool.submit(
+              () -> {
+                ghfs.getGcsFs().getGcs().deleteObjects(ImmutableList.of(tempResourceId));
+                return null;
+              }));
     } else {
       // First commit was direct to the destination; the generationId of the object we just
       // committed will be used as the destination generation id for future compose calls.
--- gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopSyncableOutputStreamTest.java
+++ gcs/src/test/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopSyncableOutputStreamTest.java
@@ -145,7 +145,7 @@ public class GoogleHadoopSyncableOutputStreamTest {
 
     verify(mockExecutorService).submit(any(Callable.class));
 
-    IOException thrown = assertThrows(IOException.class, () -> fout.close());
+    IOException thrown = assertThrows(IOException.class, fout::close);
     assertThat(thrown).hasMessageThat().contains(fakeIoException.getMessage());
 
     verify(mockExecutorService, times(2)).submit(any(Callable.class));
