cb2ac4fb03ff26cb830310cb473a4e86375f0491
Adam Stasiak <adam.stasiak@polidea.com>
2018-12-19 17:46:55 +0100

    Hue review changes

--- hue/test_hue.py
+++ hue/test_hue.py
@@ -1,9 +1,8 @@
-import os
 import unittest
 
 from parameterized import parameterized
-
-from dataproc_test_case import DataprocTestCase
+import os
+from integration_tests.dataproc_test_case import DataprocTestCase
 
 
 class HueTestCase(DataprocTestCase):
@@ -12,34 +11,36 @@ class HueTestCase(DataprocTestCase):
     TEST_SCRIPT_FILE_NAME = 'verify_hue_running.py'
 
     def verify_instance(self, name):
-        self.upload_test_file(name)
+        self.upload_test_file(
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                self.TEST_SCRIPT_FILE_NAME
+            ),
+            name)
         self.__run_test_file(name)
-        self.remove_test_script(name)
+        self.remove_test_script(self.TEST_SCRIPT_FILE_NAME, name)
 
     def __run_test_file(self, name):
-        cmd = 'gcloud compute ssh {} -- "python {} "'.format(
+        cmd = 'gcloud compute ssh {} -- "python3 {} "'.format(
             name,
             self.TEST_SCRIPT_FILE_NAME
         )
         ret_code, stdout, stderr = self.run_command(cmd)
-        print("stdout", stdout)
-        print("retcode", ret_code)
-        print("stderr", stderr)
         self.assertEqual(ret_code, 0, "Failed to run test file. Error: {}".format(stderr))
 
     @parameterized.expand([
         ("SINGLE", "1.0", ["m"]),
         ("STANDARD", "1.0", ["m", "w-0"]),
-        ("HA", "1.0", ["m-0","m-1","m-2", "w-0"]),
+        ("HA", "1.0", ["m-0", "m-1", "m-2", "w-0"]),
         ("SINGLE", "1.1", ["m"]),
         ("STANDARD", "1.1", ["m", "w-0"]),
-        ("HA", "1.1", ["m-0","m-1","m-2", "w-0"]),
+        ("HA", "1.1", ["m-0", "m-1", "m-2", "w-0"]),
         ("SINGLE", "1.2", ["m"]),
         ("STANDARD", "1.2", ["m", "w-0"]),
-        ("HA", "1.2", ["m-0","m-1","m-2", "w-0"]),
+        ("HA", "1.2", ["m-0", "m-1", "m-2", "w-0"]),
         ("SINGLE", "1.3", ["m"]),
         ("STANDARD", "1.3", ["m", "w-0"]),
-        ("HA", "1.3", ["m-0","m-1","m-2", "w-0"]),
+        ("HA", "1.3", ["m-0", "m-1", "m-2", "w-0"]),
     ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
     def test_hue(self, configuration, dataproc_version, machine_suffixes):
         self.createCluster(configuration, self.INIT_ACTION, dataproc_version)
--- hue/verify_hue_running.py
+++ hue/verify_hue_running.py
@@ -9,46 +9,18 @@ BASE = 'localhost'
 PORT = 8888
 
 
-class HueApi(object):
+class Hue(object):
     def __init__(self, base, port):
         self.path = 'http://{}:{}/accounts/login/?next=/'.format(base, port)
         self.host = socket.gethostname()
-        self.response_code = None
-        self.is_main_master = None
 
-    def get_reponse_code(self):
+
+    def get_response_code(self):
         try:
             response = requests.get(self.path)
-            self.response_code = response.status_code
+            return response.status_code
         except requests.exceptions.RequestException:
-            self.response_code = None
-
-    def validate_response_code(self):
-        if self.is_main_master:
-            if self.response_code:
-                if self.response_code < 300:
-                    print('OK - Hue UI is running on master node')
-                else:
-                    raise Exception('NOK - Could not find service UI running')
-            else:
-                raise Exception('NOK - Could not find service UI running')
-        else:
-            if '-w-' in self.host:
-                if self.response_code:
-                    if self.response_code > 300:
-                        print('OK - Hue is not running on worker node')
-                    else:
-                        raise Exception('NOK - UI should not be found on worker node')
-                else:
-                    print('OK - Hue is not running on worker node')
-            if '-m-' in self.host:
-                if self.response_code:
-                    if self.response_code > 300:
-                        print('OK - Hue is not designed to be running on additional master nodes')
-                    else:
-                        raise Exception('NOK - UI should not be found on additional master node')
-                else:
-                    print('OK - Hue is not designed to be running on additional master nodes')
+            return None
 
     def get_main_master(self):
         cmd = '/usr/share/google/get_metadata_value attributes/dataproc-master'
@@ -62,11 +34,11 @@ class HueApi(object):
         stdout, stderr = p.communicate()
         return stdout
 
-    def detect_role(self):
-        if self.host == self.get_main_master():
-            self.is_main_master = True
+    def is_main_master(self):
+        if self.host == self.get_main_master().decode('utf-8'):
+            return True
         else:
-            self.is_main_master = False
+            return False
 
 
 def main():
@@ -78,11 +50,15 @@ def main():
     Raises:
       Exception: If a response does not contain the expected value
     """
-    hueApi = HueApi(BASE, PORT)
-    hueApi.detect_role()
-    hueApi.get_reponse_code()
-    hueApi.validate_response_code()
-
+    hue = Hue(BASE, PORT)
+    is_main_master = hue.is_main_master()
+    if is_main_master:
+        code = hue.get_response_code()
+        assert code is not None, "NOK - Failed to get a status code"
+        assert code < 300, "NOK - Expected to see UI running on this node"
+    else:
+        code = hue.get_response_code()
+        assert code is None, "NOK - Expected to not see UI running on this node"
 
 if __name__ == '__main__':
     main()
