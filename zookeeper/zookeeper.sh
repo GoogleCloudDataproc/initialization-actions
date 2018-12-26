6c7448c0c2b7a9ff08341b609877d79efb875e38
Dagang Wei <38472985+functicons@users.noreply.github.com>
2018-12-26 10:26:28 -0800

    Zookeeper: verify cluster mode and worker count, node ID starts from 0. (#403)

--- zookeeper/zookeeper.sh
+++ zookeeper/zookeeper.sh
@@ -39,15 +39,29 @@ function install_apt_get() {
 ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 CLUSTER_NAME=$(hostname | sed -r 's/(.*)-[w|m](-[0-9]+)?$/\1/')
 
+# Validate the cluster mode and worker count.
+ADDITIONAL_MASTER=$(/usr/share/google/get_metadata_value attributes/dataproc-master-additional)
+if [[ -n "$ADDITIONAL_MASTER" ]]; then
+  echo "ZooKeeper init action cannot be used in HA clusters which already have ZooKeeper running."
+  exit 1
+fi
+
+WORKER_COUNT=$(/usr/share/google/get_metadata_value attributes/dataproc-worker-count)
+if [[ "$WORKER_COUNT" -lt 2 ]]; then
+  echo "ZooKeeper requires at least 2 worker nodes."
+  exit 1
+fi
+
 # Download and extract ZooKeeper Server
 update_apt_get
 install_apt_get zookeeper-server
 mkdir -p /var/lib/zookeeper
 
-# Configure ZooKeeper node ID
-NODE_NUMBER=1
+# Configure ZooKeeper node ID, master has ID 0, workers start from 1.
 if [[ "${ROLE}" == 'Worker' ]]; then
-	NODE_NUMBER=$((`hostname | sed 's/.*-w-\([0-9]\)*.*/\1/g'`+2))
+  NODE_NUMBER=$((`hostname | sed 's/.*-w-\([0-9]\)*.*/\1/g'`+1))
+else
+  NODE_NUMBER=0
 fi
 echo ${NODE_NUMBER} >| /var/lib/zookeeper/myid
 
@@ -61,9 +75,9 @@ dataDir=/var/lib/zookeeper
 clientPort=2181
 initLimit=5
 syncLimit=2
-server.1=${CLUSTER_NAME}-m:2888:3888
-server.2=${CLUSTER_NAME}-w-0:2888:3888
-server.3=${CLUSTER_NAME}-w-1:2888:3888
+server.0=${CLUSTER_NAME}-m:2888:3888
+server.1=${CLUSTER_NAME}-w-0:2888:3888
+server.2=${CLUSTER_NAME}-w-1:2888:3888
 EOF
 
 # Restart ZooKeeper
