c0fa718370ea2e2ad336978e2203bfc61472e035
Kamil Szewczyk <Kamil.Szewczyk@polidea.com>
2019-01-02 13:42:12 +0100

    Apache Tez integration test will use tests file from hdfs location.

--- tez/verify_tez.py
+++ tez/verify_tez.py
@@ -6,8 +6,12 @@ import subprocess
 
 
 def main():
+    copy_test_file_cmd = shlex.split(
+        'hadoop fs -copyFromLocal /usr/lib/tez/LICENSE-MIT /tmp/LICENSE-MIT')
+    copy_test_file_out = subprocess.Popen(
+        copy_test_file_cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     health_check_cmd = shlex.split(
-        'hadoop jar /usr/lib/tez/tez-examples.jar orderedwordcount file:///usr/lib/tez/LICENSE-MIT /tmp/tez-out')
+        'hadoop jar /usr/lib/tez/tez-examples.jar orderedwordcount /tmp/LICENSE-MIT /tmp/tez-out')
     health_check_out = subprocess.Popen(
         health_check_cmd,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     info = health_check_out.communicate()[0]
