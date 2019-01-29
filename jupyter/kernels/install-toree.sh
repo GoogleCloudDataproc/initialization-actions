62f9ef0e72d013e72f6d7e85a91b8bed09813015
czarstark <trevor.balint@gmail.com>
2019-01-29 06:57:10 +0000

    Corrected path to toree installer
    Path pointed to an outdated, in-development version of toree. It now points to a released version.

--- jupyter/kernels/install-toree.sh
+++ jupyter/kernels/install-toree.sh
@@ -9,7 +9,8 @@ set -e
 REPO_URL="https://dist.apache.org/repos/dist"
 /opt/conda/bin/pip \
     install \
-    ${REPO_URL}/dev/incubator/toree/0.2.0-incubating-rc5/toree-pip/toree-0.2.0.tar.gz
+    ${REPO_URL}/release/incubator/toree/0.2.0-incubating-rc5/toree-pip/toree-0.2.0.tar.gz
+    
 
 SPARK_MAJOR_VERSION=$(spark-submit --version |& \
   grep 'version' | head -n 1 | sed 's/.*version //' | cut -d '.' -f 1)
