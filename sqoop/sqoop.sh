6240abc261b15cb1bfaa38b7d1e6f3b89db14f0c
Kamil Szewczyk <Kamil.Szewczyk@polidea.com>
2019-02-21 13:16:15 +0100

    Sqoop init action (#268). Sqoop was compiled from release-1.4.7-rc0 with cherry-picked commits:   1. 18445290810b1df035e06fb074064d6b9c1d6e90 - SQOOP-3222   2. e13dd21209c26316d43350a23f5d533321b61352 - SQOOP-3232 Sqoop jar is downloaded from google storage.
    This PR contains automated tests with following scenarios:
    1. Verify Sqoop installation.
    2. Test import Cloud SQL -> Hadoop HDFS
    3. Test import Cloud SQL -> Cloud Bigtable
    4. Test import Cloud SQL -> HBase
    
    You can run automated tests using command:
      python -m unittests sqoop/test_sqoop.py

---
+++ sqoop/sqoop.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#    Copyright 2018 Google, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# This script installs Apache Sqoop (http://sqoop.apache.org/) on a Google Cloud
+# Dataproc cluster.
+#
+
+set -euxo pipefail
+
+readonly SQOOP_HOME='/usr/lib/sqoop'
+readonly SQOOP_CODE_LINK='https://github.com/szewi/sqoop'
+readonly SQOOP_JAR='https://storage.googleapis.com/sqoop-bigtable-connector/sqoop-1.4.7.jar'
+readonly AVRO_JAR='http://repo1.maven.org/maven2/org/apache/avro/avro/1.8.1/avro-1.8.1.jar'
+readonly BIGTABLE_HBASE_CLIENT='bigtable-hbase-1.x-hadoop-1.3.0.jar'
+readonly BIGTABLE_HBASE_DL_LINK="http://central.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-1.x-hadoop/1.3.0/${BIGTABLE_HBASE_CLIENT}"
+readonly MYSQL_JAR='/usr/share/java/mysql.jar'
+
+function err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+  return 1
+}
+
+function install_sqoop() {
+  git clone -b sqoop-bigtable-client "${SQOOP_CODE_LINK}" "${SQOOP_HOME}" \
+    || err 'Cloning Sqoop sources from code location failed.'
+  wget -q "${SQOOP_JAR}" -O "${SQOOP_HOME}/sqoop-1.4.7.jar" \
+    || err 'Downloading sqoop jar file failed.'
+  echo "export PATH=\"${SQOOP_HOME}/bin:$PATH\"" >> /etc/profile
+}
+
+function install_sqoop_dependencies_and_connectors() {
+  # Sqoop requires avro 1.8.+
+  wget -q "${AVRO_JAR}" -O "${SQOOP_HOME}/lib/avro-1.8.1.jar" \
+    || err 'Downloading avro jar file failed.'
+  # Install sqoop connectors in /usr/lib/sqoop/lib
+  wget -q "${BIGTABLE_HBASE_DL_LINK}" -O "${SQOOP_HOME}/lib/${BIGTABLE_HBASE_CLIENT}" \
+    || err 'Error getting BigTable-HBase connector.'
+  # Sqoop will use locally available mysql jdbc driver
+  ln -s "${MYSQL_JAR}" "${SQOOP_HOME}/lib/mysql.jar"
+}
+
+function main() {
+  local role
+  role="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
+  
+  # Only run the installation on Masters
+  if [[ "${role}" == 'Master' ]]; then
+    install_sqoop
+    install_sqoop_dependencies_and_connectors
+  fi
+
+}
+
+main
---
+++ sqoop/test_sqoop.py
@@ -0,0 +1,284 @@
+import unittest
+import datetime
+from parameterized import parameterized
+
+from integration_tests.dataproc_test_case import DataprocTestCase
+
+
+class SqoopTestCase(DataprocTestCase):
+    COMPONENT = 'sqoop'
+    INIT_ACTION = 'gs://dataproc-initialization-actions/sqoop/sqoop.sh'
+    SQL_TO_HDFS_HELPER_ACTIONS = \
+        'gs://dataproc-initialization-actions/cloud-sql-proxy/cloud-sql-proxy.sh'
+    SQL_TO_BIGTABLE_HELPER_ACTIONS = "{},{}".format(
+        'gs://dataproc-initialization-actions/cloud-sql-proxy/cloud-sql-proxy.sh',
+        'gs://dataproc-initialization-actions/bigtable/bigtable.sh'
+    )
+    SQL_TO_HBASE_HELPER_ACTIONS = "{},{},{}".format(
+        'gs://dataproc-initialization-actions/cloud-sql-proxy/cloud-sql-proxy.sh',
+        'gs://dataproc-initialization-actions/zookeeper/zookeeper.sh',
+        'gs://dataproc-initialization-actions/hbase/hbase.sh'
+    )
+    CLOUD_SQL_INSTANCE_NAME = None
+    CLOUD_BIGTABLE_INSTANCE_NAME = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        _, region, _ = cls.run_command("gcloud config get-value compute/region")
+        cls.REGION = region.strip() or "global"
+        _, zone, _ = cls.run_command("gcloud config get-value compute/zone")
+        cls.ZONE = zone.strip()
+        _, project, _ = cls.run_command("gcloud config get-value project")
+        project = project.strip()
+        cls.CLOUD_SQL_METADATA = '{}:{}'.format(project, cls.REGION)
+        cls.CLOUD_SQL_INSTANCE_NAME = "test-{}-sql-db".format(
+            datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+        cls.CLOUD_BIGTABLE_INSTANCE_NAME = "test-{}-db".format(
+            datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        )
+        cls.CLOUD_BIGTABLE_METADATA = "bigtable-instance={},bigtable-project={}" \
+            .format(cls.CLOUD_BIGTABLE_INSTANCE_NAME, project)
+
+        ret_code, stdout, stderr = cls.run_command(
+            'gcloud sql instances create {} --region {}'.format(
+                cls.CLOUD_SQL_INSTANCE_NAME, cls.REGION)
+        )
+        assert ret_code == 0, "Failed to create sql instance {}. Last error: {}".format(
+            cls.CLOUD_SQL_INSTANCE_NAME, stderr)
+        ret_code, stdout, stderr = cls.run_command(
+            'gcloud beta bigtable instances create {} --cluster {} --cluster-zone {} '
+            '--display-name={} --instance-type=DEVELOPMENT'.format(
+                cls.CLOUD_BIGTABLE_INSTANCE_NAME, cls.CLOUD_BIGTABLE_INSTANCE_NAME,
+                cls.ZONE, cls.CLOUD_BIGTABLE_INSTANCE_NAME)
+        )
+        assert ret_code == 0,  "Failed to create bigtable instance {}. Last error: {}".format(
+            cls.CLOUD_BIGTABLE_INSTANCE_NAME, stderr)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        ret_code, stdout, stderr = cls.run_command(
+            'gcloud sql instances delete {}'.format(
+                cls.CLOUD_SQL_INSTANCE_NAME)
+        )
+        assert ret_code == 0, "Failed to delete sql instance {}. Last error: {}".format(
+            cls.CLOUD_SQL_INSTANCE_NAME, stderr)
+
+        ret_code, stdout, stderr = cls.run_command(
+            'gcloud beta bigtable instances delete {}'.format(
+                cls.CLOUD_BIGTABLE_INSTANCE_NAME)
+        )
+        assert ret_code == 0, "Failed to delete bigtable instance {}. Last error: {}".format(
+            cls.CLOUD_BIGTABLE_INSTANCE_NAME, stderr)
+
+    def verify_instance(self, name):
+        ret_code, stdout, stderr = self.run_command(
+            'gcloud compute ssh {} --command "/usr/lib/sqoop/bin/sqoop version"'.format(
+                name
+            )
+        )
+        self.assertEqual(ret_code, 0, "Failed to validate cluster. Error: {}".format(stderr))
+
+    def verify_importing_to_hdfs(self, name):
+        ret_code, stdout, stderr = self.run_command(
+                'gcloud compute ssh {} --command "/usr/lib/sqoop/bin/sqoop {}"'.format(
+                    name,
+                    "import --connect jdbc:mysql://localhost/employees "
+                    "--username root "
+                    "--table employees --m 1"
+                )
+            )
+        self.assertEqual(ret_code, 0, "Failed to validate cluster. Error: {}".format(stderr))
+
+    def verify_importing_to_bigtable_hbase(self, name):
+        hbase_table_name = "employees-{}".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+        ret_code, stdout, stderr = self.run_command(
+                'gcloud compute ssh {} --command "/usr/lib/sqoop/bin/sqoop {}"'.format(
+                    name,
+                    "import --connect jdbc:mysql://localhost/employees "
+                    "--username root --table employees --columns \"emp_no,first_name\" "
+                    "--hbase-table {} --column-family my-column-family "
+                    "--hbase-row-key emp_no --m 1 --hbase-create-table".format(
+                        hbase_table_name
+                    )
+                )
+            )
+        self.assertEqual(ret_code, 0, "Failed to validate cluster. Error: {}".format(stderr))
+
+    def populate_database_if_needed(self, name):
+        ret_code, _, _ = self.run_command(
+            'gcloud compute ssh {} --command "{}"'.format(
+                name,
+                'mysql -u root -e "show databases" | grep "employees"'
+            )
+        )
+        if ret_code != 0:
+            ret_code, stdout, stderr = self.run_command(
+                'gcloud compute ssh {} --command "{}"'.format(
+                    name,
+                    'git clone https://github.com/datacharmer/test_db'
+                )
+            )
+            self.assertEqual(ret_code, 0, "Failed to clone example database. Error: {}".format(stderr))
+            ret_code, stdout, stderr = self.run_command(
+                'gcloud compute ssh {} --command "{}"'.format(
+                    name,
+                    'cd test_db && mysql -u root < employees.sql'
+                )
+            )
+            self.assertEqual(ret_code, 0, "Failed to populate database. Error: {}".format(stderr))
+
+    def make_database_reusable(self, name):
+        ret_code, stdout, stderr = self.run_command(
+            'gcloud compute ssh {} --command "{}"'.format(
+                name,
+                "mysql -u root -e 'drop database hive_metastore;'"
+            )
+        )
+        self.assertEqual(ret_code, 0, "Failed to populate database. Error: {}".format(stderr))
+
+
+    @parameterized.expand([
+        ("SINGLE", "1.2", ["m"]),
+        ("STANDARD", "1.2", ["m"]),
+        ("HA", "1.2", ["m-0"]),
+        ("SINGLE", "1.3", ["m"]),
+        ("STANDARD", "1.3", ["m"]),
+        ("HA", "1.3", ["m-0"]),
+    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_sqoop(self, configuration, dataproc_version, machine_suffixes):
+        self.createCluster(configuration, self.INIT_ACTION, dataproc_version)
+        for machine_suffix in machine_suffixes:
+            self.verify_instance(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+
+    @parameterized.expand([
+        ("SINGLE", "1.2", ["m"], SQL_TO_HDFS_HELPER_ACTIONS),
+        ("STANDARD", "1.2", ["m"], SQL_TO_HDFS_HELPER_ACTIONS),
+        ("HA", "1.2", ["m-0"], SQL_TO_HDFS_HELPER_ACTIONS),
+        ("SINGLE", "1.3", ["m"], SQL_TO_HDFS_HELPER_ACTIONS),
+        ("STANDARD", "1.3", ["m"], SQL_TO_HDFS_HELPER_ACTIONS),
+        ("HA", "1.3", ["m-0"], SQL_TO_HDFS_HELPER_ACTIONS),
+    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_sqoop_import_from_cloud_sql_to_hdfs(
+            self, configuration, dataproc_version, machine_suffixes, helper_actions):
+        if helper_actions:
+            init_actions = "{},{}".format(helper_actions, self.INIT_ACTION)
+        else:
+            init_actions = self.INIT_ACTION
+        self.createCluster(configuration, init_actions, dataproc_version,
+                           metadata='hive-metastore-instance={}:{}'.format(
+                               self.CLOUD_SQL_METADATA, self.CLOUD_SQL_INSTANCE_NAME),
+                           scopes='sql-admin'
+                           )
+        for machine_suffix in machine_suffixes:
+            self.populate_database_if_needed(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+            self.verify_importing_to_hdfs(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+            self.make_database_reusable(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+
+    @parameterized.expand([
+        ("SINGLE", "1.2", ["m"], SQL_TO_BIGTABLE_HELPER_ACTIONS),
+        ("STANDARD", "1.2", ["m"], SQL_TO_BIGTABLE_HELPER_ACTIONS),
+        ("HA", "1.2", ["m-0"], SQL_TO_BIGTABLE_HELPER_ACTIONS),
+        ("SINGLE", "1.3", ["m"], SQL_TO_BIGTABLE_HELPER_ACTIONS),
+        ("STANDARD", "1.3", ["m"], SQL_TO_BIGTABLE_HELPER_ACTIONS),
+        ("HA", "1.3", ["m-0"], SQL_TO_BIGTABLE_HELPER_ACTIONS),
+    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_sqoop_import_from_cloud_sql_to_bigtable(
+            self, configuration, dataproc_version, machine_suffixes, helper_actions):
+        if helper_actions:
+            init_actions = "{},{}".format(helper_actions, self.INIT_ACTION)
+        else:
+            init_actions = self.INIT_ACTION
+        self.createCluster(configuration, init_actions, dataproc_version,
+                           metadata='hive-metastore-instance={}:{},{}'.format(
+                               self.CLOUD_SQL_METADATA, self.CLOUD_SQL_INSTANCE_NAME,
+                               self.CLOUD_BIGTABLE_METADATA),
+                           scopes='cloud-platform'
+                           )
+        for machine_suffix in machine_suffixes:
+            self.populate_database_if_needed(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+            self.verify_importing_to_bigtable_hbase(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+            self.make_database_reusable(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+
+    @parameterized.expand([
+        ("SINGLE", "1.2", ["m"], SQL_TO_HBASE_HELPER_ACTIONS),
+        ("STANDARD", "1.2", ["m"], SQL_TO_HBASE_HELPER_ACTIONS),
+        ("HA", "1.2", ["m-0"], SQL_TO_HBASE_HELPER_ACTIONS),
+        ("SINGLE", "1.3", ["m"], SQL_TO_HBASE_HELPER_ACTIONS),
+        ("STANDARD", "1.3", ["m"], SQL_TO_HBASE_HELPER_ACTIONS),
+        ("HA", "1.3", ["m-0"], SQL_TO_HBASE_HELPER_ACTIONS),
+    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_sqoop_import_from_cloud_sql_to_hbase(
+            self, configuration, dataproc_version, machine_suffixes, helper_actions):
+        if helper_actions:
+            init_actions = "{},{}".format(helper_actions, self.INIT_ACTION)
+        else:
+            init_actions = self.INIT_ACTION
+        if configuration == "HA":
+            init_actions = init_actions.replace(
+                ',gs://dataproc-initialization-actions/zookeeper/zookeeper.sh', '')
+        self.createCluster(configuration, init_actions, dataproc_version,
+                           metadata='hive-metastore-instance={}:{}'.format(
+                               self.CLOUD_SQL_METADATA, self.CLOUD_SQL_INSTANCE_NAME),
+                           scopes='sql-admin'
+                           )
+        for machine_suffix in machine_suffixes:
+            self.populate_database_if_needed(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+            self.verify_importing_to_bigtable_hbase(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+            self.make_database_reusable(
+                "{}-{}".format(
+                    self.getClusterName(),
+                    machine_suffix
+                )
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
