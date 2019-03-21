67fb0e939e5fda87141ab9ffc0cd2abe63edc1df
Kamil Szewczyk <Kamil.Szewczyk@polidea.com>
2019-03-21 15:58:20 +0100

    Change sqoop version to 1.4.6

--- sqoop/sqoop.sh
+++ sqoop/sqoop.sh
@@ -21,8 +21,7 @@ set -euxo pipefail
 
 readonly SQOOP_HOME='/usr/lib/sqoop'
 readonly SQOOP_CODE_LINK='https://github.com/szewi/sqoop'
-readonly SQOOP_JAR='https://storage.googleapis.com/sqoop-bigtable-connector/sqoop-1.4.7.jar'
-readonly AVRO_JAR='http://repo1.maven.org/maven2/org/apache/avro/avro/1.8.1/avro-1.8.1.jar'
+readonly SQOOP_JAR='https://storage.googleapis.com/sqoop-bigtable-connector/sqoop-1.4.6.jar'
 readonly BIGTABLE_HBASE_CLIENT='bigtable-hbase-1.x-hadoop-1.3.0.jar'
 readonly BIGTABLE_HBASE_DL_LINK="http://central.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-1.x-hadoop/1.3.0/${BIGTABLE_HBASE_CLIENT}"
 readonly MYSQL_JAR='/usr/share/java/mysql.jar'
@@ -33,17 +32,14 @@ function err() {
 }
 
 function install_sqoop() {
-  git clone -b sqoop-bigtable-client "${SQOOP_CODE_LINK}" "${SQOOP_HOME}" \
+  git clone -b sqoop-14 "${SQOOP_CODE_LINK}" "${SQOOP_HOME}" \
     || err 'Cloning Sqoop sources from code location failed.'
-  wget -q "${SQOOP_JAR}" -O "${SQOOP_HOME}/sqoop-1.4.7.jar" \
+  wget -q "${SQOOP_JAR}" -O "${SQOOP_HOME}/sqoop-1.4.6.jar" \
     || err 'Downloading sqoop jar file failed.'
   echo "export PATH=\"${SQOOP_HOME}/bin:$PATH\"" >> /etc/profile
 }
 
 function install_sqoop_dependencies_and_connectors() {
-  # Sqoop requires avro 1.8.+
-  wget -q "${AVRO_JAR}" -O "${SQOOP_HOME}/lib/avro-1.8.1.jar" \
-    || err 'Downloading avro jar file failed.'
   # Install sqoop connectors in /usr/lib/sqoop/lib
   wget -q "${BIGTABLE_HBASE_DL_LINK}" -O "${SQOOP_HOME}/lib/${BIGTABLE_HBASE_CLIENT}" \
     || err 'Error getting BigTable-HBase connector.'
