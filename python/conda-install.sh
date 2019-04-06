5f07935d855aea3f94bcf1f2cc75a92f744007b4
DanSedov <DanSedov@users.noreply.github.com>
2019-04-05 17:28:20 -0700

    Update conda-install.sh

--- python/conda-install.sh
+++ python/conda-install.sh
@@ -9,11 +9,8 @@ function main() {
     echo "ERROR: Must specify CONDA_PACKAGES metadata key" 
     exit 1
   fi
-<<<<<<< HEAD
+
   conda install "${PACKAGES}"
-=======
-  conda install ${PACKAGES}
->>>>>>> upstream/master
 }
 
 main
