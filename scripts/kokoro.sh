dededd814446422f1c8d5da50c35677db1e46150
Igor Dvorzhak <idv@google.com>
2019-04-14 21:38:04 -0700

    Integrate with Kokoro CI

---
+++ scripts/kokoro.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euxo pipefail
+
+SCRIPT_DIR=$(dirname "$0")
+REPO_DIR=$(realpath "${SCRIPT_DIR}/..")
+
+cd "${REPO_DIR}"
+
+# Run tests
+python -m unittest hive-hcatalog.test_hive
