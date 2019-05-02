d1a7b3e46efae0c727f322abe4c245da423ed8a7
Igor Dvorzhak <idv@google.com>
2019-05-02 10:18:00 -0700

    Update push-to-gcs.sh

--- push-to-gcs.sh
+++ push-to-gcs.sh
@@ -42,10 +42,10 @@ if [[ ! -d "${MODULE}" ]]; then
   exit 4
 fi
 
-# Verify shell scripts have permission mode 750.
+# Verify shell scripts have permission mode 75x.
 for file in ${MODULE}/*.sh; do
-  if [[ "$(stat -c '%a' ${file})" != "750" ]]; then
-    echo "The permission mode of script ${file} is $(stat -c '%a' ${file}), expected: 750."
+  if [[ "$(stat -c '%a' ${file})" != 75* ]]; then
+    echo "The permission mode of script ${file} is $(stat -c '%a' ${file}), expected: 75x."
     exit 5
   fi
 done
