588157590aaca9c08cc8eeeb9b2412604f8740a7
idv <idv@google.com>
2019-05-15 10:34:39 -0700

    Add missing JUnit test class annotation
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=248358971

--- bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/output/BigQueryTimePartitioningTest.java
+++ bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/output/BigQueryTimePartitioningTest.java
@@ -17,7 +17,10 @@ import static com.google.common.truth.Truth.assertThat;
 
 import java.io.IOException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class BigQueryTimePartitioningTest {
 
   public static final String TIME_PARTITIONING_JSON =
