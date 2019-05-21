ed9edc2c98b1f2311e665bf8487e5505a3086d2d
Randy Gelhausen <rgelhau@gmail.com>
2019-05-20 23:24:09 -0400

    re-basing to add author e-mail to all commits, adding dill for saving models between clusters

--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -1,5 +1,6 @@
 import random
 import unittest
+import os
 
 from parameterized import parameterized
 
@@ -15,35 +16,34 @@ class RapidsTestCase(DataprocTestCase):
     TEST_SCRIPT_FILE_NAME = 'verify_rapids.py'
 
     def verify_instance(self, name):
-        self.__verify_gpu_driver(name)
+        self.upload_test_file(
+            os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                self.TEST_SCRIPT_FILE_NAME
+            ),
+            name)
+        self.__run_test_script(name)
+        self.remove_test_script(self.TEST_SCRIPT_FILE_NAME, name)
 
-    def __verify_gpu_driver(self, name):
+    def __run_test_script(self, name):
         ret_code, stdout, stderr = self.run_command(
-            "gcloud compute ssh {} -- \"nvidia-smi\"".format(
-                name
-            )
-        )
-        self.assertEqual(ret_code, 0, "Failed to install GPU driver. Error: {}".format(stderr))
-
-    def __verify_rapids(self, name):
-        ret_code, stdout, stderr = self.run_command(
-            'gcloud compute ssh {} -- "source activate RAPIDS && python {}"'.format(
+            'gcloud compute ssh {} -- "/opt/conda/anaconda/envs/RAPIDS/bin/python {}"'.format(
                 name,
                 self.TEST_SCRIPT_FILE_NAME,
             )
         )
-        self.assertEqual(ret_code, 0, "Failed to validate RAPIDS install. Error: {}".format(stderr))
+        self.assertEqual(ret_code, 0, "Failed to validate RAPIDS install. Last error: {}".format(stderr))
 
     @parameterized.expand([
-        ("STANDARD", "1.3", ["m"], 1, 2)
+        ("STANDARD", "1.3", ["m"])
     ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
-    def test_rapids(self, configuration, dataproc_version, machine_suffixes, coordinators, workers):
+    def test_rapids(self, configuration, dataproc_version, machine_suffixes):
         self.createCluster(configuration, self.INIT_ACTION,
                            dataproc_version, metadata=self.METADATA, beta=True,
-                           zone='us-east1-c',
-                           master_accelerator='type=nvidia-tesla-t4',
-                           worker_accelerator='type=nvidia-tesla-t4',
-                           optional_components='ANACONDA')
+                           master_accelerator='type=nvidia-tesla-p100',
+                           worker_accelerator='type=nvidia-tesla-p100',
+                           optional_components='ANACONDA',
+                           timeout_in_minutes=20)
 
         for machine_suffix in machine_suffixes:
             self.verify_instance(
@@ -52,8 +52,6 @@ class RapidsTestCase(DataprocTestCase):
                     machine_suffix
                 )
             )
-        for machine in coordinators:
-            self.__verify_rapids(machine)
 
 
 if __name__ == '__main__':
--- rapids/verify_rapids.py
+++ rapids/verify_rapids.py
@@ -1,22 +1,14 @@
-import dask, dask_cudf, socket
-from dask.distributed import Client
+import cudf, dask_cudf
 
 import cuml, dask_cuml
+
 import cugraph
 
 import xgboost, dask_xgboost
 
-if __name__ == "__main__":
-    # connect to dask cluster
-    client = Client(socket.gethostname()+':8786')
-
-    # confirm at least 1 GPU worker
-    assert(len(client.scheduler_info()['workers']) > 0)
-
-    # confirm can actually use RAPIDS libraries
-    import cudf
-    df = cudf.DataFrame()
-    df['a'] = [0, 1, 2]
-    df['b'] = [1, 2, 3]
-    df['c'] = df.a * df.b + 100
-    dmat = xgboost.DMatrix(df)
+# confirm can actually use RAPIDS libraries
+df = cudf.DataFrame()
+df['a'] = [0, 1, 2]
+df['b'] = [1, 2, 3]
+df['c'] = df.a * df.b + 100
+dmat = xgboost.DMatrix(df)
