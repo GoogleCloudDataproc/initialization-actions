ec82dd7721560a9e8a35b6cf93841bc6d72bb6ea
idv <idv@google.com>
2019-05-22 16:48:14 -0700

    Run BQ connector RegionalIntegrationTest in US region for VPCSC tests.
    RegionalIntegrationTest uses "asia-northeast1" region by default which is outside of VPCSC perimeter.
    
    	Change on 2019/05/22 by idv <idv@google.com>
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=249548183

--- bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/RegionalIntegrationTest.java
+++ bigquery/src/test/java/com/google/cloud/hadoop/io/bigquery/RegionalIntegrationTest.java
@@ -51,7 +51,8 @@ import org.junit.runners.JUnit4;
 public class RegionalIntegrationTest {
 
   private static final String RANDOM_STRING = UUID.randomUUID().toString().substring(0, 4);
-  private static final String LOCATION = "asia-northeast1";
+  private static final String LOCATION =
+      Boolean.parseBoolean(System.getenv("GOOGLE_CLOUD_TESTS_IN_VPCSC")) ? "US" : "asia-northeast1";
   private static final String TEST_BUCKET_PREFIX = "bq_integration_test";
   private static final String PROJECT_ID = TestConfiguration.getInstance().getProjectId();
   private static final String DATASET_ID =
@@ -119,7 +120,7 @@ public class RegionalIntegrationTest {
   @Before
   public void setup() throws Exception {
     bucketHelper = new TestBucketHelper(TEST_BUCKET_PREFIX);
-    String testBucket = bucketHelper.getUniqueBucketName(LOCATION);
+    String testBucket = bucketHelper.getUniqueBucketName(LOCATION.toLowerCase());
 
     // Define the schema we will be using for the output BigQuery table.
     List<BigQueryTableFieldSchema> outputTableFieldSchema = new ArrayList<>();
@@ -184,7 +185,7 @@ public class RegionalIntegrationTest {
     gcs = gcsFs.getGcs();
 
     // Create input dataset in `LOCATION`
-    String exportBucket = bucketHelper.getUniqueBucketName("us");
+    String exportBucket = bucketHelper.getUniqueBucketName("export-us");
     storage
         .buckets()
         .insert(PROJECT_ID, new Bucket().setName(exportBucket).setLocation("US"))
