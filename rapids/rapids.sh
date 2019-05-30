a0e75403f513102763448832f06d0520732bf120
Aniket Mokashi <aniket486@gmail.com>
2019-05-29 18:00:32 -0700

    Update rapids.sh

--- rapids/rapids.sh
+++ rapids/rapids.sh
@@ -6,19 +6,20 @@ readonly INIT_ACTIONS_REPO="$(/usr/share/google/get_metadata_value attributes/IN
 readonly INIT_ACTIONS_BRANCH="$(/usr/share/google/get_metadata_value attributes/INIT_ACTIONS_BRANCH \
   || echo 'master')"
 
-echo "Cloning fresh dataproc-initialization-actions from repo ${INIT_ACTIONS_REPO} and branch ${INIT_ACTIONS_BRANCH}..."
+echo "Cloning dataproc-initialization-actions from repo ${INIT_ACTIONS_REPO} and branch ${INIT_ACTIONS_BRANCH}..."
 git clone -b "${INIT_ACTIONS_BRANCH}" --single-branch "${INIT_ACTIONS_REPO}"
 
 ./dataproc-initialization-actions/rapids/internal/install-gpu-driver.sh
 
-# for use with Anaconda component
-conda env create --name RAPIDS --file /dataproc-initialization-actions/rapids/internal/conda-environment.yml
-# uses miniconda and jupyter init-actions instead of Anaconda component
-#bash rapids/internal/install-rapids.sh
+# For use with Anaconda component
+conda env create --name RAPIDS --file dataproc-initialization-actions/rapids/internal/conda-environment.yml
 
-# get Jupyter instance to pickup RAPIDS environment
-#ToDo: detect if Jupyter optional component configured
-if [[ "${ROLE}" == 'Master' ]]; then
+# For use with miniconda and jupyter init-actions instead of Anaconda component
+# bash rapids/internal/install-rapids.sh
+
+# Get Jupyter instance to pickup RAPIDS environment.
+# TODO: Detect if Jupyter optional component is configured.
+if [[ "${ROLE}" == "Master" ]]; then
     /opt/conda/anaconda/bin/conda install -y nb_conda_kernels
     service jupyter restart
 fi
