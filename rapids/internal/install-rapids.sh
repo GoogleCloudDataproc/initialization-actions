a79f4b9df3d10e13db0c5b1038121c114ec10abb
Aniket Mokashi <aniket486@gmail.com>
2019-05-29 18:11:37 -0700

    Update install-rapids.sh

--- rapids/internal/install-rapids.sh
+++ rapids/internal/install-rapids.sh
@@ -6,16 +6,16 @@ readonly DATAPROC_BUCKET="$(/usr/share/google/get_metadata_value attributes/data
 readonly CONDA_ENV_YAML_GSC_LOC="gs://dataproc-initialization-actions/rapids/conda-environment.yml"
 readonly CONDA_ENV_YAML_PATH="/root/conda-environment.yml"
 
-echo "Downloading conda environment at $CONDA_ENV_YAML_GSC_LOC to $CONDA_ENV_YAML_PATH ... "
+echo "Downloading conda environment at $CONDA_ENV_YAML_GSC_LOC to $CONDA_ENV_YAML_PATH... "
 gsutil -m cp -r $CONDA_ENV_YAML_GSC_LOC $CONDA_ENV_YAML_PATH
 gsutil -m cp -r gs://dataproc-initialization-actions/conda/bootstrap-conda.sh .
 gsutil -m cp -r gs://dataproc-initialization-actions/conda/install-conda-env.sh .
 
 chmod 755 ./*conda*.sh
 
-# Install Miniconda / conda
+# Install Miniconda/Conda
 ./bootstrap-conda.sh
-# Create / Update conda environment via conda yaml
+# Create/Update Conda environment via Conda yaml
 CONDA_ENV_YAML=$CONDA_ENV_YAML_PATH ./install-conda-env.sh
 
 source /etc/profile.d/conda.sh
