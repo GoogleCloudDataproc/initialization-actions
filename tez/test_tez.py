88c037934a9cc1e6b5d55d82716d185f28bc2175
Igor Dvorzhak <idv@google.com>
2019-06-10 10:25:07 -0700

    Fix Tez integration test

--- tez/test_tez.py
+++ tez/test_tez.py
@@ -7,8 +7,8 @@ executed on every master node. Test script run example Tez job and successful ex
 2. On dataproc 1.3 Tez is pre-installed, so clusters are created without --initialization-actions
 flag. Test script is executed on every master node.
 """
-import unittest
 import os
+import unittest
 
 from parameterized import parameterized
 
@@ -21,57 +21,56 @@ class TezTestCase(DataprocTestCase):
     TEST_SCRIPT_FILE_NAME = 'verify_tez.py'
 
     def verify_instance(self, name):
-        self.upload_test_file(
-            os.path.join(
-                os.path.dirname(os.path.abspath(__file__)),
-                self.TEST_SCRIPT_FILE_NAME
-            ),
-            name)
+        test_script_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            self.TEST_SCRIPT_FILE_NAME)
+        self.upload_test_file(test_script_path, name)
         self.__run_test_script(name)
         self.remove_test_script(self.TEST_SCRIPT_FILE_NAME, name)
 
     def __run_test_script(self, name):
         ret_code, stdout, stderr = self.run_command(
-            'gcloud compute ssh {} -- "python {}"'.format(
-                name,
-                self.TEST_SCRIPT_FILE_NAME,
-            )
-        )
-        self.assertEqual(ret_code, 0, "Failed to validate cluster. Last error: {}".format(stderr))
+            'gcloud compute ssh {} --command="python {}"'.format(
+                name, self.TEST_SCRIPT_FILE_NAME))
+        self.assertEqual(
+            ret_code, 0,
+            "Failed to validate cluster. Last error: {}".format(stderr))
 
-    @parameterized.expand([
-        ("SINGLE", "1.1", ["m"]),
-        ("STANDARD", "1.1", ["m"]),
-        ("HA", "1.1", ["m-0", "m-1", "m-2"]),
-        ("SINGLE", "1.2", ["m"]),
-        ("STANDARD", "1.2", ["m"]),
-        ("HA", "1.2", ["m-0", "m-1", "m-2"]),
-    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    @parameterized.expand(
+        [
+            ("SINGLE", "1.1", ["m"]),
+            ("STANDARD", "1.1", ["m"]),
+            ("HA", "1.1", ["m-0", "m-1", "m-2"]),
+            ("SINGLE", "1.2", ["m"]),
+            ("STANDARD", "1.2", ["m"]),
+            ("HA", "1.2", ["m-0", "m-1", "m-2"]),
+        ],
+        testcase_func_name=DataprocTestCase.generate_verbose_test_name)
     def test_tez(self, configuration, dataproc_version, machine_suffixes):
         self.createCluster(configuration, self.INIT_ACTIONS, dataproc_version)
         for machine_suffix in machine_suffixes:
-            self.verify_instance(
-                "{}-{}".format(
-                    self.getClusterName(),
-                    machine_suffix
-                )
-            )
+            self.verify_instance("{}-{}".format(self.getClusterName(),
+                                                machine_suffix))
 
-    @parameterized.expand([
-        ("SINGLE", "1.3", ["m"]),
-        ("STANDARD", "1.3", ["m"]),
-        ("HA", "1.3", ["m-0", "m-1", "m-2"]),
-    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
-    def test_tez_on_image_version_1_3(self, configuration, dataproc_version, machine_suffixes):
-        self.createCluster(configuration, init_actions=None, dataproc_version=dataproc_version,
-                           properties='\'hadoop-env:HADOOP_CLASSPATH=${HADOOP_CLASSPATH}:/etc/tez/conf:/usr/lib/tez/*:/usr/lib/tez/lib/*\'')
+    @parameterized.expand(
+        [
+            ("SINGLE", "1.3", ["m"]),
+            ("STANDARD", "1.3", ["m"]),
+            ("HA", "1.3", ["m-0", "m-1", "m-2"]),
+        ],
+        testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_tez_on_image_version_1_3(self, configuration, dataproc_version,
+                                      machine_suffixes):
+        tez_classpath = "/etc/tez/conf:/usr/lib/tez/*:/usr/lib/tez/lib/*"
+        self.createCluster(
+            configuration,
+            init_actions=[],
+            dataproc_version=dataproc_version,
+            properties="'hadoop-env:HADOOP_CLASSPATH={}:{}'".format(
+                "${HADOOP_CLASSPATH}", tez_classpath))
         for machine_suffix in machine_suffixes:
-            self.verify_instance(
-                "{}-{}".format(
-                    self.getClusterName(),
-                    machine_suffix
-                )
-            )
+            self.verify_instance("{}-{}".format(self.getClusterName(),
+                                                machine_suffix))
 
 
 if __name__ == '__main__':
