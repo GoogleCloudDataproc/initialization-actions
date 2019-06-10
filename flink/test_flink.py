bf0c11e7fd27a428bb7aa8d847270dab32a6c710
Igor Dvorzhak <idv@google.com>
2019-06-10 11:10:25 -0700

    Fix Flink integration test

--- flink/test_flink.py
+++ flink/test_flink.py
@@ -1,10 +1,9 @@
+import os
 import unittest
 
 from parameterized import parameterized
-import os
-from integration_tests.dataproc_test_case import DataprocTestCase
 
-METADATA = 'flink-start-yarn-session=false'
+from integration_tests.dataproc_test_case import DataprocTestCase
 
 
 class FlinkTestCase(DataprocTestCase):
@@ -13,70 +12,58 @@ class FlinkTestCase(DataprocTestCase):
     TEST_SCRIPT_FILE_NAME = 'validate.sh'
 
     def verify_instance(self, name, yarn_session=True):
-        self.upload_test_file(os.path.join(
+        test_script_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
-            self.TEST_SCRIPT_FILE_NAME
-        ),
-            name)
+            self.TEST_SCRIPT_FILE_NAME)
+        self.upload_test_file(test_script_path, name)
         self.__run_test_file(name, yarn_session)
         self.remove_test_script(self.TEST_SCRIPT_FILE_NAME, name)
 
     def __run_test_file(self, name, yarn_session):
-        if yarn_session == True:
-            cmd = 'gcloud compute ssh {} -- "bash {}"'.format(
-                name,
-                self.TEST_SCRIPT_FILE_NAME
-            )
-        else:
-            cmd = 'gcloud compute ssh {} -- "bash {} {}"'.format(
-                name,
-                self.TEST_SCRIPT_FILE_NAME,
-                yarn_session
-            )
+        cmd = 'gcloud compute ssh {} --command="bash {} {}"'.format(
+            name, self.TEST_SCRIPT_FILE_NAME, yarn_session)
 
         ret_code, stdout, stderr = self.run_command(cmd)
-        self.assertEqual(ret_code, 0, "Failed to run test file. Error: {}".format(stderr))
+        self.assertEqual(ret_code, 0,
+                         "Failed to run test file. Error: {}".format(stderr))
 
-    @parameterized.expand([
-        ("SINGLE", "1.1", ["m"], METADATA),
-        ("STANDARD", "1.1", ["m"], None),
-        ("HA", "1.1", ["m-0", "m-1", "m-2"], None),
-        ("SINGLE", "1.2", ["m"], METADATA),
-        ("STANDARD", "1.2", ["m"], None),
-        ("HA", "1.2", ["m-0", "m-1", "m-2"], None),
-        ("SINGLE", "1.3", ["m"], METADATA),
-        ("STANDARD", "1.3", ["m"], None),
-        ("HA", "1.3", ["m-0", "m-1", "m-2"], None),
-    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
-    def test_flink(self, configuration, dataproc_version, machine_suffixes, metadata):
-        self.createCluster(configuration, self.INIT_ACTIONS, dataproc_version, metadata=metadata)
+    @parameterized.expand(
+        [
+            ("STANDARD", "1.1", ["m"]),
+            ("HA", "1.1", ["m-0", "m-1", "m-2"]),
+            ("STANDARD", "1.2", ["m"]),
+            ("HA", "1.2", ["m-0", "m-1", "m-2"]),
+            ("STANDARD", "1.3", ["m"]),
+            ("HA", "1.3", ["m-0", "m-1", "m-2"]),
+        ],
+        testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_flink(self, configuration, dataproc_version, machine_suffixes):
+        self.createCluster(configuration, self.INIT_ACTIONS, dataproc_version)
         for machine_suffix in machine_suffixes:
-            self.verify_instance(
-                "{}-{}".format(
-                    self.getClusterName(),
-                    machine_suffix
-                )
-            )
+            self.verify_instance("{}-{}".format(self.getClusterName(),
+                                                machine_suffix))
 
-    @parameterized.expand([
-        ("STANDARD", "1.1", ["m"], METADATA),
-        ("HA", "1.1", ["m-0", "m-1", "m-2"], METADATA),
-        ("STANDARD", "1.2", ["m"], METADATA),
-        ("HA", "1.2", ["m-0", "m-1", "m-2"], METADATA),
-        ("SINGLE", "1.3", ["m"], METADATA),
-        ("STANDARD", "1.3", ["m"], None),
-        ("HA", "1.3", ["m-0", "m-1", "m-2"], None),
-    ], testcase_func_name=DataprocTestCase.generate_verbose_test_name)
-    def test_flink_with_optional_metadata(self, configuration, dataproc_version, machine_suffixes, metadata):
-        self.createCluster(configuration, self.INIT_ACTIONS, dataproc_version, metadata=metadata)
+    @parameterized.expand(
+        [
+            ("STANDARD", "1.1", ["m"]),
+            ("HA", "1.1", ["m-0", "m-1", "m-2"]),
+            ("STANDARD", "1.2", ["m"]),
+            ("HA", "1.2", ["m-0", "m-1", "m-2"]),
+            ("SINGLE", "1.3", ["m"]),
+            ("STANDARD", "1.3", ["m"]),
+            ("HA", "1.3", ["m-0", "m-1", "m-2"]),
+        ],
+        testcase_func_name=DataprocTestCase.generate_verbose_test_name)
+    def test_flink_with_optional_metadata(self, configuration,
+                                          dataproc_version, machine_suffixes):
+        self.createCluster(configuration,
+                           self.INIT_ACTIONS,
+                           dataproc_version,
+                           metadata="flink-start-yarn-session=false")
         for machine_suffix in machine_suffixes:
-            self.verify_instance(
-                "{}-{}".format(
-                    self.getClusterName(),
-                    machine_suffix
-                ),
-                yarn_session=False
-            )
+            self.verify_instance("{}-{}".format(self.getClusterName(),
+                                                machine_suffix),
+                                 yarn_session=False)
 
 
 if __name__ == '__main__':
