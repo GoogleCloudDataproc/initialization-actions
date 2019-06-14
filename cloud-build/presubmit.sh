f5d23050d44f01657d6098d7789a0fb7c355bcfa
Esteban Lau <estebanlau@google.com>
2019-06-14 16:05:28 -0700

    fastunit5

--- cloud-build/presubmit.sh
+++ cloud-build/presubmit.sh
@@ -54,7 +54,7 @@ echo "Tests: ${TESTS_TO_RUN[*]}"
 
 # Run tests of the init actions that were modified
 if [[ $RUN_ALL_TESTS == true ]]; then
-  python3.5 -m unittest
+  python3.5 -m fastunit
 elif [[ ${#TESTS_TO_RUN[@]} != 0 ]]; then
-  python3.5 -m unittest "${TESTS_TO_RUN[@]}"
+  python3.5 -m fastunit "${TESTS_TO_RUN[@]}"
 fi
