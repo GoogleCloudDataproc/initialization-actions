2be6cd760bc4af7ff06ace4d37c5994ed1396853
Igor Dvorzhak <idv@google.com>
2019-07-01 23:51:12 -0700

    Increase timeout and refactor presubmit.sh

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -2,27 +2,24 @@
 
 set -euxo pipefail
 
-# Configure gcloud
-gcloud config set core/disable_prompts TRUE
-gcloud config set compute/region us-central1
-gcloud config set compute/zone us-central1-f
+configure_gcloud() {
+  gcloud config set core/disable_prompts TRUE
+  gcloud config set compute/region us-central1
+  gcloud config set compute/zone us-central1-f
+}
 
-# Install test dependencies
-pip3 install -r integration_tests/requirements.txt
+install_test_dependencies() {
+  pip3 install -r integration_tests/requirements.txt
+}
 
-git init
+initialize_git_repo() {
+  git init
 
-git remote add origin "https://github.com/GoogleCloudPlatform/dataproc-initialization-actions.git"
-git fetch origin master
+  git remote add origin "https://github.com/GoogleCloudPlatform/dataproc-initialization-actions.git"
+  git fetch origin master
 
-git reset origin/master
-
-# Stage files to track their history
-git add --all
-
-# Infer the files that changed
-mapfile -t CHANGED_FILES < <(git diff --cached origin/master --name-only)
-echo "Changed files: ${CHANGED_FILES[*]}"
+  git reset origin/master
+}
 
 # Determines whether a given string is a prefix string of any changed file name
 is_prefix() {
@@ -34,40 +31,61 @@ is_prefix() {
   return 1
 }
 
-# Determines init actions directories that were modified
-RUN_ALL_TESTS=false
-declare -a DIRECTORIES_TO_TEST
-for dir in */; do
-  # Run all tests if common directories were modified
-  if [[ $dir =~ ^(integration_tests/|util/|cloudbuild/)$ ]]; then
-    echo "All tests will be run: '$dir' was modified"
-    RUN_ALL_TESTS=true
-    break
+determine_tests_to_run() {
+  # Stage files to track their history
+  git add --all
+
+  # Infer the files that changed
+  mapfile -t CHANGED_FILES < <(git diff --cached origin/master --name-only)
+  echo "Changed files: ${CHANGED_FILES[*]}"
+
+  # Determines init actions directories that were modified
+  RUN_ALL_TESTS=false
+  local -a modified_dirs
+  for dir in */; do
+    # Run all tests if common directories were modified
+    if [[ $dir =~ ^(integration_tests/|util/|cloudbuild/)$ ]]; then
+      echo "All tests will be run: '$dir' was modified"
+      RUN_ALL_TESTS=true
+      return 0
+    fi
+    if is_prefix "$dir"; then
+      modified_dirs+=("$dir")
+    fi
+  done
+  echo "Modified directories: ${modified_dirs[*]}"
+
+  # Determines what tests in modified init action directories to run
+  declare -a TESTS_TO_RUN
+  for modified_dir in "${modified_dirs[@]}"; do
+    local tests_in_dir
+    if ! tests_in_dir=$(compgen -G "${modified_dir}test*.py"); then
+      echo "ERROR: presubmit failed - cannot find tests inside '${modified_dir}' directory"
+      exit 1
+    fi
+    local -a tests_array
+    mapfile -t tests_array < <(echo "${tests_in_dir}")
+    TESTS_TO_RUN+=("${tests_array[@]}")
+  done
+  echo "Tests: ${TESTS_TO_RUN[*]}"
+}
+
+run_tests() {
+  if [[ $RUN_ALL_TESTS == true ]]; then
+    # Run all init action tests
+    python3 -m fastunit
+  else
+    # Run tests of the init actions that were modified
+    python3 -m fastunit "${TESTS_TO_RUN[@]}"
   fi
-  if is_prefix "$dir"; then
-    DIRECTORIES_TO_TEST+=("$dir")
-  fi
-done
+}
 
-# Run all init action tests
-if [[ $RUN_ALL_TESTS == true ]]; then
-  python3 -m fastunit
-  exit $?
-fi
+main() {
+  configure_gcloud
+  install_test_dependencies
+  initialize_git_repo
+  determine_tests_to_run
+  run_tests
+}
 
-echo "Test directories: ${DIRECTORIES_TO_TEST[*]}"
-
-# Determines what tests in modified init action directories to run
-declare -a TESTS_TO_RUN
-for test_dir in "${DIRECTORIES_TO_TEST[@]}"; do
-  if ! tests=$(compgen -G "${test_dir}test*.py"); then
-    echo "ERROR: presubmit failed - cannot find tests inside '${test_dir}' directory"
-    exit 1
-  fi
-  mapfile -t tests_array < <(echo "${tests}")
-  TESTS_TO_RUN+=("${tests_array[@]}")
-done
-echo "Tests: ${TESTS_TO_RUN[*]}"
-
-# Run tests of the init actions that were modified
-python3 -m fastunit "${TESTS_TO_RUN[@]}"
+main
