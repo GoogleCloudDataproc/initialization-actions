c7bab49179a633f8e6f4c7201b6bb166becc38a8
Igor Dvorzhak <idv@google.com>
2019-07-10 11:05:05 -0700

    Delete prematurely merged "gcs_updater" folder (#589)

--- gcs_updater/push_to_gcs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/GoogleCloudPlatform/dataproc-initialization-actions.git
-cd dataproc-initialization-actions
-gsutil -m rsync -e -R -x '^.git/' . gs://dataproc-initialization-actions
-cd ..
-rm -rf dataproc-initialization-actions
