4eaab4db81a5a9701eade2c331f591faf8d7d57f
Dagang Wei <functicons@gmail.com>
2019-10-07 17:53:31 -0700

    Make PIP init action work for Ubuntu

--- python/pip-install.sh
+++ python/pip-install.sh
@@ -4,13 +4,28 @@ set -exo pipefail
 
 readonly PACKAGES=$(/usr/share/google/get_metadata_value attributes/PIP_PACKAGES || true)
 
+function install_pip() {
+  if command -v pip >/dev/null; then
+    echo "pip is already installed."
+  fi
+
+  if command -v easy_install >/dev/null; then
+    echo "Installing pip with easy_install..."
+    easy_install pip
+  fi
+
+  echo "Installing python-pip..."
+  apt update
+  apt install python-pip -y
+}
+
 function main() {
   if [[ -z "${PACKAGES}" ]]; then
-    echo "ERROR: Must specify PIP_PACKAGES metadata key" 
+    echo "ERROR: Must specify PIP_PACKAGES metadata key"
     exit 1
   fi
 
-  easy_install pip
+  install_pip
   pip install --upgrade ${PACKAGES}
 }
 
