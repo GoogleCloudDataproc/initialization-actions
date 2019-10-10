5376232d5f619bdb211ac74a0e3978fd53b54bd0
Igor Dvorzhak <idv@google.com>
2019-10-09 17:13:54 -0700

    Do not reset hard to detect failures

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -38,7 +38,7 @@ initialize_git_repo() {
   # Fetch all PRs to get history for PRs created from forked repos
   git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
 
-  git reset --hard "${COMMIT_SHA}"
+  git reset "${COMMIT_SHA}"
 
   git rebase origin/master
 }
