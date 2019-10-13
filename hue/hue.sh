85310da6698c6d4f2d23ffd864cb3c87beb9e074
Igor Dvorzhak <idv@google.com>
2019-10-12 21:32:17 -0700

    Fix Hue test

--- hue/hue.sh
+++ hue/hue.sh
@@ -12,9 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# This script installs HUE on a master node within a Google Cloud Dataproc cluster.
+# This script installs Hue on a master node within a Google Cloud Dataproc cluster.
 
 set -euxo pipefail
+
+# Determine the role of this node
+MASTER_HOSTNAME=$(/usr/share/google/get_metadata_value attributes/dataproc-master)
+readonly MASTER_HOSTNAME
 
 function random_string() {
   tr </dev/urandom -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1
@@ -49,72 +53,65 @@ function install_hue_and_configure() {
   local new_mysql_settings="engine=mysql\$1host=127.0.0.1\$2port=3306\$3user=hue\$4password=${hue_password}"
   local hadoop_conf_dir='/etc/hadoop/conf'
 
-  # Install hue
+  # Install Hue
   retry_apt_command "apt-get install -t $(lsb_release -sc)-backports -y hue" ||
-    err "Failed to install hue"
+    err "Failed to install Hue"
 
-  # Stop hue
-  systemctl stop hue || err "Hue stop action not performed"
+  # Stop Hue
+  systemctl stop hue
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/core-site.xml" \
     --name 'hadoop.proxyuser.hue.hosts' --value '*' \
     --create_if_absent \
-    --clobber ||
-    err 'Unable to set hadoop.proxyuser.hue.hosts'
+    --clobber
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/core-site.xml" \
     --name 'hadoop.proxyuser.hue.groups' --value '*' \
     --create_if_absent \
-    --clobber ||
-    err 'Unable to set hadoop.proxyuser.hue.groups'
+    --clobber
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/hdfs-site.xml" \
     --name 'dfs.webhdfs.enabled' --value 'true' \
     --create_if_absent \
-    --clobber ||
-    err 'Unable to set dfs.webhdfs.enabled'
+    --clobber
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/hdfs-site.xml" \
     --name 'hadoop.proxyuser.hue.hosts' --value '*' \
     --create_if_absent \
-    --clobber ||
-    err 'Unable to set hadoop.proxyuser.hue.hosts'
+    --clobber
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/hdfs-site.xml" \
     --name 'hadoop.proxyuser.hue.groups' --value '*' \
     --create_if_absent \
-    --clobber ||
-    err 'Unable to set hadoop.proxyuser.hue.groups'
+    --clobber
 
   cat >>/etc/hue/conf/hue.ini <<EOF
 # Defaults to ${HADOOP_MR1_HOME} or /usr/lib/hadoop-0.20-mapreduce
 hadoop_mapred_home=/usr/lib/hadoop-mapreduce
 EOF
 
-  # If oozie installed, add hue as proxy user for oozie
+  # If Oozie installed, add Hue as proxy user for Oozie
   if [[ -f /etc/oozie/conf/oozie-site.xml ]]; then
     bdconfig set_property \
       --configuration_file '/etc/oozie/conf/oozie-site.xml' \
       --name 'oozie.service.ProxyUserService.proxyuser.hue.hosts' --value '*' \
       --create_if_absent \
-      --clobber ||
-      err 'Unable to set hadoop.proxyuser.hue.groups'
+      --clobber
 
     bdconfig set_property \
       --configuration_file '/etc/oozie/conf/oozie-site.xml' \
       --name 'oozie.service.ProxyUserService.proxyuser.hue.groups' --value '*' \
       --create_if_absent \
-      --clobber ||
-      err 'Unable to set hadoop.proxyuser.hue.groups'
+      --clobber
 
-    systemctl restart oozie || err "Unable to restart oozie"
+    systemctl restart oozie
   else
-    echo "oozie not installed, skipped configuring hue as an user proxy"
+    echo "Oozie not installed, skipped configuring Hue as an user proxy"
   fi
 
   # Fix webhdfs_url
@@ -128,15 +125,13 @@ EOF
 
   # Make hive warehouse directory
   local warehause_dir="/user/hive/warehouse"
-  hdfs dfs -mkdir "${warehause_dir}" ||
-    hdfs dfs -stat "${warehause_dir}" ||
-    err "Unable to create '${warehause_dir}' folder"
+  hdfs dfs -mkdir "${warehause_dir}" || hdfs dfs -stat "${warehause_dir}"
 
   bdconfig set_property \
     --configuration_file '/etc/hue/conf/hive-site.xml' \
     --create_if_absent \
-    --clobber \
-    --name 'hive.metastore.warehouse.dir' --value '/user/hive/warehouse'
+    --name 'hive.metastore.warehouse.dir' --value '/user/hive/warehouse' \
+    --clobber
 
   # Configure Desktop Database to use mysql
   perl -i -0777 -pe "s/${old_mysql_settings}/${new_mysql_settings}/" /etc/hue/conf/hue.ini
@@ -144,7 +139,7 @@ EOF
   # Comment out sqlite3 configuration
   sed -i 's/engine=sqlite3/## engine=sqlite3/' /etc/hue/conf/hue.ini
 
-  # Set database name to hue
+  # Set database name to Hue
   sed -i 's/name=\/var\/lib\/hue\/desktop.db/name=hue/' /etc/hue/conf/hue.ini
 
   # Disable logging and set random secret key
@@ -152,37 +147,25 @@ EOF
   sed -i "s/secret_key=.*/secret_key=$(random_string)/" /etc/hue/conf/hue.ini
   set -x
 
-  # Create database, give hue user permissions
+  # Create database, give Hue user permissions
   mysql -u root -proot-password -e "
       CREATE DATABASE hue;
       CREATE USER 'hue'@'localhost' IDENTIFIED BY '${hue_password}';
-      GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'localhost';" ||
-    err "Unable to create database"
-
-  # Restart mysql
-  systemctl restart mysql || err "Unable to restart mysql"
+      GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'localhost';"
 
   # Hue creates all needed tables
-  /usr/lib/hue/build/env/bin/hue syncdb --noinput || err "Database sync failed"
-  /usr/lib/hue/build/env/bin/hue migrate || err "Data migration failed"
+  /usr/lib/hue/build/env/bin/hue syncdb --noinput
+  /usr/lib/hue/build/env/bin/hue migrate
 
   # Restart servers
-  systemctl restart hadoop-hdfs-namenode || err "Unable to restart hadoop-hdfs-namenode"
-  systemctl restart hadoop-yarn-resourcemanager ||
-    err "Unable to restart hadoop-yarn-resourcemanager"
-  systemctl restart hue || err "Unable to restart hue"
-  systemctl restart mysql || err "Unable to restart mysql"
+  systemctl restart hadoop-hdfs-namenode hadoop-yarn-resourcemanager
+
+  # Start Hue
+  systemctl start hue
 }
 
-function main() {
-  # Determine the role of this node
-  local master_hostname
-  master_hostname=$(/usr/share/google/get_metadata_value attributes/dataproc-master)
-  # Only run on the master node ("0"-master in HA mode) of the cluster
-  if [[ "${HOSTNAME}" == "${master_hostname}" ]]; then
-    update_apt_get || err "Unable to update apt-get"
-    install_hue_and_configure || err "Hue install process failed"
-  fi
-}
-
-main
+# Only run on the master node ("0"-master in HA mode) of the cluster
+if [[ "${HOSTNAME}" == "${MASTER_HOSTNAME}" ]]; then
+  update_apt_get || err "Unable to update apt-get"
+  install_hue_and_configure || err "Hue install process failed"
+fi
