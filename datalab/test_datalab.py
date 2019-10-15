7398a48e7d07da4250c856d1c8d5a030710f0c45
Igor Dvorzhak <idv@google.com>
2019-10-15 12:55:09 -0700

    Fix Datalab init actions tests (#650)

--- datalab/test_datalab.py
+++ datalab/test_datalab.py
@@ -8,9 +8,7 @@ from integration_tests.dataproc_test_case import DataprocTestCase
 class DatalabTestCase(DataprocTestCase):
     COMPONENT = 'datalab'
     INIT_ACTIONS = ['datalab/datalab.sh']
-    PYTHON_3_INIT_ACTIONS = [
-        'conda/bootstrap-conda.sh', 'conda/install-conda-env.sh'
-    ]
+    PYTHON_3_INIT_ACTIONS = ['conda/bootstrap-conda.sh']
 
     def verify_instance(self, name):
         self.assert_instance_command(
@@ -19,14 +17,11 @@ class DatalabTestCase(DataprocTestCase):
 
     @parameterized.expand(
         [
-            ("STANDARD", "1.1", ["m"], "python2"),
-            ("STANDARD", "1.1", ["m"], "python3"),
             ("STANDARD", "1.2", ["m"], "python2"),
             ("STANDARD", "1.2", ["m"], "python3"),
             ("STANDARD", "1.3", ["m"], "python2"),
             ("STANDARD", "1.3", ["m"], "python3"),
-            ("STANDARD", "1.4", ["m"], "python2"),
-            ("STANDARD", "1.4", ["m"], "python3"),
+            ("STANDARD", "1.4", ["m"], "python3-default"),
         ],
         testcase_func_name=DataprocTestCase.generate_verbose_test_name)
     def test_datalab(self, configuration, dataproc_version, machine_suffixes,
@@ -35,7 +30,6 @@ class DatalabTestCase(DataprocTestCase):
         metadata = 'INIT_ACTIONS_REPO={}'.format(self.INIT_ACTIONS_REPO)
         if python == "python3":
             init_actions = self.PYTHON_3_INIT_ACTIONS + init_actions
-            metadata += ',CONDA_PACKAGES="python==3.5"'
 
         self.createCluster(configuration,
                            init_actions,
