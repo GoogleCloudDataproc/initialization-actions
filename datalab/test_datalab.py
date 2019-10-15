9a474b81a449e0fdbb639a72e56d352209d5fad7
Igor Dvorzhak <idv@google.com>
2019-10-15 07:56:11 -0700

    Fix Datalab init actions tests

--- datalab/test_datalab.py
+++ datalab/test_datalab.py
@@ -19,14 +19,11 @@ class DatalabTestCase(DataprocTestCase):
 
     @parameterized.expand(
         [
-            ("STANDARD", "1.1", ["m"], "python2"),
-            ("STANDARD", "1.1", ["m"], "python3"),
             ("STANDARD", "1.2", ["m"], "python2"),
             ("STANDARD", "1.2", ["m"], "python3"),
             ("STANDARD", "1.3", ["m"], "python2"),
             ("STANDARD", "1.3", ["m"], "python3"),
-            ("STANDARD", "1.4", ["m"], "python2"),
-            ("STANDARD", "1.4", ["m"], "python3"),
+            ("STANDARD", "1.4", ["m"], "python3-default"),
         ],
         testcase_func_name=DataprocTestCase.generate_verbose_test_name)
     def test_datalab(self, configuration, dataproc_version, machine_suffixes,
@@ -35,7 +32,7 @@ class DatalabTestCase(DataprocTestCase):
         metadata = 'INIT_ACTIONS_REPO={}'.format(self.INIT_ACTIONS_REPO)
         if python == "python3":
             init_actions = self.PYTHON_3_INIT_ACTIONS + init_actions
-            metadata += ',CONDA_PACKAGES="python==3.5"'
+            metadata += ',CONDA_PACKAGES="python=3"'
 
         self.createCluster(configuration,
                            init_actions,
