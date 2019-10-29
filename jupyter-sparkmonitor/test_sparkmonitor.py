fc336d99aa7088638d47bef6748f94c2687b1134
Roderick Yao <roderickyao@google.com>
2019-10-29 17:31:12 -0400

    Simplified the script without downloading the repo.

--- jupyter-sparkmonitor/test_sparkmonitor.py
+++ jupyter-sparkmonitor/test_sparkmonitor.py
@@ -23,48 +23,16 @@ class JupyterTestCase(DataprocTestCase):
             ("STANDARD", "1.4", ["m"]),
         ],
         testcase_func_name=DataprocTestCase.generate_verbose_test_name)
-    def test_jupyter(self, configuration, dataproc_version, machine_suffixes):
-        metadata = 'INIT_ACTIONS_REPO={}'.format(self.INIT_ACTIONS_REPO)
+    def test_jupyter_sparkmonitor(self, configuration, dataproc_version, machine_suffixes):
         self.createCluster(configuration,
                            self.INIT_ACTIONS,
                            dataproc_version,
-                           metadata=metadata,
                            timeout_in_minutes=15,
                            machine_type="n1-standard-2")
 
         for machine_suffix in machine_suffixes:
             self.verify_instance(
                 "{}-{}".format(self.getClusterName(), machine_suffix), "8123")
-
-    @parameterized.expand(
-        [
-            ("SINGLE", "1.1", ["m"]),
-            ("STANDARD", "1.1", ["m"]),
-            ("SINGLE", "1.2", ["m"]),
-            ("STANDARD", "1.2", ["m"]),
-            ("SINGLE", "1.3", ["m"]),
-            ("STANDARD", "1.3", ["m"]),
-        ],
-        testcase_func_name=DataprocTestCase.generate_verbose_test_name)
-    def test_jupyter_with_metadata(self, configuration, dataproc_version,
-                                   machine_suffixes):
-        jupyter_port = "8125"
-
-        metadata = 'INIT_ACTIONS_REPO={}'.format(self.INIT_ACTIONS_REPO)
-        metadata += ',JUPYTER_PORT={},JUPYTER_CONDA_PACKAGES={}'.format(
-            jupyter_port, "numpy:pandas:scikit-learn")
-
-        self.createCluster(configuration,
-                           self.INIT_ACTIONS,
-                           dataproc_version,
-                           metadata=metadata,
-                           timeout_in_minutes=15,
-                           machine_type="n1-standard-2")
-
-        for machine_suffix in machine_suffixes:
-            self.verify_instance(
-                "{}-{}".format(self.getClusterName(), machine_suffix),
-                jupyter_port)
 
 
 if __name__ == '__main__':
