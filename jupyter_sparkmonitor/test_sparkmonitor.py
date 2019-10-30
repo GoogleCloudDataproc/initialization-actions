7d5f456d2bba17a346eaf73ee244f90c227087fd
roderickyao <yhqs540@gmail.com>
2019-10-30 19:53:33 -0400

    Adding tests

---
+++ jupyter_sparkmonitor/test_sparkmonitor.py
@@ -0,0 +1,34 @@
+import pkg_resources
+from absl.testing import absltest
+from absl.testing import parameterized
+from integration_tests.dataproc_test_case import DataprocTestCase
+
+
+class JupyterTestCase(DataprocTestCase):
+    COMPONENT = 'jupyter_sparkmonitor'
+    INIT_ACTIONS = ['jupyter_sparkmonitor/sparkmonitor.sh']
+
+    def verify_instance(self, name, jupyter_port):
+        verify_cmd = "curl {} -L {}:{} | grep 'Jupyter Notebook'".format(
+            "--retry 10 --retry-delay 10 --retry-connrefused", name,
+            jupyter_port)
+        self.assert_instance_command(name, verify_cmd)
+
+    @parameterized.parameters(
+        ("SINGLE", ["m"]),
+        ("STANDARD", ["m"]),
+    )
+    def test_sparkmonitor(self, configuration, dataproc_version, machine_suffixes):
+        self.createCluster(configuration,
+                           self.INIT_ACTIONS,
+                           dataproc_version,
+                           timeout_in_minutes=15,
+                           machine_type="n1-standard-2")
+
+        for machine_suffix in machine_suffixes:
+            self.verify_instance(
+                "{}-{}".format(self.getClusterName(), machine_suffix), "8123")
+
+
+if __name__ == '__main__':
+    absltest.main()
