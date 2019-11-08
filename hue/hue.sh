5f60b64628c97a62c6e646ff6fc372d2ef1720da
Igor Dvorzhak <idv@google.com>
2019-11-08 14:08:24 -0800

    Fix Hue init action with Datapoc 1.5

--- hue/hue.sh
+++ hue/hue.sh
@@ -58,39 +58,45 @@ function install_hue_and_configure() {
     err "Failed to install Hue"
 
   # Stop Hue
-  systemctl stop hue
+  systemctl stop hue || err "Hue stop action not performed"
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/core-site.xml" \
     --name 'hadoop.proxyuser.hue.hosts' --value '*' \
     --create_if_absent \
-    --clobber
+    --clobber ||
+    err 'Unable to set hadoop.proxyuser.hue.hosts'
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/core-site.xml" \
     --name 'hadoop.proxyuser.hue.groups' --value '*' \
     --create_if_absent \
-    --clobber
+    --clobber ||
+    err 'Unable to set hadoop.proxyuser.hue.groups'
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/hdfs-site.xml" \
     --name 'dfs.webhdfs.enabled' --value 'true' \
     --create_if_absent \
-    --clobber
+    --clobber ||
+    err 'Unable to set dfs.webhdfs.enabled'
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/hdfs-site.xml" \
     --name 'hadoop.proxyuser.hue.hosts' --value '*' \
     --create_if_absent \
-    --clobber
+    --clobber ||
+    err 'Unable to set hadoop.proxyuser.hue.hosts'
 
   bdconfig set_property \
     --configuration_file "${hadoop_conf_dir}/hdfs-site.xml" \
     --name 'hadoop.proxyuser.hue.groups' --value '*' \
     --create_if_absent \
-    --clobber
+    --clobber ||
+    err 'Unable to set hadoop.proxyuser.hue.groups'
 
-  cat >>/etc/hue/conf/hue.ini <<EOF
+  cat >>/etc/hue/conf/hue.ini <<'EOF'
+
 # Defaults to ${HADOOP_MR1_HOME} or /usr/lib/hadoop-0.20-mapreduce
 hadoop_mapred_home=/usr/lib/hadoop-mapreduce
 EOF
@@ -101,23 +107,49 @@ EOF
       --configuration_file '/etc/oozie/conf/oozie-site.xml' \
       --name 'oozie.service.ProxyUserService.proxyuser.hue.hosts' --value '*' \
       --create_if_absent \
-      --clobber
+      --clobber ||
+      err 'Unable to set hadoop.proxyuser.hue.groups'
 
     bdconfig set_property \
       --configuration_file '/etc/oozie/conf/oozie-site.xml' \
       --name 'oozie.service.ProxyUserService.proxyuser.hue.groups' --value '*' \
       --create_if_absent \
-      --clobber
+      --clobber ||
+      err 'Unable to set hadoop.proxyuser.hue.groups'
 
-    systemctl restart oozie
+    systemctl restart oozie || err "Unable to restart oozie"
   else
     echo "Oozie not installed, skipped configuring Hue as an user proxy"
   fi
 
-  # Fix webhdfs_url
+  # Uncomment supported interpreters
+  local interpreters=(
+    hive sql spark pyspark r jar py text markdown pig java spark2 mapreduce solr druid impala
+    sqoop1 distcp shell presto
+  )
+  for interpreter in "${interpreters[@]}"; do
+    sed -i "/^ *# \[\[\[${interpreter}\]\]\]/,/^ *$/s/^\( *\)# \?/\1/" /etc/hue/conf/hue.ini
+  done
+
+  # Configure webhdfs_url
   sed -i "s/${old_hdfs_url}/${new_hdfs_url}/" /etc/hue/conf/hue.ini
-  # Uncomment every line containing localhost, replacing localhost with the FQDN
-  sed -i "s/#*\([^#]*=.*\)localhost/\1$(hostname --fqdn)/" /etc/hue/conf/hue.ini
+
+  # Replace localhost in all values with FQDN
+  sed -i "s/\(.*=.*\)localhost/\1$(hostname --fqdn)/" /etc/hue/conf/hue.ini
+
+  # Uncomment necessary config keys
+  local config_keys=(
+    default_from_email django_server_email resourcemanager_host resourcemanager_api_url
+    proxy_api_url history_server_api_url spark_history_server_url hive_server_host livy_server_url
+    sql_server_host server_url host_ports rest_url oozie_url whitelist hbase_clusters solr_url
+    ensemble
+  )
+  for config_key in "${config_keys[@]}"; do
+    sed -i "s/#[# ]*\(${config_key} \?=\)/\1/" /etc/hue/conf/hue.ini
+  done
+
+  # Uncomment Sentry configuration
+  sed -i "s/#[# ]*\(hostname \?= \?$(hostname --fqdn)\)/\1/" /etc/hue/conf/hue.ini
 
   # Comment out any duplicate resourcemanager_api_url fields after the first one
   sed -i '0,/resourcemanager_api_url/! s/resourcemanager_api_url/## resourcemanager_api_url/' \
@@ -139,7 +171,7 @@ EOF
   # Comment out sqlite3 configuration
   sed -i 's/engine=sqlite3/## engine=sqlite3/' /etc/hue/conf/hue.ini
 
-  # Set database name to Hue
+  # Set database name to 'hue'
   sed -i 's/name=\/var\/lib\/hue\/desktop.db/name=hue/' /etc/hue/conf/hue.ini
 
   # Disable logging and set random secret key
@@ -147,11 +179,12 @@ EOF
   sed -i "s/secret_key=.*/secret_key=$(random_string)/" /etc/hue/conf/hue.ini
   set -x
 
-  # Create database, give Hue user permissions
+  # Create a database, give 'hue' user permissions
   mysql -u root -proot-password -e "
       CREATE DATABASE hue;
       CREATE USER 'hue'@'localhost' IDENTIFIED BY '${hue_password}';
-      GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'localhost';"
+      GRANT ALL PRIVILEGES ON hue.* TO 'hue'@'localhost';" ||
+    err "Unable to create database"
 
   # Hue creates all needed tables
   /usr/lib/hue/build/env/bin/hue syncdb --noinput
