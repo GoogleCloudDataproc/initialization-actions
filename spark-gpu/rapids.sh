d5924c6611eb8a490bc51ad970985770db28e1ba
Dong Meng <mengdong0427@gmail.com>
2019-11-26 23:16:39 -0800

    add rapids spark and cudf version

--- spark-gpu/rapids.sh
+++ spark-gpu/rapids.sh
@@ -13,6 +13,14 @@ readonly DEAFULT_GCS_BUCKET=gs://my-bucket
 readonly GCS_BUCKET="$(/usr/share/google/get_metadata_value attributes/GCS_BUCKET ||
   echo ${DEAFULT_GCS_BUCKET})"
 
+readonly DEAFULT_RAPIDS_SPARK_VERSION=2.x-1.0.0-Beta3
+readonly RAPIDS_SPARK_VERSION="$(/usr/share/google/get_metadata_value attributes/RAPIDS_SPARK_VERSION ||
+  echo ${DEAFULT_RAPIDS_SPARK_VERSION})"
+
+readonly DEAFULT_RAPIDS_CUDF_VERSION=0.9.2-cuda10
+readonly RAPIDS_CUDF_VERSION="$(/usr/share/google/get_metadata_value attributes/RAPIDS_CUDF_VERSION ||
+  echo ${DEAFULT_RAPIDS_CUDF_VERSION})"
+
 echo "Cloning RAPIDS initialization action from '${INIT_ACTIONS_REPO}' ..."
 RAPIDS_INIT_ACTION_DIR=$(mktemp -d -t rapids-init-action-XXXX)
 readonly RAPIDS_INIT_ACTION_DIR
@@ -29,10 +37,10 @@ if [[ "${ROLE}" != 'Master' ]]; then
   # Ensure we have GPU drivers installed.
   "${RAPIDS_INIT_ACTION_DIR}/internal/install-gpu-driver.sh"
 else
-  gsutil cp ${GCS_BUCKET}/xgboost4j-spark_2.11-1.0.0-Beta2.jar /usr/lib/spark/python/lib/
-  gsutil cp ${GCS_BUCKET}/xgboost4j-spark_2.11-1.0.0-Beta2.jar /usr/lib/spark/jars/
-  gsutil cp ${GCS_BUCKET}/xgboost4j_2.11-1.0.0-Beta2.jar /usr/lib/spark/jars/
-  gsutil cp ${GCS_BUCKET}/cudf-0.9.1-cuda10.jar /usr/lib/spark/jars/
+  gsutil cp ${GCS_BUCKET}/xgboost4j-spark_${RAPIDS_SPARK_VERSION}.jar /usr/lib/spark/python/lib/
+  gsutil cp ${GCS_BUCKET}/xgboost4j-spark_${RAPIDS_SPARK_VERSION}.jar /usr/lib/spark/jars/
+  gsutil cp ${GCS_BUCKET}/xgboost4j_${RAPIDS_SPARK_VERSION}.jar /usr/lib/spark/jars/
+  gsutil cp ${GCS_BUCKET}/cudf-${RAPIDS_CUDF_VERSION}.jar /usr/lib/spark/jars/
 fi
 
 
