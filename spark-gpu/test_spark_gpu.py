4ccfa7e85dfe338131b34a37c28abea8ffded72d
Dong Meng <mengdong0427@gmail.com>
2020-01-24 11:43:05 +0800

    add test cases for spark-gpu

---
+++ spark-gpu/test_spark_gpu.py
@@ -0,0 +1,27 @@
+from absl.testing import absltest
+from absl.testing import parameterized
+
+from integration_tests.dataproc_test_case import DataprocTestCase
+
+
+class SparkGPUTestCase(DataprocTestCase):
+    COMPONENT = 'spark-gpu'
+    INIT_ACTIONS = ['spark-gpu/rapids.sh']
+
+    @parameterized.parameters(
+        ("STANDARD", ["m", "w-0"], 'type=nvidia-tesla-t4,count=1'), )
+    def test_spark_gpu(self, configuration, machine_suffixes, worker_accelerator):
+        init_actions = self.INIT_ACTIONS
+
+        self.createCluster(
+            configuration,
+            init_actions,
+            beta=True,
+            worker_accelerator=worker_accelerator)
+        for machine_suffix in machine_suffixes:
+            self.verify_instance("{}-{}".format(self.getClusterName(),
+                                                machine_suffix))
+
+
+if __name__ == '__main__':
+    absltest.main()
