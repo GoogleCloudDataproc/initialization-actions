10c9c4a145b561b9d79fe580c226d3c907dc820c
Igor Dvorzhak <idv@google.com>
2020-02-19 10:55:03 -0800

    Minor #cleanup (#299)

--- util/src/main/java/com/google/cloud/hadoop/util/CredentialFactory.java
+++ util/src/main/java/com/google/cloud/hadoop/util/CredentialFactory.java
@@ -166,8 +166,8 @@ public class CredentialFactory {
                 .setRequestInitializer(getRequestInitializer());
         request.put("assertion", assertion);
         return request.execute();
-      } catch (GeneralSecurityException exception) {
-        throw (IOException) new IOException().initCause(exception);
+      } catch (GeneralSecurityException e) {
+        throw new IOException("Failed to refresh token", e);
       }
     }
   }
