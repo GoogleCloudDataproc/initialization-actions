d6ed8e923f73dd36b0798b506e635464faf1aae7
DougM <mengdong@users.noreply.github.com>
2020-03-12 15:40:35 -0700

    Add Spark XGBoost4J to RAPIDS init action (#627)
    Co-authored-by: Igor Dvorzhak <idv@google.com>

--- rapids/rapids.sh
+++ rapids/rapids.sh
@@ -11,9 +11,14 @@ function get_metadata_attribute() {
 readonly ROLE=$(/usr/share/google/get_metadata_value attributes/dataproc-role)
 readonly MASTER=$(/usr/share/google/get_metadata_value attributes/dataproc-master)
 
+readonly RUNTIME=$(get_metadata_attribute 'rapids-runtime' 'DASK')
 readonly RUN_WORKER_ON_MASTER=$(get_metadata_attribute 'dask-cuda-worker-on-master' 'true')
 
 readonly CUDA_VERSION=$(get_metadata_attribute 'cuda-version' '10.0')
+readonly CUDF_VERSION=$(get_metadata_attribute 'cudf-version' '0.9.2')
+
+readonly RAPIDS_VERSION=$(get_metadata_attribute 'rapids-version' '1.0.0-Beta4')
+readonly RAPIDS_SPARK_VERSION=$(get_metadata_attribute 'rapids-spark-version' '2.x')
 
 readonly DASK_LAUNCHER='/usr/local/bin/dask-launcher.sh'
 readonly DASK_SERVICE='dask-cluster'
@@ -22,6 +27,37 @@ readonly RAPIDS_ENV_BIN="/opt/conda/anaconda/envs/${RAPIDS_ENV}/bin"
 
 BUILD_DIR=$(mktemp -d -t rapids-init-action-XXXX)
 readonly BUILD_DIR
+
+function execute_with_retries() {
+  local -r cmd=$1
+  for ((i = 0; i < 10; i++)); do
+    if eval "$cmd"; then
+      return 0
+    fi
+    sleep 5
+  done
+  return 1
+}
+
+function install_xgboost4j() {
+  local -r repo_url='https://repo1.maven.org/maven2/ai/rapids'
+
+  if [[ "${CUDA_VERSION}" == "10.0" ]]; then
+    local -r cudf_cuda_version="10"
+  else
+    local -r cudf_cuda_version="${CUDA_VERSION//\./-}"
+  fi
+
+  wget -nv --timeout=30 --tries=5 --retry-connrefused \
+    "${repo_url}/xgboost4j-spark_${RAPIDS_SPARK_VERSION}/${RAPIDS_VERSION}/xgboost4j-spark_${RAPIDS_SPARK_VERSION}-${RAPIDS_VERSION}.jar" \
+    -P /usr/lib/spark/jars/
+  wget -nv --timeout=30 --tries=5 --retry-connrefused \
+    "${repo_url}/xgboost4j_${RAPIDS_SPARK_VERSION}/${RAPIDS_VERSION}/xgboost4j_${RAPIDS_SPARK_VERSION}-${RAPIDS_VERSION}.jar" \
+    -P /usr/lib/spark/jars/
+  wget -nv --timeout=30 --tries=5 --retry-connrefused \
+    "${repo_url}/cudf/${CUDF_VERSION}/cudf-${CUDF_VERSION}-cuda${cudf_cuda_version}.jar" \
+    -P /usr/lib/spark/jars/
+}
 
 function create_conda_env() {
   echo "Create RAPIDS Conda environment..."
@@ -53,7 +89,7 @@ EOF
 }
 
 function install_conda_kernel() {
-  /opt/conda/anaconda/bin/conda install -y nb_conda_kernels
+  conda install -y nb_conda_kernels
   # Restart Jupyter service to pickup RAPIDS environment.
   service jupyter restart || true
 }
@@ -97,15 +133,22 @@ EOF
 }
 
 function main() {
-  create_conda_env
-  if [[ "${ROLE}" == "Master" ]]; then
-    install_conda_kernel
+  if [[ "${RUNTIME}" == "DASK" ]]; then
+    create_conda_env
+    if [[ "${ROLE}" == "Master" ]]; then
+      install_conda_kernel
+    fi
+    install_systemd_dask_service
+    echo "Starting Dask cluster..."
+    systemctl start "${DASK_SERVICE}"
+    echo "RAPIDS initialized with Dask runtime"
+  elif [[ "${RUNTIME}" == "SPARK" ]]; then
+    install_xgboost4j
+    echo "RAPIDS initialized with Spark runtime"
+  else
+    echo "Unsupported RAPIDS Runtime: ${RUNTIME}"
+    exit 1
   fi
-  install_systemd_dask_service
-
-  echo "Starting Dask cluster..."
-  systemctl start "${DASK_SERVICE}"
-  echo "Dask cluster instantiation successful"
 }
 
 main
--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -10,42 +10,69 @@ from integration_tests.dataproc_test_case import DataprocTestCase
 class RapidsTestCase(DataprocTestCase):
     COMPONENT = 'rapids'
     INIT_ACTIONS = ['gpu/install_gpu_driver.sh', 'rapids/rapids.sh']
+
     GPU_P100 = 'type=nvidia-tesla-p100'
 
-    TEST_SCRIPT_FILE_NAME = 'verify_rapids.py'
+    DASK_TEST_SCRIPT_FILE_NAME = 'verify_rapids_dask.py'
 
-    def verify_instance(self, name):
+    def verify_dask_instance(self, name):
         self.upload_test_file(
             os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                         self.TEST_SCRIPT_FILE_NAME), name)
-        self._run_test_script(name)
-        self.remove_test_script(self.TEST_SCRIPT_FILE_NAME, name)
+                         self.DASK_TEST_SCRIPT_FILE_NAME), name)
+        self._run_dask_test_script(name)
+        self.remove_test_script(self.DASK_TEST_SCRIPT_FILE_NAME, name)
 
-    def _run_test_script(self, name):
+    def _run_dask_test_script(self, name):
         verify_cmd = "/opt/conda/anaconda/envs/RAPIDS/bin/python {}".format(
-            self.TEST_SCRIPT_FILE_NAME)
+            self.DASK_TEST_SCRIPT_FILE_NAME)
         self.assert_instance_command(name, verify_cmd)
 
-    @parameterized.parameters(("STANDARD", ["m"], GPU_P100, GPU_P100))
-    def test_rapids(self, configuration, machine_suffixes, master_accelerator,
-                    worker_accelerator):
+    def verify_spark_instance(self, name):
+        self.assert_instance_command(name, "nvidia-smi")
+
+    @parameterized.parameters(("STANDARD", ["m"], GPU_P100, False),
+                              ("STANDARD", ["m"], GPU_P100, True))
+    def test_rapids_dask(self, configuration, machine_suffixes, accelerator,
+                         dask_cuda_worker_on_master):
         # Init action supported on Dataproc 1.3+
         if self.getImageVersion() < pkg_resources.parse_version("1.3"):
             return
 
+        metadata = 'gpu-driver-provider=NVIDIA,rapids-runtime=DASK'
+        if dask_cuda_worker_on_master:
+            master_accelerator = accelerator
+        else:
+            metadata += ',dask-cuda-worker-on-master=false'
+            master_accelerator = None
         self.createCluster(configuration,
                            self.INIT_ACTIONS,
-                           metadata='gpu-driver-provider=NVIDIA',
-                           beta=True,
+                           metadata=metadata,
                            master_accelerator=master_accelerator,
-                           worker_accelerator=worker_accelerator,
+                           worker_accelerator=accelerator,
                            optional_components='ANACONDA',
                            machine_type='n1-standard-2',
                            timeout_in_minutes=20)
 
         for machine_suffix in machine_suffixes:
-            self.verify_instance("{}-{}".format(self.getClusterName(),
-                                                machine_suffix))
+            self.verify_dask_instance("{}-{}".format(self.getClusterName(),
+                                                     machine_suffix))
+
+    @parameterized.parameters(("STANDARD", ["w-0"], GPU_P100))
+    def test_rapids_spark(self, configuration, machine_suffixes, accelerator):
+        # Init action supported on Dataproc 1.3+
+        if self.getImageVersion() < pkg_resources.parse_version("1.3"):
+            return
+
+        self.createCluster(
+            configuration,
+            self.INIT_ACTIONS,
+            metadata='gpu-driver-provider=NVIDIA,rapids-runtime=SPARK',
+            machine_type='n1-standard-2',
+            worker_accelerator=accelerator,
+            timeout_in_minutes=20)
+        for machine_suffix in machine_suffixes:
+            self.verify_spark_instance("{}-{}".format(self.getClusterName(),
+                                                      machine_suffix))
 
 
 if __name__ == '__main__':
