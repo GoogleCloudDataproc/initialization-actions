fa7786399c2d3793673e47d3fa7e71c4116b0f99
Esteban Lau <estebanlau@google.com>
2020-04-13 10:12:49 -0700

    Fix path to run-all-tests.sh

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -106,7 +106,7 @@ main() {
   initialize_git_repo
   if [[ $RUN_ALL_TESTS=="true" ]]; then
     TESTS_TO_RUN=(":DataprocInitActionsTestSuite")
-    bash run-all-tests.sh
+    bash cloudbuild/run-all-tests.sh
   else
     determine_tests_to_run
     #determine_tests_to_run
