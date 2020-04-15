b056fd4af90156db35609a47fde71074c300e531
Igor Dvorzhak <idv@google.com>
2020-04-15 07:39:40 -0700

    [python/conda-install.sh] Support Conda channels configuration (#745)

--- python/conda-install.sh
+++ python/conda-install.sh
@@ -2,9 +2,19 @@
 
 set -exo pipefail
 
+# Space-separated list of Conda channels to add before installing packages
+readonly CHANNELS=$(/usr/share/google/get_metadata_value attributes/CONDA_CHANNELS || true)
+
+# Space-separated list of Conda packages to install
 readonly PACKAGES=$(/usr/share/google/get_metadata_value attributes/CONDA_PACKAGES || true)
 
 function main() {
+  if [[ -n "${CHANNELS}" ]]; then
+    for channel in ${CHANNELS}; do
+      conda config --add channels "${channel}"
+    done
+  fi
+
   if [[ -z "${PACKAGES}" ]]; then
     echo "ERROR: Must specify CONDA_PACKAGES metadata key"
     exit 1
