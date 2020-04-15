328e7b1dd33f3cb3b18fef2964d952c568d7a228
ChethanUK <chethanuk@outlook.com>
2020-04-15 18:26:57 +0530

    Updating presto to v332

--- starburst-presto/presto.sh
+++ starburst-presto/presto.sh
@@ -22,8 +22,8 @@ readonly ROLE="$(/usr/share/google/get_metadata_value attributes/dataproc-role)"
 readonly PRESTO_MASTER_FQDN="$(/usr/share/google/get_metadata_value attributes/dataproc-master)"
 readonly WORKER_COUNT=$(/usr/share/google/get_metadata_value attributes/dataproc-worker-count)
 readonly PRESTO_BASE_URL=https://storage.googleapis.com/starburstdata/presto
-readonly PRESTO_MAJOR_VERSION="323"
-readonly PRESTO_VERSION="${PRESTO_MAJOR_VERSION}-e.3"
+readonly PRESTO_MAJOR_VERSION="332"
+readonly PRESTO_VERSION="${PRESTO_MAJOR_VERSION}-e.0"
 readonly HTTP_PORT="$(/usr/share/google/get_metadata_value attributes/presto-port || echo 8080)"
 readonly INIT_SCRIPT="/usr/lib/systemd/system/presto.service"
 PRESTO_JVM_MB=0
@@ -34,6 +34,26 @@ PRESTO_HEADROOM_NODE_MB=256
 function err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
   return 1
+}
+
+function retry_apt_command() {
+  cmd="$1"
+  for ((i = 0; i < 10; i++)); do
+    if eval "$cmd"; then
+      return 0
+    fi
+    sleep 5
+  done
+  return 1
+}
+
+function update_apt_get() {
+  retry_apt_command "apt-get update"
+}
+
+function install_apt_get() {
+  local pkgs="$*"
+  retry_apt_command "apt-get install -y $pkgs"
 }
 
 function wait_for_presto_cluster_ready() {
@@ -228,6 +248,9 @@ EOF
 # Configure Presto
 function configure_and_start_presto() {
   mkdir -p /opt/presto-server/etc/catalog
+
+  update_apt_get
+  install_apt_get openjdk-11-jre
 
   configure_node_properties
   configure_hive
