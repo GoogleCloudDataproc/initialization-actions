1c008069f2f0f5237c86ece8b8c2d197d5eb0336
Esteban Lau <estebanlau@google.com>
2020-04-15 13:32:10 -0700

    Upload test results by component

--- cloudbuild/run-all-tests.sh
+++ cloudbuild/run-all-tests.sh
@@ -30,26 +30,45 @@ ls bazel-bin -R
 ls bazel-out -R
 ls bazel-testlogs -R
 
-get_cache_status() {
-	cache_status_filepath=$(readlink -f bazel-testlogs/hue/test_hue/shard_1_of_3/test.cache_status)
-	cache_status=$(cat $cache_status_filepath)
-	echo $cache_status
+get_stable_status() {
+	stable_status_filepath=$(readlink -f bazel-out/stable-status.txt)
+	stable_status=$(cat $stable_status_filepath)
+	echo $stable_status
 }
 
+# Reads the test log of a given component
 get_test_logs() {
-	logs_filepath=$(readlink -f bazel-testlogs/hue/test_hue/shard_1_of_3/test.log)
+	component=$1
+	logs_filepath=$(readlink -f bazel-testlogs/${component}/test_${component}/shard_1_of_3/test.log)
 	logs=$(cat $logs_filepath)
 	echo $logs
 }
 
+get_test_xml() {
+	component=$1
+	xml_filepath=$(readlink -f bazel-testlogs/${component}/test_${component}/shard_1_of_3/test.xml)
+    xml=$(cat $xml_filepath)
+	echo $xml
+}
+
 cat bazel-out/k8-fastbuild/testlogs/hue/test_hue/shard_1_of_3/test.log
 
-echo $(get_cache_status) > cache_file.txt
-gsutil cp cache_file.txt gs://init-actions-github-tests/logs/init_actions_tests/1/cache_file.txt
-echo $(get_test_logs) > test_file.txt
-gsutil cp test_file.txt gs://init-actions-github-tests/logs/init_actions_tests/1/test_file.txt
+echo $(get_stable_status) > status_file.txt
+gsutil cp status_file.txt gs://init-actions-github-tests/logs/init_actions_tests/1/status_file.txt
+
+shopt -s nullglob
+COMPONENT_DIRS=(bazel-testlogs/*)
+COMPONENT_DIRS=("${COMPONENT_DIRS[@]##*/}") # Get only the component name
+for dir in "${COMPONENT_DIRS[@]}"; do
+  # Create started.json, finished.json and build-log.txt
+  echo $(get_test_logs $dir) > build-log.txt
+  echo $(get_test_xml $dir) > test.xml
+
+  # Upload to GCS
+  gsutil cp build-log.txt gs://init-actions-github-tests/logs/init_actions_tests/${BUILD_ID}/${dir}/build-log.txt
+  gsutil cp test.xml gs://init-actions-github-tests/logs/init_actions_tests/${BUILD_ID}/${dir}/test.xml
+done
+#echo $(get_test_logs) > test_file.txt
+#gsutil cp test_file.txt gs://init-actions-github-tests/logs/init_actions_tests/1/test_file.txt
 #ls bazel-testlogs/hue/test_hue
 
-
-# echo "rand contents" > rand_file.txt
-# gsutil cp rand_file.txt gs://init-actions-github-tests/logs/init_actions_tests/1/rand_file.txt
