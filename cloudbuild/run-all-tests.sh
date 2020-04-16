f1df39881427d402477051f0672aab6174edac8b
Esteban Lau <estebanlau@google.com>
2020-04-16 10:07:21 -0700

    Create finished.json file and upload to GCS

--- cloudbuild/run-all-tests.sh
+++ cloudbuild/run-all-tests.sh
@@ -30,12 +30,6 @@ ls bazel-bin -R
 ls bazel-out -R
 ls bazel-testlogs -R
 
-get_stable_status() {
-	stable_status_filepath=$(readlink -f bazel-out/stable-status.txt)
-	stable_status=$(cat $stable_status_filepath)
-	echo $stable_status
-}
-
 # Reads the test log of a given component
 get_test_logs() {
 	component=$1
@@ -51,22 +45,34 @@ get_test_xml() {
 	echo $xml
 }
 
-cat bazel-out/k8-fastbuild/testlogs/hue/test_hue/shard_1_of_3/test.log
-
-echo $(get_stable_status) > status_file.txt
-gsutil cp status_file.txt gs://init-actions-github-tests/logs/init_actions_tests/1/status_file.txt
+create_finished_json() {
+  component=$1
+  echo $(get_test_xml $component) > test.xml
+  failures_num=$(grep -oP '(?<=failures=")(\d)' "test.xml")
+  errors_num=$(grep -oP '(?<=errors=")(\d)' "test.xml")
+  if [ "$errors_num" == "0" ] && [ "$failures_num" == "0" ]; then
+  	status="SUCCESS"
+  else
+  	status="FAILED"
+  fi
+  jq -n \
+  	--arg timestamp $(date +%s) \
+  	--arg result $status \
+  	'{"timestamp":$timestamp, "result":$result, "metadata": {}}' > finished.json
+}
 
 shopt -s nullglob
 COMPONENT_DIRS=(bazel-testlogs/*)
 COMPONENT_DIRS=("${COMPONENT_DIRS[@]##*/}") # Get only the component name
 for dir in "${COMPONENT_DIRS[@]}"; do
-  # Create started.json, finished.json and build-log.txt
+  # Create build-log.txt
   echo $(get_test_logs $dir) > build-log.txt
-  echo $(get_test_xml $dir) > test.xml
-  failures_num=$(grep -oP '(?<=failures=)"(.*?)"' "test.xml")
-  echo $failures_num
+
+  # Create finished.json
+  create_finished_json $dir
 
   # Upload to GCS
+  gsutil cp finished.json gs://init-actions-github-tests/logs/init_actions_tests/${BUILD_ID}/${dir}/finished.json
   gsutil cp build-log.txt gs://init-actions-github-tests/logs/init_actions_tests/${BUILD_ID}/${dir}/build-log.txt
   gsutil cp test.xml gs://init-actions-github-tests/logs/init_actions_tests/${BUILD_ID}/${dir}/test.xml
 done
