de493c6d543cb4f6f1fe1b2dd0e216534760c91a
Esteban Lau <estebanlau@google.com>
2020-04-16 15:46:16 -0700

    Create counter.txt file in GCS to track build number

--- cloudbuild/run-all-tests.sh
+++ cloudbuild/run-all-tests.sh
@@ -63,8 +63,9 @@ for dir in "${COMPONENT_DIRS[@]}"; do
   create_finished_json $dir
 
   #Get build number
-  build_num=(($(cat cloudbuild/counter.txt)+1))
+  build_num=$(($(gsutil cat gs://init-actions-github-tests/counter.txt)+1))
   echo "$build_num" > counter.txt
+  gsutil cp counter.txt gs://init-actions-github-tests/counter.txt
 
   # Upload to GCS
   gsutil cp finished.json gs://init-actions-github-tests/logs/init_actions_tests/${build_num}/${dir}/finished.json
