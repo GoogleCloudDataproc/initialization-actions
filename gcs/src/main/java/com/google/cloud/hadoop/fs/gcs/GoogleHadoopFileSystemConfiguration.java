GoogleCloudDataproc/hadoop-connectors

Add an option for enabling/disabling checksums
Checksums may impact performance, so at the least we want to be able to
run with/without them to compare.

Change-Id: I02cc262d1081f4a71d8232c07421bc23f862411b

gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfiguration.java
@@ -483,6 +483,11 @@ public class GoogleHadoopFileSystemConfiguration {
   public static final GoogleHadoopFileSystemConfigurationProperty<Boolean> GCS_GRPC_ENABLE =
       new GoogleHadoopFileSystemConfigurationProperty<>("fs.gs.grpc.enable", false);
 
+  /** Configuration key for enabling checksum validation for the gRPC API. */
+  public static final GoogleHadoopFileSystemConfigurationProperty<Boolean>
+      GCS_GRPC_CHECKSUMS_ENABLE =
+          new GoogleHadoopFileSystemConfigurationProperty<>("fs.gs.grpc.checksums.enable", false);
+
   /** Override configuration file path. This file must be a valid Hadoop configuration file. */
   public static final GoogleHadoopFileSystemConfigurationProperty<String> GCS_CONFIG_OVERRIDE_FILE =
       new GoogleHadoopFileSystemConfigurationProperty<>("fs.gs.config.override.file", null);
@@ -586,6 +591,7 @@ public class GoogleHadoopFileSystemConfiguration {
         .setFadvise(GCS_INPUT_STREAM_FADVISE.get(config, config::getEnum))
         .setMinRangeRequestSize(GCS_INPUT_STREAM_MIN_RANGE_REQUEST_SIZE.get(config, config::getInt))
         .setGenerationReadConsistency(GCS_GENERATION_READ_CONSISTENCY.get(config, config::getEnum))
+        .setGrpcChecksumsEnabled(GCS_GRPC_CHECKSUMS_ENABLE.get(config, config::getBoolean))
         .build();
   }
 
@@ -596,6 +602,7 @@ public class GoogleHadoopFileSystemConfiguration {
         .setUploadChunkSize(GCS_OUTPUT_STREAM_UPLOAD_CHUNK_SIZE.get(config, config::getInt))
         .setDirectUploadEnabled(
             GCS_OUTPUT_STREAM_DIRECT_UPLOAD_ENABLE.get(config, config::getBoolean))
+        .setGrpcChecksumsEnabled(GCS_GRPC_CHECKSUMS_ENABLE.get(config, config::getBoolean))
         .build();
   }
 

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageReadOptions.java
@@ -54,6 +54,7 @@ public abstract class GoogleCloudStorageReadOptions {
   public static final int DEFAULT_MIN_RANGE_REQUEST_SIZE = 512 * 1024;
   public static final GenerationReadConsistency DEFAULT_GENERATION_READ_CONSISTENCY =
       GenerationReadConsistency.LATEST;
+  public static final boolean GRPC_CHECKSUMS_ENABLED_DEFAULT = false;
 
   // Default builder should be initialized after default values,
   // otherwise it will access not initialized default values.
@@ -72,7 +73,8 @@ public abstract class GoogleCloudStorageReadOptions {
         .setInplaceSeekLimit(DEFAULT_INPLACE_SEEK_LIMIT)
         .setFadvise(DEFAULT_FADVISE)
         .setMinRangeRequestSize(DEFAULT_MIN_RANGE_REQUEST_SIZE)
-        .setGenerationReadConsistency(DEFAULT_GENERATION_READ_CONSISTENCY);
+        .setGenerationReadConsistency(DEFAULT_GENERATION_READ_CONSISTENCY)
+        .setGrpcChecksumsEnabled(GRPC_CHECKSUMS_ENABLED_DEFAULT);
   }
 
   /** See {@link Builder#setBackoffInitialIntervalMillis}. */
@@ -110,6 +112,9 @@ public abstract class GoogleCloudStorageReadOptions {
 
   /** See {@link Builder#setGenerationReadConsistency}. */
   public abstract GenerationReadConsistency getGenerationReadConsistency();
+
+  /** See {@link Builder#setGrpcChecksumsEnabled}. */
+  public abstract boolean getGrpcChecksumsEnabled();
 
   public abstract Builder toBuilder();
 
@@ -217,13 +222,23 @@ public abstract class GoogleCloudStorageReadOptions {
      */
     public abstract Builder setGenerationReadConsistency(GenerationReadConsistency consistency);
 
+    /**
+     * Sets whether to validate checksums when doing gRPC reads. If enabled, for sequential reads of
+     * a whole object, the object checksums will be validated.
+     *
+     * <p>TODO(b/134521856): Update this to discuss per-request checksums once the server supplies
+     * them and we're validating them.
+     */
+    public abstract Builder setGrpcChecksumsEnabled(boolean grpcChecksumsEnabled);
+
     abstract GoogleCloudStorageReadOptions autoBuild();
 
     public GoogleCloudStorageReadOptions build() {
       GoogleCloudStorageReadOptions options = autoBuild();
       checkState(
           options.getInplaceSeekLimit() >= 0,
-          "inplaceSeekLimit must be non-negative! Got %s", options.getInplaceSeekLimit());
+          "inplaceSeekLimit must be non-negative! Got %s",
+          options.getInplaceSeekLimit());
       return options;
     }
   }

GoogleCloudDataproc/initialization-actions

gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfiguration.java
@@ -358,6 +358,10 @@ public class GoogleHadoopFileSystemConfiguration {
   public static final HadoopConfigurationProperty<Boolean> GCS_GRPC_ENABLE =
       new HadoopConfigurationProperty<>("fs.gs.grpc.enable", false);
 
+  /** Configuration key for enabling checksum validation for the gRPC API. */
+  public static final HadoopConfigurationProperty<Boolean> GCS_GRPC_CHECKSUMS_ENABLE =
+      new HadoopConfigurationProperty<>("fs.gs.grpc.checksums.enable", false);
+
   /** Override configuration file path. This file must be a valid Hadoop configuration file. */
   public static final HadoopConfigurationProperty<String> GCS_CONFIG_OVERRIDE_FILE =
       new HadoopConfigurationProperty<>("fs.gs.config.override.file", null);
@@ -484,6 +488,7 @@ public class GoogleHadoopFileSystemConfiguration {
         .setBufferSize(GCS_INPUT_STREAM_BUFFER_SIZE.get(config, config::getInt))
         .setFadvise(GCS_INPUT_STREAM_FADVISE.get(config, config::getEnum))
         .setMinRangeRequestSize(GCS_INPUT_STREAM_MIN_RANGE_REQUEST_SIZE.get(config, config::getInt))
+        .setGrpcChecksumsEnabled(GCS_GRPC_CHECKSUMS_ENABLE.get(config, config::getBoolean))
         .build();
   }
 
@@ -495,6 +500,7 @@ public class GoogleHadoopFileSystemConfiguration {
         .setUploadCacheSize(GCS_OUTPUT_STREAM_UPLOAD_CACHE_SIZE.get(config, config::getInt))
         .setDirectUploadEnabled(
             GCS_OUTPUT_STREAM_DIRECT_UPLOAD_ENABLE.get(config, config::getBoolean))
+        .setGrpcChecksumsEnabled(GCS_GRPC_CHECKSUMS_ENABLE.get(config, config::getBoolean))
         .build();
   }
 

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageReadOptions.java
@@ -45,6 +45,7 @@ public abstract class GoogleCloudStorageReadOptions {
   public static final long DEFAULT_INPLACE_SEEK_LIMIT = 0L;
   public static final Fadvise DEFAULT_FADVISE = Fadvise.SEQUENTIAL;
   public static final int DEFAULT_MIN_RANGE_REQUEST_SIZE = 512 * 1024;
+  public static final boolean GRPC_CHECKSUMS_ENABLED_DEFAULT = false;
 
   // Default builder should be initialized after default values,
   // otherwise it will access not initialized default values.
@@ -62,7 +63,8 @@ public abstract class GoogleCloudStorageReadOptions {
         .setBufferSize(DEFAULT_BUFFER_SIZE)
         .setInplaceSeekLimit(DEFAULT_INPLACE_SEEK_LIMIT)
         .setFadvise(DEFAULT_FADVISE)
-        .setMinRangeRequestSize(DEFAULT_MIN_RANGE_REQUEST_SIZE);
+        .setMinRangeRequestSize(DEFAULT_MIN_RANGE_REQUEST_SIZE)
+        .setGrpcChecksumsEnabled(GRPC_CHECKSUMS_ENABLED_DEFAULT);
   }
 
   public abstract Builder toBuilder();
@@ -99,6 +101,8 @@ public abstract class GoogleCloudStorageReadOptions {
 
   /** See {@link Builder#setMinRangeRequestSize}. */
   public abstract int getMinRangeRequestSize();
+
+  public abstract Builder toBuilder();
 
   /** Mutable builder for GoogleCloudStorageReadOptions. */
   @AutoValue.Builder
@@ -188,13 +192,23 @@ public abstract class GoogleCloudStorageReadOptions {
      */
     public abstract Builder setMinRangeRequestSize(int size);
 
+    /**
+     * Sets whether to validate checksums when doing gRPC reads. If enabled, for sequential reads of
+     * a whole object, the object checksums will be validated.
+     *
+     * <p>TODO(b/134521856): Update this to discuss per-request checksums once the server supplies
+     * them and we're validating them.
+     */
+    public abstract Builder setGrpcChecksumsEnabled(boolean grpcChecksumsEnabled);
+
     abstract GoogleCloudStorageReadOptions autoBuild();
 
     public GoogleCloudStorageReadOptions build() {
       GoogleCloudStorageReadOptions options = autoBuild();
       checkState(
           options.getInplaceSeekLimit() >= 0,
-          "inplaceSeekLimit must be non-negative! Got %s", options.getInplaceSeekLimit());
+          "inplaceSeekLimit must be non-negative! Got %s",
+          options.getInplaceSeekLimit());
       return options;
     }
   }