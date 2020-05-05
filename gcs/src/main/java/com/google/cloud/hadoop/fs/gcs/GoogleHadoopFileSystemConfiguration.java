GoogleCloudDataproc/hadoop-connectors

Add option to enable gRPC API (disabled by default)
Change-Id: I8aab0a4151c862ed36ec92a6ec0388e08f76cc1a

gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfiguration.java
@@ -479,6 +479,10 @@ public class GoogleHadoopFileSystemConfiguration {
               "fs.gs.inputstream.min.range.request.size",
               GoogleCloudStorageReadOptions.DEFAULT_MIN_RANGE_REQUEST_SIZE);
 
+  /** Configuration key for enabling use of the gRPC API for read/write. */
+  public static final GoogleHadoopFileSystemConfigurationProperty<Boolean> GCS_GRPC_ENABLE =
+      new GoogleHadoopFileSystemConfigurationProperty<>("fs.gs.grpc.enable", false);
+
   /** Override configuration file path. This file must be a valid Hadoop configuration file. */
   public static final GoogleHadoopFileSystemConfigurationProperty<String> GCS_CONFIG_OVERRIDE_FILE =
       new GoogleHadoopFileSystemConfigurationProperty<>("fs.gs.config.override.file", null);
@@ -550,7 +554,8 @@ public class GoogleHadoopFileSystemConfiguration {
         .setReadChannelOptions(getReadChannelOptions(config))
         .setWriteChannelOptions(getWriteChannelOptions(config))
         .setRequesterPaysOptions(getRequesterPaysOptions(config, projectId))
-        .setCooperativeLockingOptions(getCooperativeLockingOptions(config));
+        .setCooperativeLockingOptions(getCooperativeLockingOptions(config))
+        .setGrpcEnabled(GCS_GRPC_ENABLE.get(config, config::getBoolean));
   }
 
   private static PerformanceCachingGoogleCloudStorageOptions getPerformanceCachingOptions(

GoogleCloudDataproc/initialization-actions

gcs/src/main/java/com/google/cloud/hadoop/fs/gcs/GoogleHadoopFileSystemConfiguration.java
@@ -354,6 +354,10 @@ public class GoogleHadoopFileSystemConfiguration {
           "fs.gs.inputstream.min.range.request.size",
           GoogleCloudStorageReadOptions.DEFAULT_MIN_RANGE_REQUEST_SIZE);
 
+  /** Configuration key for enabling use of the gRPC API for read/write. */
+  public static final HadoopConfigurationProperty<Boolean> GCS_GRPC_ENABLE =
+      new HadoopConfigurationProperty<>("fs.gs.grpc.enable", false);
+
   /** Override configuration file path. This file must be a valid Hadoop configuration file. */
   public static final HadoopConfigurationProperty<String> GCS_CONFIG_OVERRIDE_FILE =
       new HadoopConfigurationProperty<>("fs.gs.config.override.file", null);
@@ -451,7 +455,8 @@ public class GoogleHadoopFileSystemConfiguration {
         .setHttpRequestHeaders(GCS_HTTP_HEADERS.getPropsWithPrefix(config))
         .setEncryptionAlgorithm(GCS_ENCRYPTION_ALGORITHM.get(config, config::get))
         .setEncryptionKey(RedactedString.create(GCS_ENCRYPTION_KEY.getPassword(config)))
-        .setEncryptionKeyHash(RedactedString.create(GCS_ENCRYPTION_KEY_HASH.getPassword(config)));
+        .setEncryptionKeyHash(RedactedString.create(GCS_ENCRYPTION_KEY_HASH.getPassword(config)))
+        .setGrpcEnabled(GCS_GRPC_ENABLE.get(config, config::getBoolean));
   }
 
   private static PerformanceCachingGoogleCloudStorageOptions getPerformanceCachingOptions(