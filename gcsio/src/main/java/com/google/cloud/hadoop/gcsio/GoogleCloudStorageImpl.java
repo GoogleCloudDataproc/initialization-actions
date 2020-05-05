hongyegong/bigdata-interop

Add flag for Direct Path support
The enable_grpclb system property needs to be set for gRPC to use Direct
Path. The gRPC team has told me that this should be fine to set even if not
using Direct Path, so I think it makes the most sense to just set it in
the connector so that the users don't have to worry about it.

Change-Id: Iacbe8d5706056f81c0906cf9742994440781ce30

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -77,7 +77,6 @@ import java.nio.channels.SeekableByteChannel;
 import java.nio.channels.WritableByteChannel;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -259,14 +258,19 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
         .setApplicationName(options.getAppName())
         .build();
 
-    // Create the gRPC stub;
-    Map<String, Object> serviceConfig = getGrpcServiceConfig(options.getReadChannelOptions());
-    this.gcsGrpcStub =
-        StorageGrpc.newStub(
-            GoogleDefaultChannelBuilder.forTarget(GRPC_TARGET)
-                .defaultServiceConfig(serviceConfig)
-                .build())
-            .withExecutor(threadPool);
+    // Create the gRPC stub if necessary;
+    if (storageOptions.isGrpcEnabled()) {
+      if (System.getProperty("io.grpc.internal.DnsNameResolverProvider.enable_grpclb") == null) {
+        System.setProperty("io.grpc.internal.DnsNameResolverProvider.enable_grpclb", "true");
+      }
+      Map<String, Object> serviceConfig = getGrpcServiceConfig(options.getReadChannelOptions());
+      this.gcsGrpcStub =
+          StorageGrpc.newStub(
+              GoogleDefaultChannelBuilder.forTarget(GRPC_TARGET)
+              .defaultServiceConfig(serviceConfig)
+              .build())
+          .withExecutor(threadPool);
+    }
   }
 
   private Map<String, Object> getGrpcServiceConfig(GoogleCloudStorageReadOptions readOptions) {

GoogleCloudDataproc/initialization-actions

gcsio/src/main/java/com/google/cloud/hadoop/gcsio/GoogleCloudStorageImpl.java
@@ -259,14 +259,19 @@ public class GoogleCloudStorageImpl implements GoogleCloudStorage {
             .setApplicationName(options.getAppName())
             .build();
 
-    // Create the gRPC stub;
-    Map<String, Object> serviceConfig = getGrpcServiceConfig(options.getReadChannelOptions());
-    this.gcsGrpcStub =
-        StorageGrpc.newStub(
-            GoogleDefaultChannelBuilder.forTarget(GRPC_TARGET)
-                .defaultServiceConfig(serviceConfig)
-                .build())
-            .withExecutor(backgroundTasksThreadPool);
+    // Create the gRPC stub if necessary;
+    if (storageOptions.isGrpcEnabled()) {
+      if (System.getProperty("io.grpc.internal.DnsNameResolverProvider.enable_grpclb") == null) {
+        System.setProperty("io.grpc.internal.DnsNameResolverProvider.enable_grpclb", "true");
+      }
+      Map<String, Object> serviceConfig = getGrpcServiceConfig(options.getReadChannelOptions());
+      this.gcsGrpcStub =
+          StorageGrpc.newStub(
+              GoogleDefaultChannelBuilder.forTarget(GRPC_TARGET)
+              .defaultServiceConfig(serviceConfig)
+              .build())
+          .withExecutor(backgroundTasksThreadPool);
+    }
   }
 
   private Map<String, Object> getGrpcServiceConfig(GoogleCloudStorageReadOptions readOptions) {