5c4e340dc326639fb39733c77f9d42a046486584
Dagang Wei <functicons@gmail.com>
2020-06-30 12:27:52 -0700

    Add retry in pip install (#773)

--- python/pip-install.sh
+++ python/pip-install.sh
@@ -4,6 +4,22 @@ set -exo pipefail
 
 readonly PACKAGES=$(/usr/share/google/get_metadata_value attributes/PIP_PACKAGES || true)
 
+function err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+  exit 1
+}
+
+function run_with_retry() {
+  local -r cmd=("$@")
+  for ((i = 0; i < 10; i++)); do
+    if "${cmd[@]}"; then
+      return 0
+    fi
+    sleep 5
+  done
+  err "Failed to run command: ${cmd[*]}"
+}
+
 function install_pip() {
   if command -v pip >/dev/null; then
     echo "pip is already installed."
@@ -12,13 +28,13 @@ function install_pip() {
 
   if command -v easy_install >/dev/null; then
     echo "Installing pip with easy_install..."
-    easy_install pip
+    run_with_retry easy_install pip
     return 0
   fi
 
   echo "Installing python-pip..."
-  apt update
-  apt install python-pip -y
+  run_with_retry apt update
+  run_with_retry apt install python-pip -y
 }
 
 function main() {
@@ -28,7 +44,8 @@ function main() {
   fi
 
   install_pip
-  pip install --upgrade ${PACKAGES}
+  run_with_retry pip install --upgrade ${PACKAGES}
 }
 
 main
+
