f6c0ed0a1b2cee83d83e3cdecfa7a72a0545e5bd
DougM <mengdong0427@gmail.com>
2020-07-15 17:36:29 -0700

    drop 2.0 test

--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -38,8 +38,8 @@ class RapidsTestCase(DataprocTestCase):
 
     @parameterized.parameters(("STANDARD", ["m", "w-0"], GPU_P100))
     def test_rapids_dask(self, configuration, machine_suffixes, accelerator):
-        # Init action supported on Dataproc 1.3+
-        if self.getImageVersion() < pkg_resources.parse_version("1.3"):
+        # Init action supported on Dataproc 1.3+, rapids dask works with 2.0/preview and up
+        if self.getImageVersion() < pkg_resources.parse_version("2.0"):
             return
 
         metadata = 'gpu-driver-provider=NVIDIA,rapids-runtime=DASK'
