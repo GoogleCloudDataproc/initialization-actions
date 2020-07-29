2a6b7bb1d3b5df93bc6a019d39be4b294a0b75a8
Igor Dvorzhak <idv@google.com>
2020-07-29 13:59:48 -0700

    Update test_rapids.py

--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -48,7 +48,7 @@ class RapidsTestCase(DataprocTestCase):
                            master_accelerator=accelerator,
                            worker_accelerator=accelerator,
                            optional_components=['ANACONDA'],
-                           timeout_in_minutes=120)
+                           timeout_in_minutes=60)
 
         for machine_suffix in machine_suffixes:
             self.verify_dask_instance("{}-{}".format(self.getClusterName(),
