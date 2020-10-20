8638efffaf1fbaf27acfefe2734b78ae26dd1119
Igor Dvorzhak <idv@google.com>
2020-10-19 19:10:34 -0700

    Update presubmit.sh

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -28,19 +28,18 @@ configure_gcloud_ssh_key() {
 # so we can diff what changed relatively to master branch.
 initialize_git_repo() {
   rm -fr .git
-  
   git init
   
   git config user.email "ia-tests@presubmit.example.com"
   git config user.name "ia-tests"
   
   git remote add origin "https://github.com/GoogleCloudDataproc/initialization-actions.git"
-
+  git fetch origin master
   # Fetch all PRs to get history for PRs created from forked repos
   git fetch origin "+refs/pull/*/merge:refs/remotes/origin/pr/*"
+
   git reset --hard "${COMMIT_SHA}"
   
-  git fetch origin master
   git log --oneline --max-count=10
   local master_hash
   master_hash=$(git show-ref -s origin/master)
