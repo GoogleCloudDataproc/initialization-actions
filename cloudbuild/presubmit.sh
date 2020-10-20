37c27973dbe1c8cc36cd3fbf7342452a96dcb64f
Igor Dvorzhak <idv@google.com>
2020-10-19 19:10:47 -0700

    Update presubmit.sh

--- cloudbuild/presubmit.sh
+++ cloudbuild/presubmit.sh
@@ -32,7 +32,7 @@ initialize_git_repo() {
   
   git config user.email "ia-tests@presubmit.example.com"
   git config user.name "ia-tests"
-  
+
   git remote add origin "https://github.com/GoogleCloudDataproc/initialization-actions.git"
   git fetch origin master
   # Fetch all PRs to get history for PRs created from forked repos
