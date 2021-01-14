3e8123d4ae19356c08ebaf5650abfe5226923d57
Igor Dvorzhak <idv@google.com>
2021-01-14 12:39:12 -0800

    Update rapids.sh

--- rapids/rapids.sh
+++ rapids/rapids.sh
@@ -13,7 +13,9 @@ readonly SPARK_VERSION_ENV=$(spark-submit --version 2>&1 | sed -n 's/.*version[[
 if [[ "${SPARK_VERSION_ENV}" == "3"* ]]; then
   readonly DEFAULT_CUDF_VERSION="0.17"
   readonly DEFAULT_SPARK_RAPIDS_VERSION="0.3.0"
-  readonly SPARK_VERSION="${SPARK_VERSION_ENV}"
+  # TODO: uncomment when Spark 3.1 jars will be released.
+  # readonly SPARK_VERSION="${SPARK_VERSION_ENV}"
+  readonly SPARK_VERSION="3.0"
 else
   readonly DEFAULT_CUDF_VERSION="0.9.2"
   readonly DEFAULT_SPARK_RAPIDS_VERSION="Beta5"
