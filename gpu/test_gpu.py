4b430709cc25c7207eeb33345fe426d58dc0c18f
bradmiro <bmiro@google.com>
2021-01-14 19:56:28 -0500

    formatting fixes

--- gpu/test_gpu.py
+++ gpu/test_gpu.py
@@ -7,9 +7,9 @@ from integration_tests.dataproc_test_case import DataprocTestCase
 
 
 class NvidiaGpuDriverTestCase(DataprocTestCase):
-  COMPONENT = 'gpu'
-  INIT_ACTIONS = ['gpu/install_gpu_driver.sh']
-  GPU_V100 = 'type=nvidia-tesla-v100'
+  COMPONENT = "gpu"
+  INIT_ACTIONS = ["gpu/install_gpu_driver.sh"]
+  GPU_V100 = "type=nvidia-tesla-v100"
 
   def verify_instance(self, name):
     self.assert_instance_command(name, "nvidia-smi")
@@ -23,9 +23,9 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
         name, "sudo find /usr -iname libcudnn.so | grep .")
 
   @parameterized.parameters(
-    ("STANDARD", ["m"], GPU_V100, None, None),
-    ("STANDARD", ["m", "w-0", "w-1"], GPU_V100, GPU_V100, "OS"),
-    ("STANDARD", ["w-0", "w-1"], None, GPU_V100, "NVIDIA"),
+      ("STANDARD", ["m"], GPU_V100, None, None),
+      ("STANDARD", ["m", "w-0", "w-1"], GPU_V100, GPU_V100, "OS"),
+      ("STANDARD", ["w-0", "w-1"], None, GPU_V100, "NVIDIA"),
   )
   def test_install_gpu_default_agent(self, configuration, machine_suffixes,
                                      master_accelerator, worker_accelerator,
@@ -33,16 +33,17 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
     metadata = None
     if driver_provider is not None:
       metadata = "gpu-driver-provider={}".format(driver_provider)
-    self.createCluster(configuration,
-                       self.INIT_ACTIONS,
-                       machine_type='n1-standard-2',
-                       master_accelerator=master_accelerator,
-                       worker_accelerator=worker_accelerator,
-                       metadata=metadata,
-                       timeout_in_minutes=30)
+    self.createCluster(
+        configuration,
+        self.INIT_ACTIONS,
+        machine_type="n1-standard-2",
+        master_accelerator=master_accelerator,
+        worker_accelerator=worker_accelerator,
+        metadata=metadata,
+        timeout_in_minutes=30)
     for machine_suffix in machine_suffixes:
-        self.verify_instance("{}-{}".format(self.getClusterName(),
-                                            machine_suffix))
+      self.verify_instance("{}-{}".format(self.getClusterName(),
+                                          machine_suffix))
 
   @parameterized.parameters(
       ("STANDARD", ["w-0", "w-1"], None, GPU_V100, None),
@@ -52,19 +53,20 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
   def test_install_gpu_without_agent(self, configuration, machine_suffixes,
                                      master_accelerator, worker_accelerator,
                                      driver_provider):
-      metadata = 'install-gpu-agent=false'
-      if driver_provider is not None:
-          metadata += ",gpu-driver-provider={}".format(driver_provider)
-      self.createCluster(configuration,
-                         self.INIT_ACTIONS,
-                         machine_type='n1-standard-2',
-                         master_accelerator=master_accelerator,
-                         worker_accelerator=worker_accelerator,
-                         metadata=metadata,
-                         timeout_in_minutes=30)
-      for machine_suffix in machine_suffixes:
-        self.verify_instance("{}-{}".format(self.getClusterName(),
-                                            machine_suffix))
+    metadata = "install-gpu-agent=false"
+    if driver_provider is not None:
+      metadata += ",gpu-driver-provider={}".format(driver_provider)
+    self.createCluster(
+        configuration,
+        self.INIT_ACTIONS,
+        machine_type="n1-standard-2",
+        master_accelerator=master_accelerator,
+        worker_accelerator=worker_accelerator,
+        metadata=metadata,
+        timeout_in_minutes=30)
+    for machine_suffix in machine_suffixes:
+      self.verify_instance("{}-{}".format(self.getClusterName(),
+                                          machine_suffix))
 
   @parameterized.parameters(
       ("STANDARD", ["m", "w-0", "w-1"], GPU_V100, GPU_V100, None),
@@ -74,23 +76,47 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
   def test_install_gpu_with_agent(self, configuration, machine_suffixes,
                                   master_accelerator, worker_accelerator,
                                   driver_provider):
-    metadata = 'install-gpu-agent=true'
+    metadata = "install-gpu-agent=true"
     if driver_provider is not None:
       metadata += ",gpu-driver-provider={}".format(driver_provider)
     self.createCluster(
         configuration,
         self.INIT_ACTIONS,
-        machine_type='n1-standard-2',
+        machine_type="n1-standard-2",
         master_accelerator=master_accelerator,
         worker_accelerator=worker_accelerator,
         metadata=metadata,
         timeout_in_minutes=30,
-        scopes='https://www.googleapis.com/auth/monitoring.write')
+        scopes="https://www.googleapis.com/auth/monitoring.write")
     for machine_suffix in machine_suffixes:
       self.verify_instance("{}-{}".format(self.getClusterName(),
                                           machine_suffix))
       self.verify_instance_gpu_agent("{}-{}".format(self.getClusterName(),
                                                     machine_suffix))
+
+  @parameterized.parameters(
+      ("STANDARD", ["m"], GPU_V100, None, "10.1"),
+      ("STANDARD", ["m", "w-0", "w-1"], GPU_V100, GPU_V100, "10.1"),
+      ("STANDARD", ["w-0", "w-1"], None, GPU_V100, "10.1"),
+      ("STANDARD", ["w-0", "w-1"], None, GPU_V100, "10.2"),
+      ("STANDARD", ["w-0", "w-1"], None, GPU_V100, "11.0"),
+      ("STANDARD", ["w-0", "w-1"], None, GPU_V100, "11.1"),
+  )
+  def test_install_gpu_cuda_nvidia(self, configuration, machine_suffixes,
+                                   master_accelerator, worker_accelerator,
+                                   cuda_version):
+    metadata = "gpu-driver-provider=NVIDIA,cuda-version={}".format(cuda_version)
+    self.createCluster(
+        configuration,
+        self.INIT_ACTIONS,
+        machine_type="n1-standard-2",
+        master_accelerator=master_accelerator,
+        worker_accelerator=worker_accelerator,
+        metadata=metadata,
+        timeout_in_minutes=30)
+    for machine_suffix in machine_suffixes:
+      self.verify_instance("{}-{}".format(self.getClusterName(),
+                                          machine_suffix))
 
   @parameterized.parameters(
       ("STANDARD", ["m", "w-0", "w-1"], GPU_V100, GPU_V100, "NVIDIA",
@@ -102,22 +128,22 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
     if "Debian" in platform.uname().version:
         self.skipTest("Not supported on Debian.")
 
-    metadata = 'cudnn-version={}'.format(cudnn_version)
+    metadata = "cudnn-version={}".format(cudnn_version)
     if driver_provider is not None:
         metadata += ",gpu-driver-provider={}".format(driver_provider)
     self.createCluster(
         configuration,
         self.INIT_ACTIONS,
-        machine_type='n1-standard-2',
+        machine_type="n1-standard-2",
         master_accelerator=master_accelerator,
         worker_accelerator=worker_accelerator,
         metadata=metadata,
         timeout_in_minutes=30,
-        scopes='https://www.googleapis.com/auth/monitoring.write')
+        scopes="https://www.googleapis.com/auth/monitoring.write")
     for machine_suffix in machine_suffixes:
-        self.verify_instance_cudnn("{}-{}".format(self.getClusterName(),
-                                            machine_suffix))
+      self.verify_instance_cudnn("{}-{}".format(self.getClusterName(),
+                                                machine_suffix))
 
 
-if __name__ == '__main__':
-    absltest.main()
+if __name__ == "__main__":
+  absltest.main()
