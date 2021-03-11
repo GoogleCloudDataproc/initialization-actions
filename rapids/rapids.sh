bde5cd39725a6193cb4889daa1816e655a1ec27b
bradmiro <bmiro@google.com>
2021-03-11 18:52:37 -0500

    adding separate RAPIDS versions for Dask/Spark runtimes

--- rapids/rapids.sh
+++ rapids/rapids.sh
@@ -36,7 +36,17 @@ readonly RUN_WORKER_ON_MASTER=$(get_metadata_attribute 'dask-cuda-worker-on-mast
 # RAPIDS config
 readonly CUDA_VERSION=$(get_metadata_attribute 'cuda-version' ${DEFAULT_CUDA_VERSION})
 readonly CUDF_VERSION=$(get_metadata_attribute 'cudf-version' ${DEFAULT_CUDF_VERSION})
-readonly RAPIDS_VERSION=$(get_metadata_attribute 'rapids-version' '0.18')
+
+# TODO: standardize versions when Spark 3.1 jars will be released. 
+if [[ "${RUNTIME}" == "DASK" ]]; then
+  DEFAULT_RAPIDS_VERSION="0.18"
+elif [[ "${RUNTIME}" == "SPARK" ]]; then
+  DEFAULT_RAPIDS_VERSION="0.17"
+else
+  echo "Unsupported RAPIDS Runtime: ${RUNTIME}"
+  exit 1
+fi
+readonly RAPIDS_VERSION=$(get_metadata_attribute 'rapids-version' ${DEFAULT_RAPIDS_VERSION})
 
 # SPARK config
 readonly SPARK_RAPIDS_VERSION=$(get_metadata_attribute 'spark-rapids-version' ${DEFAULT_SPARK_RAPIDS_VERSION})
