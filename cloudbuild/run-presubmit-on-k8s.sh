9368d6e9003b91aca8eaa471f1275dabc85c232e
Igor Dvorzhak <idv@google.com>
2021-03-16 19:56:42 -0700

    Update run-presubmit-on-k8s.sh

--- cloudbuild/run-presubmit-on-k8s.sh
+++ cloudbuild/run-presubmit-on-k8s.sh
@@ -11,6 +11,7 @@ readonly POD_NAME=presubmit-${DATAPROC_IMAGE_VERSION//./-}-${BUILD_ID//_/-}
 gcloud container clusters get-credentials "${CLOUDSDK_CONTAINER_CLUSTER}"
 
 kubectl run "${POD_NAME}" --generator=run-pod/v1 --image="$IMAGE" \
+  --pod-running-timeout=10m \
   --requests "cpu=1.3,memory=4.9Gi" --restart=Never \
   --env="COMMIT_SHA=$COMMIT_SHA" \
   --env="IMAGE_VERSION=$DATAPROC_IMAGE_VERSION" \
