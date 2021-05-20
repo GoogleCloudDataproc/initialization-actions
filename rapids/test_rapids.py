6fa4ef7ce314f35c7c80be8784ea1f391166fa74
Brad Miro <bmiro@google.com>
2021-05-20 00:24:21 -0400

    Update test_rapids.py
    Removing OS restrictions on Spark RAPIDS test

--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -80,12 +80,6 @@ class RapidsTestCase(DataprocTestCase):
     ("SINGLE", ["m"], GPU_P100),
     ("STANDARD", ["w-0"], GPU_P100))
   def test_rapids_spark(self, configuration, machine_suffixes, accelerator):
-    if self.getImageOs() == 'centos':
-      self.skipTest("Not supported in CentOS-based images")
-      
-    if self.getImageOs() == 'debian':
-      self.skipTest("Not supported in Debian-based images")
-
     if self.getImageVersion() <= pkg_resources.parse_version("1.4"):
       self.skipTest("Not supported in pre 1.5 images")
 
