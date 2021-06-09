4cab1603c1768a9aaed67e897fd0bca2118d592b
bradmiro <bmiro@google.com>
2021-06-09 14:36:21 -0400

    fix rapids

--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -53,9 +53,6 @@ class RapidsTestCase(DataprocTestCase):
     ("STANDARD", ["m", "w-0"], GPU_P100, "yarn"),
     ("STANDARD", ["m"], GPU_P100, "standalone"))
   def test_rapids_dask(self, configuration, machine_suffixes, accelerator, dask_runtime):
-    if self.getImageOs() == 'centos':
-      self.skipTest("Not supported in CentOS-based images")
-
     if self.getImageVersion() < pkg_resources.parse_version("2.0"):
       self.skipTest("Not supported in pre 2.0 images")
 
@@ -80,9 +77,6 @@ class RapidsTestCase(DataprocTestCase):
     ("SINGLE", ["m"], GPU_P100),
     ("STANDARD", ["w-0"], GPU_P100))
   def test_rapids_spark(self, configuration, machine_suffixes, accelerator):
-    if self.getImageOs() == 'centos':
-      self.skipTest("Not supported in CentOS-based images")
-
     if self.getImageVersion() <= pkg_resources.parse_version("1.4"):
       self.skipTest("Not supported in pre 1.5 images")
 
