c29ac38c6d57400fcece1662ade6b8c79a1207bf
Hao Zhu <viadeazhu@gmail.com>
2021-08-19 12:01:42 -0700

    Signed-off-by: Hao Zhu <hazhu@nvidia.com>
    change xgboost jar back to 1.3.0

--- rapids/rapids.sh
+++ rapids/rapids.sh
@@ -17,7 +17,7 @@ readonly DEFAULT_SPARK_RAPIDS_VERSION="21.08.0"
 if [[ "${SPARK_VERSION_ENV}" == "3"* ]]; then
   readonly DEFAULT_CUDA_VERSION="11.0"
   readonly DEFAULT_CUDF_VERSION="21.08.2"
-  readonly DEFAULT_XGBOOST_VERSION="1.4.2"
+  readonly DEFAULT_XGBOOST_VERSION="1.3.0"
   readonly DEFAULT_XGBOOST_GPU_SUB_VERSION="0.1.0"
   # TODO: uncomment when Spark 3.1 jars will be released - RAPIDS work with Spark 3.1, this is just for Maven URL
   # readonly SPARK_VERSION="${SPARK_VERSION_ENV}"
