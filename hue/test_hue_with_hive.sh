09e0c2f6565e2623f755e74785464387222c60ad
Danyel Cabello <danyel.nerv@gmail.com>
2022-04-05 23:58:57 -0400

    Clean unnecessary exit condition

--- hue/test_hue_with_hive.sh
+++ hue/test_hue_with_hive.sh
@@ -3,7 +3,4 @@ curl -s localhost:8888/hue/accounts/login?next=/ -c /tmp/login.cookies --output 
 csrftoken=$(sed -n -e "/.*csrftoken.*/s/.*csrftoken\t//p" /tmp/login.cookies)
 curl -s localhost:8888/hue/accounts/login -H "Content-Type: application/x-www-form-urlencoded" -H "Referer: http://localhost:8888/accounts/login?next=/"   -d "username=admin&password=admin&csrfmiddlewaretoken=${csrftoken}&next=/"  -b /tmp/login.cookies -c /tmp/auth.cookies -o /dev/null
 curl -s localhost:8888/desktop/api2/get_config/ -b /tmp/auth.cookies | grep -q '"dialect": "hive"'
-if (( $? == 0 )); then
-  exit 0
-fi
-exit 1
+exit
