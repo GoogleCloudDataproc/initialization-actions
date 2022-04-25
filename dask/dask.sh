9bcad6752a2cc01182b6aa949122700786f81231
Igor Dvorzhak <idv@google.com>
2022-04-25 15:47:37 -0700

    Update dask.sh

--- dask/dask.sh
+++ dask/dask.sh
@@ -40,7 +40,7 @@ CONDA_PACKAGES=("dask=2022.1.1" "dask-bigquery" "dask-ml")
 if [[ "${DASK_RUNTIME}" == "yarn" ]]; then
   # Pin `distributed` pacakge version because `dask-yarn` 0.9
   # is not compatible with `distributed` package 2022.3 and newer
-  CONDA_PACKAGES+=("dask-yarn=0.9", "distributed=2022.2")
+  CONDA_PACKAGES+=("dask-yarn=0.9" "distributed=2022.2")
   
   # Update protobuf on Dataproc 1.x
   if [ "$(echo "$DATAPROC_VERSION < 2.0" | bc)" -eq 1 ]; then
