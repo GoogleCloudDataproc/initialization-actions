6d51cd632cc2d4af83cd71e6c3bdd9cecc810daa
pinxians <pinxians@google.com>
2022-06-03 14:19:03 -0700

    Rename blackholw -> blcok.

--- ranger/block_vm_metadata_server.sh
+++ ranger/block_vm_metadata_server.sh
@@ -1,6 +1,6 @@
 set -xe
 
-# Blackhole the VM metadata server except for root.
+# Block tcp connection to the VM metadata server except for root so that normal users can't get the VM service account's credential.
 iptables -A OUTPUT -d 169.254.169.254 -p tcp -m owner --uid-owner root -j ACCEPT
 iptables -A OUTPUT -d 169.254.169.254 -p tcp -j REJECT
 
