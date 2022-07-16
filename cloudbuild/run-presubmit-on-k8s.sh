99fe75a6b05598c04a01f31dae843b83d0242037
Igor Dvorzhak <idv@google.com>
2022-07-16 00:31:37 -0700

    Explictly request ephemeral storage to prevent evictions

--- cloudbuild/run-presubmit-on-k8s.sh
+++ cloudbuild/run-presubmit-on-k8s.sh
@@ -10,15 +10,37 @@ readonly POD_NAME=presubmit-${DATAPROC_IMAGE_VERSION//./-}-${BUILD_ID//_/-}
 
 gcloud container clusters get-credentials "${CLOUDSDK_CONTAINER_CLUSTER}"
 
+readonly OVERRIDES="
+{
+  \"spec\": {
+    \"template\":{
+      \"spec\": {
+        \"containers\": [{
+          \"name\":\"${POD_NAME}\",
+          \"image\":\"${IMAGE}\",
+          \"resources\": {
+            \"requests\": {
+              \"cpu\": \"0.6\",
+              \"memory\": \"1.5Gi\",
+              \"ephemeral-storage\": \"2Gi\"
+            }
+          }
+        }]
+      }
+    }
+  }
+}"
+
 kubectl run "${POD_NAME}" \
   --image="${IMAGE}" \
-  --pod-running-timeout=15m \
+  --pod-running-timeout=10m \
+  --overrides="${OVERRIDES}" \
   --restart=Never \
   --env="COMMIT_SHA=${COMMIT_SHA}" \
   --env="IMAGE_VERSION=${DATAPROC_IMAGE_VERSION}" \
   --command -- bash /init-actions/cloudbuild/presubmit.sh
 
-# Delete POD on exit and desribe it before deletion if exit was unsuccessful
+# Delete POD on exit and describe it before deletion if exit was unsuccessful
 trap '[[ $? != 0 ]] && kubectl describe "pod/${POD_NAME}"; kubectl delete pods "${POD_NAME}"' EXIT
 
 kubectl wait --for=condition=Ready "pod/${POD_NAME}" --timeout=600s
