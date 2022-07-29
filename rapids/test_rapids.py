dcb5830a46dff584947650b0cbec56cbe67a10a2
liyuan <84758614+nvliyuan@users.noreply.github.com>
2022-07-30 05:33:18 +0800

    [rapids] Update Spark RAPIDS to 22.06 and XGBoost to DMLC 1.6.1 (#999)
    Co-authored-by: Brad Miro <bmiro@google.com>
    Co-authored-by: Igor Dvorzhak <idv@google.com>

--- rapids/test_rapids.py
+++ rapids/test_rapids.py
@@ -1,5 +1,4 @@
 import os
-import pathlib
 
 import pkg_resources
 from absl.testing import absltest
@@ -12,60 +11,61 @@ class RapidsTestCase(DataprocTestCase):
   COMPONENT = "rapids"
   INIT_ACTIONS = ["gpu/install_gpu_driver.sh", "rapids/rapids.sh"]
   DASK_INIT_ACTIONS = [
-    "gpu/install_gpu_driver.sh", 
-    "dask/dask.sh",
-    "rapids/rapids.sh"]
+      "gpu/install_gpu_driver.sh", "dask/dask.sh", "rapids/rapids.sh"
+  ]
 
   GPU_P100 = "type=nvidia-tesla-p100"
 
   # Tests for RAPIDS init action
   DASK_RAPIDS_TEST_SCRIPT_FILE_NAME = "verify_rapids_dask.py"
-  SPARK_TEST_SCRIPT_FILE_NAME = "verify_rapids_spark.py"
-  XGBOOST_SPARK_TEST_SCRIPT_FILE_NAME = "verify_xgboost_spark.py"
+  XGBOOST_SPARK_TEST_SCRIPT_FILE_NAME = "verify_xgboost_spark.scala"
 
   def verify_dask_instance(self, name):
     self.assert_instance_command(
-      name, "pgrep -f dask-cuda-worker || "
-            "grep 'class: \"dask_cuda.CUDAWorker\"' /etc/dask/config.yaml")
+        name, "pgrep -f dask-cuda-worker || "
+        "grep 'class: \"dask_cuda.CUDAWorker\"' /etc/dask/config.yaml")
 
     self.upload_test_file(
         os.path.join(
-            os.path.dirname(os.path.abspath(__file__)), 
+            os.path.dirname(os.path.abspath(__file__)),
             self.DASK_RAPIDS_TEST_SCRIPT_FILE_NAME), name)
     verify_cmd = "/opt/conda/default/bin/python {}".format(
         self.DASK_RAPIDS_TEST_SCRIPT_FILE_NAME)
     self.assert_instance_command(name, verify_cmd)
-    self.remove_test_script(
-        self.DASK_RAPIDS_TEST_SCRIPT_FILE_NAME, name)
+    self.remove_test_script(self.DASK_RAPIDS_TEST_SCRIPT_FILE_NAME, name)
 
   def verify_spark_instance(self, name):
     self.assert_instance_command(name, "nvidia-smi")
 
   def verify_spark_job(self):
-    self.assert_dataproc_job(
-        self.name, "pyspark",
-        "{}/rapids/{}".format(self.INIT_ACTIONS_REPO,
-                              self.SPARK_TEST_SCRIPT_FILE_NAME))
-    if self.getImageVersion() > pkg_resources.parse_version("1.5"):
-      self.assert_dataproc_job(
-          self.name, "pyspark",
-          "{}/rapids/{}".format(self.INIT_ACTIONS_REPO,
-                                self.XGBOOST_SPARK_TEST_SCRIPT_FILE_NAME))
+    instance_name = "{}-m".format(self.getClusterName())
+    self.upload_test_file(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            self.XGBOOST_SPARK_TEST_SCRIPT_FILE_NAME), instance_name)
+    self.assert_instance_command(
+        instance_name, """echo :quit | spark-shell \
+         --conf spark.executor.resource.gpu.amount=1 \
+         --conf spark.task.resource.gpu.amount=1 \
+         --conf spark.dynamicAllocation.enabled=false -i {}""".format(
+             self.XGBOOST_SPARK_TEST_SCRIPT_FILE_NAME))
+    self.remove_test_script(self.XGBOOST_SPARK_TEST_SCRIPT_FILE_NAME,
+                            instance_name)
 
-  @parameterized.parameters(
-    ("STANDARD", ["m", "w-0"], GPU_P100, None),
-    ("STANDARD", ["m", "w-0"], GPU_P100, "yarn"),
-    ("STANDARD", ["m"], GPU_P100, "standalone"))
-  def test_rapids_dask(self, configuration, machine_suffixes, accelerator, dask_runtime):
-    if self.getImageOs() == 'rocky':
+  @parameterized.parameters(("STANDARD", ["m", "w-0"], GPU_P100, None),
+                            ("STANDARD", ["m", "w-0"], GPU_P100, "yarn"),
+                            ("STANDARD", ["m"], GPU_P100, "standalone"))
+  def test_rapids_dask(self, configuration, machine_suffixes, accelerator,
+                       dask_runtime):
+    if self.getImageOs() == "rocky":
       self.skipTest("Not supported in Rocky Linux-based images")
 
     if self.getImageVersion() < pkg_resources.parse_version("2.0"):
       self.skipTest("Not supported in pre 2.0 images")
 
-    metadata="gpu-driver-provider=NVIDIA,rapids-runtime=DASK"
+    metadata = "gpu-driver-provider=NVIDIA,rapids-runtime=DASK"
     if dask_runtime:
-      metadata+=",dask-runtime={}".format(dask_runtime)
+      metadata += ",dask-runtime={}".format(dask_runtime)
 
     self.createCluster(
         configuration,
@@ -80,11 +80,10 @@ class RapidsTestCase(DataprocTestCase):
       self.verify_dask_instance("{}-{}".format(self.getClusterName(),
                                                machine_suffix))
 
-  @parameterized.parameters(
-    ("SINGLE", ["m"], GPU_P100),
-    ("STANDARD", ["w-0"], GPU_P100))
+  @parameterized.parameters(("SINGLE", ["m"], GPU_P100),
+                            ("STANDARD", ["w-0"], GPU_P100))
   def test_rapids_spark(self, configuration, machine_suffixes, accelerator):
-    if self.getImageOs() == 'rocky':
+    if self.getImageOs() == "rocky":
       self.skipTest("Not supported in Rocky Linux-based images")
 
     optional_components = None
@@ -109,10 +108,10 @@ class RapidsTestCase(DataprocTestCase):
     # Only need to do this once
     self.verify_spark_job()
 
-  @parameterized.parameters(
-    ("STANDARD", ["m", "w-0"], GPU_P100, "11.2"))
-  def test_non_default_cuda_versions(self, configuration, machine_suffixes, accelerator, cuda_version):
-    if self.getImageOs() == 'rocky':
+  @parameterized.parameters(("STANDARD", ["m", "w-0"], GPU_P100, "11.2"))
+  def test_non_default_cuda_versions(self, configuration, machine_suffixes,
+                                     accelerator, cuda_version):
+    if self.getImageOs() == "rocky":
       self.skipTest("Not supported in Rocky Linux-based images")
 
     if self.getImageVersion() <= pkg_resources.parse_version("2.0"):
@@ -135,6 +134,7 @@ class RapidsTestCase(DataprocTestCase):
                                                 machine_suffix))
     # Only need to do this once
     self.verify_spark_job()
+
 
 if __name__ == "__main__":
   absltest.main()
