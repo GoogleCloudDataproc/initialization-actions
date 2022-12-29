c3b642d72ca8bd0b1bf1e30a5366ab54a99409ce
C.J. Collier <cjac@google.com>
2022-12-28 20:01:22 -0800

    tested with rapids version 22.10

--- rapids/rapids.sh
+++ rapids/rapids.sh
@@ -8,7 +8,7 @@ function get_metadata_attribute() {
   /usr/share/google/get_metadata_value "attributes/${attribute_name}" || echo -n "${default_value}"
 }
 
-readonly DEFAULT_DASK_RAPIDS_VERSION="22.08"
+readonly DEFAULT_DASK_RAPIDS_VERSION="22.10"
 readonly RAPIDS_VERSION=$(get_metadata_attribute 'rapids-version' ${DEFAULT_DASK_RAPIDS_VERSION})
 
 readonly SPARK_VERSION_ENV=$(spark-submit --version 2>&1 | sed -n 's/.*version[[:blank:]]\+\([0-9]\+\.[0-9]\).*/\1/p' | head -n1)
