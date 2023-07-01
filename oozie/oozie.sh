1474c9652f87d6387b89105a30b84b4ecaece8c5
C.J. Collier <cjac@google.com>
2023-06-30 17:08:04 -0700

    initial commit of patch set to support oozie.sh on Dataproc 2.1 clusters

--- oozie/oozie.sh
+++ oozie/oozie.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+# Copyright 2015,2016,2017,2018,2019,2020 Google LLC and contributors
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
