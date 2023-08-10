a81ab2ea64622bcafa37ef040eafcea3e50b4bbd
Akshat <akshatbhargavadeepa@gmail.com>
2023-08-10 22:48:53 +0530

    chore: Updated node name to assert the status command on

--- otel/test_otel.py
+++ otel/test_otel.py
@@ -9,7 +9,7 @@ class OpenTelemetryTestCase(DataprocTestCase):
   INIT_ACTIONS = ['otel/otel.sh']
 
   def verify_service_status(self):
-    instance_name = self.getClusterName() + 'm'
+    instance_name = self.getClusterName() + '-m'
     self.assert_instance_command(instance=instance_name, cmd="systemctl status otelcol-contrib")
 
   @parameterized.parameters(
