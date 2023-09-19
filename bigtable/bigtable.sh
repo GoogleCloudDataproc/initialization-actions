21764e63fa1058abd177e1401f652a4a7c637eb5
C.J. Collier <cjac@google.com>
2023-09-19 12:05:57 -0700

    [bigtable] Update bigtable for 2.1 images
    * update Copyright notice
    * update the name of the dataproc image version variable
    * update BIGTABLE_HBASE_CLIENT_1X_VERSION to '1.29.2'
    * update BIGTABLE_HBASE_CLIENT_2X_REPO to "https://repo1.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-2.x"
    * update BIGTABLE_HBASE_CLIENT_2X_VERSION to '2.7.4'
    * update BIGTABLE_HBASE_CLIENT_2X_JAR to "bigtable-hbase-2.x-${BIGTABLE_HBASE_CLIENT_2X_VERSION}.jar"
    * Specifying HBASE_VERSION as "2.4.17"
    * install_bigtable_client need not accept arguments
    * refactor hbase installation steps into install_hbase
    * add hbase installation steps for 2.1 clusters

--- bigtable/bigtable.sh
+++ bigtable/bigtable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#    Copyright 2018 Google, Inc.
+#    Copyright 2018,2023 Google LLC
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -21,17 +21,26 @@ set -euxo pipefail
 # Use Python from /usr/bin instead of /opt/conda.
 export PATH=/usr/bin:$PATH
 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+# Detect dataproc image version from its various names
+if (! test -v DATAPROC_IMAGE_VERSION) && test -v DATAPROC_VERSION; then
+  DATAPROC_IMAGE_VERSION="${DATAPROC_VERSION}"
+fi
+
 readonly HBASE_HOME='/usr/lib/hbase'
 
 readonly BIGTABLE_HBASE_CLIENT_1X_REPO="https://repo1.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-1.x-hadoop"
-readonly BIGTABLE_HBASE_CLIENT_1X_VERSION='1.26.1'
+readonly BIGTABLE_HBASE_CLIENT_1X_VERSION='1.29.2'
 readonly BIGTABLE_HBASE_CLIENT_1X_JAR="bigtable-hbase-1.x-hadoop-${BIGTABLE_HBASE_CLIENT_1X_VERSION}.jar"
 readonly BIGTABLE_HBASE_CLIENT_1X_URL="${BIGTABLE_HBASE_CLIENT_1X_REPO}/${BIGTABLE_HBASE_CLIENT_1X_VERSION}/${BIGTABLE_HBASE_CLIENT_1X_JAR}"
 
-readonly BIGTABLE_HBASE_CLIENT_2X_REPO="https://repo1.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-2.x-hadoop"
-readonly BIGTABLE_HBASE_CLIENT_2X_VERSION='1.26.1'
-readonly BIGTABLE_HBASE_CLIENT_2X_JAR="bigtable-hbase-2.x-hadoop-${BIGTABLE_HBASE_CLIENT_2X_VERSION}.jar"
+readonly BIGTABLE_HBASE_CLIENT_2X_REPO="https://repo1.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-2.x"
+readonly BIGTABLE_HBASE_CLIENT_2X_VERSION='2.7.4'
+readonly BIGTABLE_HBASE_CLIENT_2X_JAR="bigtable-hbase-2.x-${BIGTABLE_HBASE_CLIENT_2X_VERSION}.jar"
 readonly BIGTABLE_HBASE_CLIENT_2X_URL="${BIGTABLE_HBASE_CLIENT_2X_REPO}/${BIGTABLE_HBASE_CLIENT_2X_VERSION}/${BIGTABLE_HBASE_CLIENT_2X_JAR}"
+
+readonly HBASE_VERSION="2.4.17"
 
 readonly SCH_REPO="https://repo.hortonworks.com/content/groups/public/com/hortonworks"
 readonly SHC_VERSION='1.1.1-2.1-s_2.11'
@@ -61,8 +70,14 @@ function err() {
 }
 
 function install_bigtable_client() {
-  local -r bigtable_hbase_client_jar="$1"
-  local -r bigtable_hbase_client_url="$2"
+  if [[ ${DATAPROC_IMAGE_VERSION%%.*} -ge 2 ]]; then
+    local -r bigtable_hbase_client_jar="$BIGTABLE_HBASE_CLIENT_2X_JAR"
+    local -r bigtable_hbase_client_url="$BIGTABLE_HBASE_CLIENT_2X_URL"
+  else
+    local -r bigtable_hbase_client_jar="$BIGTABLE_HBASE_CLIENT_1X_JAR"
+    local -r bigtable_hbase_client_url="$BIGTABLE_HBASE_CLIENT_1X_URL"
+  fi
+
   local out="${HBASE_HOME}/lib/${bigtable_hbase_client_jar}"
   wget -nv --timeout=30 --tries=5 --retry-connrefused \
     "${bigtable_hbase_client_url}" -O "${out}"
@@ -155,27 +170,78 @@ SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/usr/lib/hbase/lib/*"
 SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/etc/hbase/conf"
 EOF
 
-  if [[ ${DATAPROC_VERSION%%.*} -ge 2 ]]; then
+  if [[ ${DATAPROC_IMAGE_VERSION%%.*} -ge 2 ]]; then
     configure_bigtable_client_2x || err 'Failed to configure big table 2.x client.'
   else
     configure_bigtable_client_1x || err 'Failed to configure big table 1.x client.'
   fi
 }
 
-function main() {
+function install_hbase() {
   if command -v apt-get >/dev/null; then
-    retry_command "apt-get update" || err 'Unable to update packages lists.'
-    retry_command "apt-get install -y hbase" || err 'Unable to install HBase.'
+    case "${DATAPROC_IMAGE_VERSION}" in
+      "1.3" | "1.4" | "1.5" | "2.0" )
+
+        # Prior to Dataproc 2.1, hbase could be installed from dpkg repositories
+        retry_command "apt-get update" || err 'Unable to update packages lists.'
+        retry_command "apt-get install -y hbase" || err 'Unable to install HBase.'
+        ;;
+
+      "2.1")
+        # get hbase tar from official site
+        wget "https://dlcdn.apache.org/hbase/${HBASE_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz" -P /tmp || err 'Unable to download tar'
+        # extract binaries from bundle
+        tar xzf "/tmp/hbase-${HBASE_VERSION}-bin.tar.gz" -C /tmp/
+        mkdir -p "${HBASE_HOME}"
+        cp -a "/tmp/hbase-${HBASE_VERSION}/." "${HBASE_HOME}/"
+
+      #   DEPENDENCIES="maven"
+      #   local tmp_dir=$(mktemp -d -t bigtable-init-action-hbase-XXXX)
+      #   HBASE_SPARK_JAR="${tmp_dir}/hbase-connectors/spark/hbase-spark/target/hbase-spark-1.0.1-SNAPSHOT.jar"
+
+      #   # This repository only works for cloudera clusters
+      #   # wget https://archive.cloudera.com/p/HDP/2.x/2.6.3.0/${distribution}/hdp.list -O /etc/apt/sources.list.d/hdp.list
+
+      #   retry_command "apt-get update" || err 'Unable to update packages lists.'
+      #   retry_command "apt-get install -y ${DEPENDENCIES}" || err 'Unable to install dependencies.'
+
+      #   cd $tmp_dir
+      #   git clone https://github.com/apache/hbase-connectors.git
+      #   cd hbase-connectors
+      #   # These versions are from https://cloud.google.com/dataproc/docs/concepts/versioning/dataproc-release-2.1
+      #   # except hbase, which is the maximal stable version from https://hbase.apache.org/downloads.html
+      #   mvn -Dspark.version=3.3.2 \
+      #     -Dscala.version=2.12.14 \
+      #     -Dhadoop-three.version=3.3.3 \
+      #     -Dscala.binary.version=2.12 \
+      #     -Dhbase.version=2.5.5-hadoop3 \
+      #     clean install \
+      #     -DskipTests
+      #   cp "${HBASE_SPARK_JAR}" /usr/lib/spark/
+
+      #   # clean up a bit
+      #   mvn dependency:purge-local-repository
+      #   apt-get remove -y maven
+      #   apt-get clean
+        ;;
+      "*")
+        echo "unsupported DATAPROC_IMAGE_VERSION: ${DATAPROC_IMAGE_VERSION}" >&2
+        exit 1
+        ;;
+    esac
   else
     retry_command "yum -y update" || err 'Unable to update packages lists.'
     retry_command "yum -y install hbase" || err 'Unable to install HBase.'
   fi
+}
 
-  if [[ ${DATAPROC_VERSION%%.*} -ge 2 ]]; then
-    install_bigtable_client "$BIGTABLE_HBASE_CLIENT_2X_JAR" "$BIGTABLE_HBASE_CLIENT_2X_URL" || err 'Unable to install big table client.'
-  else
-    install_bigtable_client "$BIGTABLE_HBASE_CLIENT_1X_JAR" "$BIGTABLE_HBASE_CLIENT_1X_URL" || err 'Unable to install big table client.'
+function main() {
 
+  # Install HBASE
+  install_hbase || err 'Failed to install HBase.'
+  install_bigtable_client || err 'Unable to install big table client.'
+
+  if [[ ${DATAPROC_IMAGE_VERSION%%.*} -lt 2 ]]; then
     install_shc || err 'Failed to install Spark-HBase connector.'
   fi
 
