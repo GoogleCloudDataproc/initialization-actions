9824caa099eabc6ee676713cd7fb839a412e3466
C.J. Collier <cjac@google.com>
2023-10-19 15:28:13 -0700

    [bigtable] Update to work with 2.0+ (#1099)
    * [bigtable] Update to work with 2.1+
    
    * updated copyright date
    * set variable distribution
    * updated BIGTABLE_HBASE_CLIENT_1X_VERSION to latest, '1.29.2'
    * ensured that BIGTABLE_INSTANCE metadata variable is supplied
    * simplified call to install_bigtable_client to remove requirement for arguments
    * Create "${HBASE_HOME}/conf/hbase-site.xml" if not exist
    * renamed DATAPROC_VERSION variable to DATAPROC_IMAGE_VERSION
    * moved hbase installation logic out of function main into a new function install_hbase
    * using hbase apt-get logic only for dataproc versions < 2.0
    * developed new hbase installation logic for dataproc versions >= 2.0
    * specifying HBASE_VERSION="2.3.6"
    * reporting to customer when installation to unsupported version of dataproc is attempted
    * use differing Bigtable*Registry class for hbase versions < vs >= 2.3.6
    * install hbase client 2x on dataproc 2.0+
    * integrated tests from BigTable product eng team
    * now using bigtable-hbase client version 2.12.0
    * this should work for rocky

--- bigtable/bigtable.sh
+++ bigtable/bigtable.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#    Copyright 2018 Google, Inc.
+#    Copyright 2018,2023 Google LLC
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -21,15 +21,23 @@ set -euxo pipefail
 # Use Python from /usr/bin instead of /opt/conda.
 export PATH=/usr/bin:$PATH
 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+
+# Detect dataproc image version from its various names
+if (! test -v DATAPROC_IMAGE_VERSION) && test -v DATAPROC_VERSION; then
+  DATAPROC_IMAGE_VERSION="${DATAPROC_VERSION}"
+fi
+
 readonly HBASE_HOME='/usr/lib/hbase'
+mkdir -p "${HBASE_HOME}/{lib,conf,logs}"
 
 readonly BIGTABLE_HBASE_CLIENT_1X_REPO="https://repo1.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-1.x-hadoop"
-readonly BIGTABLE_HBASE_CLIENT_1X_VERSION='1.26.1'
+readonly BIGTABLE_HBASE_CLIENT_1X_VERSION='1.29.2'
 readonly BIGTABLE_HBASE_CLIENT_1X_JAR="bigtable-hbase-1.x-hadoop-${BIGTABLE_HBASE_CLIENT_1X_VERSION}.jar"
 readonly BIGTABLE_HBASE_CLIENT_1X_URL="${BIGTABLE_HBASE_CLIENT_1X_REPO}/${BIGTABLE_HBASE_CLIENT_1X_VERSION}/${BIGTABLE_HBASE_CLIENT_1X_JAR}"
 
 readonly BIGTABLE_HBASE_CLIENT_2X_REPO="https://repo1.maven.org/maven2/com/google/cloud/bigtable/bigtable-hbase-2.x-hadoop"
-readonly BIGTABLE_HBASE_CLIENT_2X_VERSION='1.26.1'
+readonly BIGTABLE_HBASE_CLIENT_2X_VERSION='2.12.0'
 readonly BIGTABLE_HBASE_CLIENT_2X_JAR="bigtable-hbase-2.x-hadoop-${BIGTABLE_HBASE_CLIENT_2X_VERSION}.jar"
 readonly BIGTABLE_HBASE_CLIENT_2X_URL="${BIGTABLE_HBASE_CLIENT_2X_REPO}/${BIGTABLE_HBASE_CLIENT_2X_VERSION}/${BIGTABLE_HBASE_CLIENT_2X_JAR}"
 
@@ -41,6 +49,10 @@ readonly SHC_URL="${SCH_REPO}/shc-core/${SHC_VERSION}/${SHC_JAR}"
 readonly SHC_EXAMPLES_URL="${SCH_REPO}/shc-examples/${SHC_VERSION}/${SHC_EXAMPLES_JAR}"
 
 readonly BIGTABLE_INSTANCE="$(/usr/share/google/get_metadata_value attributes/bigtable-instance)"
+if [[ -z "${BIGTABLE_INSTANCE}" ]]; then
+  echo "failed to determine bigtable-instance attribute ; https://github.com/GoogleCloudDataproc/initialization-actions/tree/master/bigtable#using-this-initialization-action"
+  exit 1
+fi
 readonly BIGTABLE_PROJECT="$(/usr/share/google/get_metadata_value attributes/bigtable-project ||
     /usr/share/google/get_metadata_value ../project/project-id)"
 
@@ -61,8 +73,14 @@ function err() {
 }
 
 function install_bigtable_client() {
-  local -r bigtable_hbase_client_jar="$1"
-  local -r bigtable_hbase_client_url="$2"
+  if [[ $(echo "${DATAPROC_IMAGE_VERSION} >= 2.0" | bc -l) == 1 ]]; then
+    local -r bigtable_hbase_client_jar="$BIGTABLE_HBASE_CLIENT_2X_JAR"
+    local -r bigtable_hbase_client_url="$BIGTABLE_HBASE_CLIENT_2X_URL"
+  else
+    local -r bigtable_hbase_client_jar="$BIGTABLE_HBASE_CLIENT_1X_JAR"
+    local -r bigtable_hbase_client_url="$BIGTABLE_HBASE_CLIENT_1X_URL"
+  fi
+
   local out="${HBASE_HOME}/lib/${bigtable_hbase_client_jar}"
   wget -nv --timeout=30 --tries=5 --retry-connrefused \
     "${bigtable_hbase_client_url}" -O "${out}"
@@ -103,13 +121,25 @@ EOF
 </configuration>
 EOF
 
-  bdconfig merge_configurations \
-    --configuration_file "${HBASE_HOME}/conf/hbase-site.xml" \
-    --source_configuration_file "$hbase_config" \
-    --clobber
+  if [[ -f "${HBASE_HOME}/conf/hbase-site.xml" ]]; then
+    bdconfig merge_configurations \
+      --configuration_file "${HBASE_HOME}/conf/hbase-site.xml" \
+      --source_configuration_file "$hbase_config" \
+      --clobber
+  else
+    cp "$hbase_config" "${HBASE_HOME}/conf/hbase-site.xml"
+  fi
 }
 
 function configure_bigtable_client_2x() {
+
+  INSTALLED_HBASE_VERSION=`hbase version | perl -ne 'print $1 if /^HBase (.+)$/'`
+  if [[ $(echo "${INSTALLED_HBASE_VERSION%.*} < 2.3" | bc -l) == 1 ]]; then
+    BIGTABLE_REGISTRY="BigtableAsyncRegistry"
+  else
+    BIGTABLE_REGISTRY="BigtableConnectionRegistry"
+  fi
+
   local -r hbase_config=$(mktemp /tmp/hbase-site.xml-XXXX)
   cat <<EOF >${hbase_config}
 <?xml version="1.0"?>
@@ -123,7 +153,7 @@ function configure_bigtable_client_2x() {
   </property>
   <property>
     <name>hbase.client.registry.impl</name>
-    <value>org.apache.hadoop.hbase.client.BigtableAsyncRegistry</value>
+    <value>org.apache.hadoop.hbase.client.${BIGTABLE_REGISTRY}</value>
   </property>
   <property>
     <name>hbase.client.async.connection.impl</name>
@@ -135,10 +165,15 @@ function configure_bigtable_client_2x() {
 </configuration>
 EOF
 
-  bdconfig merge_configurations \
-    --configuration_file "${HBASE_HOME}/conf/hbase-site.xml" \
-    --source_configuration_file "$hbase_config" \
-    --clobber
+  if [[ -f "${HBASE_HOME}/conf/hbase-site.xml" ]]; then
+    bdconfig merge_configurations \
+      --configuration_file "${HBASE_HOME}/conf/hbase-site.xml" \
+      --source_configuration_file "$hbase_config" \
+      --clobber
+  else
+    cp "$hbase_config" "${HBASE_HOME}/conf/hbase-site.xml"
+  fi
+
 }
 
 function configure_bigtable_client() {
@@ -155,30 +190,62 @@ SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/usr/lib/hbase/lib/*"
 SPARK_DIST_CLASSPATH="${SPARK_DIST_CLASSPATH}:/etc/hbase/conf"
 EOF
 
-  if [[ ${DATAPROC_VERSION%%.*} -ge 2 ]]; then
+  if [[ $(echo "${DATAPROC_IMAGE_VERSION} >= 2.0" | bc -l) == 1 ]]; then
     configure_bigtable_client_2x || err 'Failed to configure big table 2.x client.'
   else
     configure_bigtable_client_1x || err 'Failed to configure big table 1.x client.'
   fi
 }
 
+function install_hbase() {
+  case "${DATAPROC_IMAGE_VERSION}" in
+    "1.3" | "1.4" | "1.5" )
+      if command -v apt-get >/dev/null; then
+        # On images prior to Dataproc 2.1, hbase 1.x can be installed from Google's bigtop repositories
+        retry_command "apt-get update" || err 'Unable to update packages lists.'
+        retry_command "apt-get install -y hbase" || err 'Unable to install HBase.'
+      else
+        retry_command "yum -y update" || err 'Unable to update packages lists.'
+        retry_command "yum -y install hbase" || err 'Unable to install HBase.'
+      fi
+      ;;
+
+    "2.0" | "2.1" | "2.2" )
+
+      # get hbase tar from official site
+      # Variants:
+      # * hadoop3-client-bin
+      # * hadoop3-bin
+      # * client-bin
+      # * bin
+      echo "preparing to install hbase"
+      local HBASE_VERSION="2.3.6"
+      local VARIANT="bin"
+      local BASENAME="hbase-${HBASE_VERSION}-${VARIANT}.tar.gz"
+      echo "hbase dist basename: ${BASENAME}"
+      wget "https://archive.apache.org/dist/hbase/${HBASE_VERSION}/${BASENAME}" -P /tmp || err 'Unable to download tar'
+
+      # extract binaries from bundle
+      mkdir -p "/tmp/hbase-${HBASE_VERSION}/" "${HBASE_HOME}"
+      tar xzf "/tmp/${BASENAME}" --strip-components=1 -C "/tmp/hbase-${HBASE_VERSION}/"
+      cp -a "/tmp/hbase-${HBASE_VERSION}/." "${HBASE_HOME}/"
+
+      # install hbase and hbase-config.sh into normal user $PATH
+      ln -sf ${HBASE_HOME}/bin/hbase /usr/bin/
+      ln -sf ${HBASE_HOME}/bin/hbase-config.sh /usr/bin/
+      echo "hbase binary distribution installed"
+      ;;
+    "*")
+      echo "unsupported DATAPROC_IMAGE_VERSION: ${DATAPROC_IMAGE_VERSION}" >&2
+      exit 1
+      ;;
+  esac
+}
+
 function main() {
-  if command -v apt-get >/dev/null; then
-    retry_command "apt-get update" || err 'Unable to update packages lists.'
-    retry_command "apt-get install -y hbase" || err 'Unable to install HBase.'
-  else
-    retry_command "yum -y update" || err 'Unable to update packages lists.'
-    retry_command "yum -y install hbase" || err 'Unable to install HBase.'
-  fi
-
-  if [[ ${DATAPROC_VERSION%%.*} -ge 2 ]]; then
-    install_bigtable_client "$BIGTABLE_HBASE_CLIENT_2X_JAR" "$BIGTABLE_HBASE_CLIENT_2X_URL" || err 'Unable to install big table client.'
-  else
-    install_bigtable_client "$BIGTABLE_HBASE_CLIENT_1X_JAR" "$BIGTABLE_HBASE_CLIENT_1X_URL" || err 'Unable to install big table client.'
-
-    install_shc || err 'Failed to install Spark-HBase connector.'
-  fi
-
+  install_hbase             || err 'Failed to install HBase.'
+  install_bigtable_client   || err 'Unable to install big table client.'
+  install_shc               || err 'Failed to install Spark-HBase connector.'
   configure_bigtable_client || err 'Failed to configure big table client.'
 }
 
