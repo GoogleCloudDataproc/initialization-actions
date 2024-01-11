d3813a696dedd5d730793853d61c5b9a77decb80
Vamshi Krishna Gajendram <gajendrum@gmail.com>
2024-01-11 08:46:56 +0530

    Skip restart in opsagent/opsagent.sh

--- opsagent/opsagent.sh
+++ opsagent/opsagent.sh
@@ -28,5 +28,3 @@ fi
 
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 bash add-google-cloud-ops-agent-repo.sh --also-install
-
-systemctl restart google-cloud-ops-agent
