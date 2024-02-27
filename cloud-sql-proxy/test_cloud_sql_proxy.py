a23e906b3eb2c6842562f619380525fcde5bc2ac
Prince Datta <prince.datta@cloudsufi.com>
2024-02-27 22:32:27 +0530

    code refactor

--- cloud-sql-proxy/test_cloud_sql_proxy.py
+++ cloud-sql-proxy/test_cloud_sql_proxy.py
@@ -53,7 +53,8 @@ class CloudSqlProxyTestCase(DataprocTestCase):
     self.DATABASE_VERSION_POSTGRESQL = 'POSTGRES_14'
     self.INSTANCE_POSTGRESQL = 'postgresql'
     create_cmd_fmt = 'gcloud sql instances create {}' \
-                     ' --region {} --async --format=json --database-version={} --tier=db-f1-micro'
+                     ' --region {} --async --format=json --database-version={} --tier=db-perf-optimized-N-4' \
+                     '--edition=ENTERPRISE_PLUS'
     _, stdout, _ = self.assert_command(
       create_cmd_fmt.format(self.DB_NAME_POSTGRESQL, self.REGION, self.DATABASE_VERSION_POSTGRESQL))
     operation_id = json.loads(stdout.strip())['name']
@@ -81,12 +82,13 @@ class CloudSqlProxyTestCase(DataprocTestCase):
 
     self.verify_cluster(self.getClusterName())
     self.name = None
+    self.assert_command('gcloud dataproc clusters delete {} --region={}'.format(self.getClusterName(), self.REGION))
 
   def verify_postgresql(self, configuration):
-    metadata = 'hive-metastore-instance={}:{},hive-metastore-db=metastore'.format(self.PROJECT_METADATA,
-                                                                                  self.DB_NAME_POSTGRESQL)
+    # metadata = 'hive-metastore-instance={}:{},hive-metastore-db=metastore'.format(self.PROJECT_METADATA,
+    #                                                                               self.DB_NAME_POSTGRESQL)
     self.createCluster(
-      configuration, self.INIT_ACTIONS, metadata=metadata, scopes='sql-admin')
+      configuration, self.INIT_ACTIONS, scopes='sql-admin')
 
     self.verify_cluster(self.getClusterName())
 
