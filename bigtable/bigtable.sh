d8fccf06ef779ec50851798e6ac700deef059122
kuldeepkk-dev <111517881+kuldeepkk-dev@users.noreply.github.com>
2024-04-18 00:17:41 -0400

    Fix for bigtable.sh - Debian backports repo issue.

--- bigtable/bigtable.sh
+++ bigtable/bigtable.sh
@@ -261,7 +261,7 @@ function install_hbase() {
 }
 
 function main() {
-  if [[ ${OS_NAME} == debian ]]; then
+  if [[ ${OS_NAME} == debian ]] && [[ ${DATAPROC_IMAGE_VERSION} == 2.0 || ${DATAPROC_IMAGE_VERSION} == 1.5 ]]; then
     remove_old_backports
   fi
   install_hbase             || err 'Failed to install HBase.'
