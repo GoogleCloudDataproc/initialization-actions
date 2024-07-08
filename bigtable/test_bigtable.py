57595cdb7b0d043d0429455361672d4b612f05d4
C.J. Collier <cjac@google.com>
2024-07-08 14:02:08 -0700

    default init action timeout is 15m

--- bigtable/test_bigtable.py
+++ bigtable/test_bigtable.py
@@ -70,7 +70,7 @@ class BigTableTestCase(DataprocTestCase):
     )
     def test_bigtable(self, configuration, machine_suffixes):
         self.createCluster(
-            configuration, self.INIT_ACTIONS, metadata=self.metadata)
+            configuration, self.INIT_ACTIONS, metadata=self.metadata, timeout_in_minutes=15)
 
         for machine_suffix in machine_suffixes:
             self.verify_instance("{}-{}".format(self.getClusterName(),
