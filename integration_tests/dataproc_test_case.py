2b81095864b3a2e64efa171382a496c8efcd64e7
C.J. Collier <cjac@google.com>
2024-07-30 12:39:33 -0700

    adjusting time limits

--- integration_tests/dataproc_test_case.py
+++ integration_tests/dataproc_test_case.py
@@ -170,7 +170,9 @@ class DataprocTestCase(parameterized.TestCase):
           args.append("--zone={}".format(self.cluster_zone))
 
         if not FLAGS.skip_cleanup:
-            args.append("--max-age=7m")
+            args.append("--max-age=30m")
+
+        args.append("--max-idle=5m")
 
         cmd = "{} dataproc clusters create {} {}".format(
             "gcloud beta" if beta else "gcloud", self.name, " ".join(args))
