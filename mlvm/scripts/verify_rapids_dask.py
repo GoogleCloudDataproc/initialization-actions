9e2697603ffc7cec1b4bc4b5729c98a208ad70cc
Prince Datta <prince.datta@cloudsufi.com>
2024-08-05 10:17:17 +0530

    Fixing dask rapids verification script.

--- mlvm/scripts/verify_rapids_dask.py
+++ mlvm/scripts/verify_rapids_dask.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import dask.dataframe as dd
 import xgboost as xgb
 
-# Confirm XGBoost is available
 # Create a Pandas DataFrame
 df = pd.DataFrame({
     'a': [0, 1, 2],
@@ -10,12 +8,6 @@ df = pd.DataFrame({
 })
 df['c'] = df['a'] * df['b'] + 100
 
-# Convert the DataFrame to a DMatrix for XGBoost
 dmat = xgb.DMatrix(df)
 
-# Confirm Dask is available
-# Convert the DataFrame to a Dask DataFrame
-ds = dd.from_pandas(df['c'], npartitions=2)
-
-# Compute the Dask DataFrame
-computed_ds = ds.compute()
+computed_df = df['c']
