d6a86cb7a5b263d1f92a1b2ed177c2d93b74ab1e
C.J. Collier <cjac@google.com>
2024-10-10 12:32:07 -0700

    added comments for difficut to understand functions

--- gpu/install_gpu_driver.sh
+++ gpu/install_gpu_driver.sh
@@ -56,8 +56,10 @@ function remove_old_backports {
   done
 }
 
+# Return true if the first argument is equal to or less than the second argument
 function compare_versions_lte { [ "$1" = "$(echo -e "$1\n$2" | sort -V | head -n1)" ] ; }
 
+# Return true if the first argument is less than the second argument
 function compare_versions_lt() {
   [ "$1" = "$2" ] && return 1 || compare_versions_lte $1 $2
 }
