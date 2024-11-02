b8f631b12f0383d261d3b5ffaf3b44d2241777b2
Prince Datta <prince.datta@cloudsufi.com>
2024-11-02 22:46:40 +0530

    Enable kerberos for gpu driver init action.

--- gpu/test_gpu.py
+++ gpu/test_gpu.py
@@ -38,6 +38,7 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
       ("SINGLE", ["m"], GPU_T4, None, None),
       ("STANDARD", ["m"], GPU_T4, None, None),
       ("STANDARD", ["m", "w-0", "w-1"], GPU_T4, GPU_T4, "NVIDIA"),
+      ("KERBEROS", ["m"], GPU_T4, None, None),
   )
   def test_install_gpu_default_agent(self, configuration, machine_suffixes,
                                      master_accelerator, worker_accelerator,
@@ -61,6 +62,7 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
 
   @parameterized.parameters(
       ("SINGLE", ["m"], GPU_T4, None, None),
+      ("KERBEROS", ["m"], GPU_T4, None, None),
   )
   def test_install_gpu_without_agent(self, configuration, machine_suffixes,
                                      master_accelerator, worker_accelerator,
@@ -86,6 +88,7 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
       ("STANDARD", ["m", "w-0", "w-1"], GPU_T4, GPU_T4, None),
       ("STANDARD", ["w-0", "w-1"], None, GPU_T4, "NVIDIA"),
       ("STANDARD", ["m"], GPU_T4, None, "NVIDIA"),
+      ("KERBEROS", ["m"], GPU_T4, None, None),
   )
   def test_install_gpu_with_agent(self, configuration, machine_suffixes,
                                   master_accelerator, worker_accelerator,
@@ -115,6 +118,7 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
       ("STANDARD", ["m"],               GPU_T4, None,   "11.8"),
       ("STANDARD", ["m", "w-0", "w-1"], GPU_T4, GPU_T4, "12.4"),
       ("STANDARD", ["w-0", "w-1"],      None,   GPU_T4, "11.8"),
+      ("KERBEROS", ["m"],               GPU_T4, None,   "12.4"),
   )
   def test_install_gpu_cuda_nvidia(self, configuration, machine_suffixes,
                                    master_accelerator, worker_accelerator,
@@ -138,6 +142,7 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
   @parameterized.parameters(
       ("STANDARD", ["m"], GPU_H100, GPU_A100, "NVIDIA", "12.4"),
       ("STANDARD", ["m"], GPU_H100, GPU_A100, "NVIDIA", "11.8"),
+      ("KERBEROS", ["m"], GPU_H100, GPU_A100, "NVIDIA", "12.4"),
   )
   def test_install_gpu_with_mig(self, configuration, machine_suffixes,
                                   master_accelerator, worker_accelerator,
@@ -164,7 +169,8 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
 
   @parameterized.parameters(
       ("SINGLE", GPU_T4, None, None),
-      ("STANDARD", GPU_T4, GPU_T4, "NVIDIA")
+      ("STANDARD", GPU_T4, GPU_T4, "NVIDIA"),
+      ("KERBEROS", GPU_T4, GPU_T4, "NVIDIA"),
   )
   def test_gpu_allocation(self, configuration, master_accelerator,
                           worker_accelerator, driver_provider):
@@ -204,6 +210,7 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
     ("STANDARD", ["m", "w-0", "w-1"], GPU_T4, GPU_T4, "11.8"),
     ("STANDARD", ["w-0", "w-1"], None, GPU_T4, "11.8"),
     ("STANDARD", ["w-0", "w-1"], None, GPU_T4, "12.4"),
+    ("KERBEROS", ["m"], GPU_T4, None, "12.4"),
   )
   def test_install_gpu_cuda_nvidia_with_spark_job(self, configuration, machine_suffixes,
                                    master_accelerator, worker_accelerator,
