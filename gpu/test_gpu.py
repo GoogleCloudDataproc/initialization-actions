70d118082e13c13e5c8197a1c85ae42a6c2dcb42
Prince Datta <prince.datta@cloudsufi.com>
2024-11-30 18:41:27 +0530

    enable kerberos for gpu drivers

--- gpu/test_gpu.py
+++ gpu/test_gpu.py
@@ -148,7 +148,6 @@ class NvidiaGpuDriverTestCase(DataprocTestCase):
 #       ("SINGLE", ["m"],               GPU_T4, None,   "12.0"),
         ("SINGLE", ["m"],               GPU_T4, None,   "11.8"),
       ("STANDARD", ["m", "w-0", "w-1"], GPU_T4, GPU_T4, "12.4"),
-      ("KERBEROS", ["m"],               GPU_T4, GPU_T4, "12.4"),
 #     ("STANDARD", ["w-0", "w-1"],      None,   GPU_T4, "11.8"),
   )
   def test_install_gpu_cuda_nvidia(self, configuration, machine_suffixes,
