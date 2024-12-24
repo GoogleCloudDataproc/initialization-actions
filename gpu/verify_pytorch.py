8b4f4f8623d241d4a022a9346e893279d32cd1ce
C.J. Collier <cjac@google.com>
2024-12-24 09:39:58 -0800

    can include extended test suite later

--- gpu/verify_pytorch.py
+++
@@ -1,8 +0,0 @@
-import torch
-print("get CUDA details : == : ")
-use_cuda = torch.cuda.is_available()
-if use_cuda:
-    print('__CUDNN VERSION:', torch.backends.cudnn.version())
-    print('__Number CUDA Devices:', torch.cuda.device_count())
-    print('__CUDA Device Name:',torch.cuda.get_device_name(0))
-    print('__CUDA Device Total Memory [GB]:',torch.cuda.get_device_properties(0).total_memory/1e9)
