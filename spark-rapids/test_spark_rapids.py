3245fedfafd856fc78c21082700818d6ad966f61
C.J. Collier <cjac@google.com>
2024-12-27 17:49:37 -0800

    different defaults are now selected based on dataproc image version and so installation should succeed

--- spark-rapids/test_spark_rapids.py
+++ spark-rapids/test_spark_rapids.py
@@ -62,9 +62,6 @@ class SparkRapidsTestCase(DataprocTestCase):
     and configuration == 'SINGLE':
       self.skipTest("2.1-rocky8 and 2.0-rocky8 single instance tests are known to fail")
 
-    if self.getImageVersion() <= pkg_resources.parse_version("2.0"):
-      self.skipTest("Not supported in 2.0 and earlier images")
-
     optional_components = None
     metadata = "gpu-driver-provider=NVIDIA,rapids-runtime=SPARK"
 
@@ -93,9 +90,6 @@ class SparkRapidsTestCase(DataprocTestCase):
     and configuration == 'SINGLE':
       self.skipTest("2.1-rocky8 and 2.0-rocky8 single instance tests are known to fail")
 
-    if self.getImageVersion() <= pkg_resources.parse_version("2.0"):
-      self.skipTest("Not supported in 2.0 and earlier images")
-
     optional_components = None
     metadata = "gpu-driver-provider=NVIDIA,rapids-runtime=SPARK"
 
@@ -120,8 +114,10 @@ class SparkRapidsTestCase(DataprocTestCase):
   def test_non_default_cuda_versions(self, configuration, machine_suffixes,
                                      accelerator, cuda_version, driver_version):
 
-    if self.getImageVersion() <= pkg_resources.parse_version("2.0"):
-      self.skipTest("Not supported in 2.0 and earlier images")
+    if pkg_resources.parse_version(cuda_version) > pkg_resources.parse_version("12.1.1") \
+    and ( ( self.getImageOs() == 'ubuntu' and self.getImageVersion() <= pkg_resources.parse_version("2.0") ) or \
+          ( self.getImageOs() == 'debian' and self.getImageVersion() <= pkg_resources.parse_version("2.1") ) ):
+      self.skipTest("CUDA > 12.1.1 not supported on older debian/ubuntu releases")
 
     metadata = ("gpu-driver-provider=NVIDIA,rapids-runtime=SPARK"
                 ",cuda-version={0},driver-version={1}".format(cuda_version, driver_version))
