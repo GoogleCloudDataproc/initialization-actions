c05a7460275106c77973ec5496c5f3ee9223ea0d
Pahulpreet Singh <pahulpreets@google.com>
2025-01-28 02:26:42 +0000

    address TODOs and add license header
    Signed-off-by: Pahulpreet Singh <pahulpreets@google.com>

--- hive-lineage/README.md
+++ hive-lineage/README.md
@@ -15,13 +15,11 @@ CLUSTER_NAME=<cluster_name>
 gcloud dataproc clusters create ${CLUSTER_NAME} \
   --region ${REGION} \
   --scopes cloud-platform \
-  --initialization-actions gs://dataproc-hive-lineage-prototype/v3/initialization-actions/enable_lineage.sh
+  --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/hive-lineage/hive-lineage.sh
 ```
 
-(TODO: Update the gs bucket with the regionalized `goog-dataproc-initialization-actions-`)
-
 If you want to run Hive jobs involving bigquery tables, hive-bigquery connector needs to be installed as well.
-(TODO: Link to connectors init action)
+See [connectors](../connectors/README.md) init action.
 
 ```shell 
 REGION=<region>
@@ -29,8 +27,6 @@ CLUSTER_NAME=<cluster_name>
 gcloud dataproc clusters create ${CLUSTER_NAME} \
   --region ${REGION} \
   --scopes cloud-platform \
-  --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/connectors/connectors.sh,gs://dataproc-hive-lineage-prototype/v3/initialization-actions/enable_lineage.sh \
+  --initialization-actions gs://goog-dataproc-initialization-actions-${REGION}/connectors/connectors.sh,gs://goog-dataproc-initialization-actions-${REGION}/hive-lineage/hive-lineage.sh \
   --metadata hive-bigquery-connector-version=2.0.3
 ```
-
-(TODO: Once multiple versions are supported, update instructions accordingly)
--- hive-lineage/hive-lineage.sh
+++ hive-lineage/hive-lineage.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #   This initialization script installs the required
 #   jars and sets the hive conf to enable lineage.
-#
-#   To use this script, add the following using the
-#   --initialization-actions flag during cluster creation.
-#   gs://dataproc-hive-lineage-prototype/v3/initialization-actions/enable_lineage.sh
 
 set -euxo pipefail
 
@@ -13,14 +21,15 @@ export HIVE_HOME="/usr/lib/hive"
 export HIVE_CONF_DIR="/etc/hive/conf"
 export HIVE_CONF_FILE="$HIVE_CONF_DIR/hive-site.xml"
 export HIVE_LIB_DIR="/usr/lib/hive/lib"
-export INSTALLATION_SOURCE="gs://dataproc-hive-lineage-prototype/v3/jars" # TODO: Update the gcs bucket once finalised
+export INSTALLATION_SOURCE="gs://hadoop-lib/hive-lineage"
+export HIVE_OL_HOOK_VERSION="1.0.0-preview"
 export HIVE_OL_HOOK="io.openlineage.hive.hooks.HiveOpenLineageHook"
 
 
 function set_hive_lineage_conf() {
   declare -A properties=(
     ["hive.exec.post.hooks"]="$HIVE_OL_HOOK"
-    ["hive.exec.exec.hooks"]="$HIVE_OL_HOOK"
+    ["hive.exec.failure.hooks"]="$HIVE_OL_HOOK"
     ["hive.openlineage.transport.type"]="gcplineage"
     ["hive.conf.validation"]="false" # to allow custom properties, like hive.openlineage.namespace
   )
@@ -34,9 +43,8 @@ function set_hive_lineage_conf() {
 }
 
 function install_jars() {
-  # TODO: Allow customisation of the jar version
   echo "Installing openlineage-hive hook"
-  gsutil cp -P "$INSTALLATION_SOURCE/hive-openlineage-hook-shaded.jar" "$HIVE_LIB_DIR/hive-openlineage-hook.jar"
+  gsutil cp -P "$INSTALLATION_SOURCE/hive-openlineage-hook-$HIVE_HIVE_OL_HOOK_VERSION.jar" "$HIVE_LIB_DIR/hive-openlineage-hook.jar"
 }
 
 function restart_hive_server2_master() {
